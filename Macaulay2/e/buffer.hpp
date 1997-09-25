42a3de43af27258cf65fc21f725749de1cbbf7a6
Mike Stillman <mike@math.cornell.edu>
1997-09-25 13:49:30 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@99 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ Macaulay2/e/buffer.hpp
@@ -0,0 +1,55 @@
+// Copyright 1997 by  Michael E. Stillman
+#ifndef _buffer_hpp_
+#define _buffer_hpp_
+
+const int BUFFER_INITIAL_CAPACITY = 100;
+
+class buffer
+{
+  int _size;
+  int _capacity;
+  char *_buf;
+  void expand(int newcap);
+public:
+  buffer() : _size(0), 
+    _capacity(BUFFER_INITIAL_CAPACITY), 
+    _buf(new char[BUFFER_INITIAL_CAPACITY]) {}
+  ~buffer() { delete [] _buf; }
+  
+  void reset() { _size = 0; }
+
+  int size() { return _size; }
+  int capacity() { return _capacity; }
+  char *str() { _buf[_size+1] = '\0'; return _buf; }
+
+  void put(const char *s);		// Place null-terminated string into buffer
+  void put(const char *s, int len);	// Place a string possible containing null chars
+  void put(char c);		// Place a single character
+  void put(int n);		// Format the integer, place into buffer
+  void put(int n, int width);	// Format the integer, with given width field.
+  void put(long n);		// Format the integer, place into buffer
+  void put(long n, int width);	// Format the integer, with given width field.
+  void put(unsigned int n);		// Format the integer, place into buffer
+  void put(unsigned int n, int width);	// Format the integer, with given width field.
+  void put(unsigned long n);		// Format the integer, place into buffer
+  void put(unsigned long n, int width);	// Format the integer, with given width field.
+
+  // To put an endline in:
+  // o.put(newline);
+
+  // To print the resulting string
+  // buffer o;
+  // o.put("hi there");
+  // o.put(5);
+  // o.put(newline);
+  // cerr << o.str();
+
+  buffer &operator<<(const char *s) { put(s); return *this; }
+  buffer &operator<<(long n) { put(n); return *this; }
+  buffer &operator<<(unsigned int n) { put(n); return *this; }
+  buffer &operator<<(unsigned long n) { put(n); return *this; }
+  buffer &operator<<(int n) { put(n); return *this; }
+  buffer &operator<<(char c) { put(c); return *this; }
+};
+
+#endif
