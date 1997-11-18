5772526293a94c4c862e2508c03bcda8f8093383
Mike Stillman <mike@math.cornell.edu>
1997-11-18 17:14:01 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@116 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ M2/Macaulay2/e/geopoly.hpp
@@ -0,0 +1,146 @@
+// Copyright 1996  Michael E. Stillman
+
+// This should probably be done by:
+// (a) making a type Ring, that FreeModule, and res_poly
+//     both can inherit from: but this is a bit of a kludge...
+// (b) making a vector type with a next and coeff field, that
+//     is then inherited by vecterm, resterm.
+// Redefine:
+// Ring
+//    routines that should be implemented in this class:
+//    add_to, compare, Ring_of, remove
+// Nterm *
+//    fields of this structure type should include:
+//    next, coeff
+
+const int GEOHEAP_SIZE = 15;
+
+class geobucket
+{
+  const Ring *F;		// Our elements will be vectors in here
+  const Ring *K;		// The coefficient ring
+  Nterm * heap[GEOHEAP_SIZE];
+  int top_of_heap;
+
+public:
+  geobucket(const Ring *F);
+  ~geobucket();
+
+  void add(Nterm * p);
+  Nterm * remove_lead_term();	// Returns NULL if none.
+
+  Nterm * value();		// Returns the linearized value, and resets the geobucket.
+
+  Nterm * debug_list(int i) { return heap[i]; } // DO NOT USE, except for debugging purposes!
+};
+
+static int heap_size[GEOHEAP_SIZE] = {4, 16, 64, 256, 1024, 4096, 
+				    16384, 65536, 262144, 1048576, 4194304,
+				    16777216, 67108864, 268435456,
+				    1073741824};
+
+inline geobucket::geobucket(const Ring *FF)
+: F(FF),
+  K(FF->Ring_of()->Ncoeffs()),
+  top_of_heap(-1)
+{
+  // set K
+  int i;
+  for (i=0; i<GEOHEAP_SIZE; i++)
+    heap[i] = NULL;
+}
+
+inline geobucket::~geobucket()
+{
+  // The user of this class must insure that all 'vecterm's
+  // have been removed first.  Thus, we don't need to
+  // do anything here.
+}
+
+inline void geobucket::add(Nterm * p)
+{
+  int len = F->n_terms(p);
+  int i= 0;
+  while (len >= heap_size[i]) i++;
+
+  ring_elem tmp1 = heap[i];
+  ring_elem tmp2 = p;
+  F->add_to(tmp1, tmp2);
+  heap[i] = tmp1;
+
+  len = F->n_terms(heap[i]);
+  p = NULL;
+  while (len >= heap_size[i])
+    {
+      i++;
+
+      tmp1 = heap[i];
+      tmp2 = heap[i-1];
+      F->add_to(tmp1,tmp2);
+      heap[i] = tmp1;
+
+      len = F->n_terms(heap[i]);
+      heap[i-1] = NULL;
+    }
+  if (i > top_of_heap)
+    top_of_heap = i;
+}
+
+inline Nterm * geobucket::remove_lead_term()
+{
+  int lead_so_far = -1;
+  for (int i=0; i <= top_of_heap; i++)
+    {
+      if (heap[i] == NULL) continue;
+      if (lead_so_far < 0) 
+	{
+	  lead_so_far = i;
+	  continue;
+	}
+      int cmp = EQ; //F->compare(heap[lead_so_far], heap[i]);
+      if (cmp == GT) continue;
+      if (cmp == LT)
+	{
+	  lead_so_far = i;
+	  continue;
+	}
+      // At this point we have equality
+      K->add_to(heap[lead_so_far]->coeff, heap[i]->coeff);
+      Nterm * tmp = heap[i];
+      heap[i] = tmp->next;
+      tmp->next = NULL;
+      F->remove(tmp);
+
+      if (K->is_zero(heap[lead_so_far]->coeff))
+	{
+	  // Remove, and start over
+	  tmp = heap[lead_so_far];
+	  heap[lead_so_far] = tmp->next;
+	  tmp->next = NULL;
+	  F->remove(tmp);
+	  lead_so_far = -1;
+	  i = -1;
+	}
+    }
+  if (lead_so_far < 0) return NULL;
+  Nterm * result = heap[lead_so_far];
+  heap[lead_so_far] = result->next;
+  result->next = NULL;
+  return result;
+}
+
+inline Nterm * geobucket::value()
+{
+  Nterm * result = NULL;
+  for (int i=0; i<=top_of_heap; i++)
+    {
+      if (heap[i] == NULL) continue;
+      ring_elem tmp1 = result;
+      ring_elem tmp2 = heap[i];
+      F->add_to(tmp1, tmp2);
+      result = tmp1;
+      heap[i] = NULL;
+    }
+  top_of_heap = -1;
+  return result;
+}
