2aa5be5d4a391fdc75616634b985ea436e4983c4
Daniel Grayson <dan@math.uiuc.edu>
1998-12-20 00:28:56 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@351 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/html/Macaulay2-button.scm
+++ Macaulay2/html/Macaulay2-button.scm
@@ -1,7 +1,7 @@
 (define (cadddr x) (car (cdddr x)))
 
 (define (Macaulay2-button text filename)
-  (let* ((padding 4) (image-width 110) (image-height 30) (color '(101 176 206))
+  (let* ((padding 4) (image-width 110) (image-height 30) 
 	 (image (car (gimp-image-new image-width image-height RGB)))
 	 (drawable (car (gimp-layer-new image image-width image-height RGB "layer 1" 100 NORMAL)))
 	 (border 0) (antialias TRUE) (size 20.0) (size-type POINTS) (foundry "*") (family "charter")
@@ -12,8 +12,18 @@
 	 (x-text (/ (- image-width text-width) 2)) (y-text (- image-height text-height))
 	 )
     (gimp-image-add-layer image drawable 0)
-    (gimp-palette-set-background color)
-    (gimp-edit-fill image drawable)
+    (gimp-palette-set-background  '(20 40 40))
+    (gimp-palette-set-foreground '(128 255 255))
+    (gimp-blend image drawable FG-BG-RGB NORMAL SHAPEBURST-SPHERICAL
+		100.0			;opacity
+		0			;offset
+		REPEAT-NONE
+		FALSE 0 0		;supersample
+		0 0			;x1 y1
+		0 0			;x2 y2
+		)
+    ; (gimp-edit-fill image drawable)
+    (gimp-palette-set-foreground '(0 0 255))
     (gimp-floating-sel-anchor
      (car (gimp-text image drawable x-text y-text text border antialias size size-type
 		     foundry family weight slant set-width spacing)))
@@ -26,8 +36,8 @@
  "Macaulay2-button"
  "<Toolbox>/Xtns/Macaulay2/Button"
  "Button for Macaulay 2"
- "Dan Grayson" "public domain" "1997"
+ "Dan Grayson <dan@math.uiuc.edu>" "public domain" "1997"
  "" 
- SF-VALUE "text" "\"up\""
+ SF-VALUE "text" "\"Next\""
  SF-VALUE "filename" "\"up.gif\""
  )
