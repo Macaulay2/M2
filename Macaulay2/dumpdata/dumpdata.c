f7f72760e45bc847fd895583a3cf566422943b85
Daniel Grayson <dan@math.uiuc.edu>
1999-01-12 23:35:52 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@357 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/dumpdata/dumpdata.c
+++ Macaulay2/dumpdata/dumpdata.c
@@ -1,5 +1,9 @@
+#include <unistd.h>
 #include <stdio.h>
-#include <gc.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "dumpdata.h"
 
 static int isStack(map m) {
@@ -9,54 +13,66 @@ static int isStack(map m) {
 }
 
 static int isDumpable(map m) {
-  return m->w == 'w' && !isStack(m);
+  return m->w == 'w' && m->r == 'r' && m->p == 'p' && !isStack(m);
 }
 
-static void printmap(map m) {
-  printf("%08x-%08x %c%c%c%c %08x %02x:%02x %8d %s%s%s\n",
+static int checksum(unsigned char *p, unsigned int len) {
+  unsigned int c = 0;
+  while (0 < len--) c = 23 * c + *p++;
+  return c;
+}
+
+static void printmap(FILE *f, map m) {
+  fprintf(f,"%08x-%08x %c%c%c%c %08x %02x:%02x %8d %10u %s\n",
 	 (int)m->from, (int)m->to,
 	 m->r, m->w, m->x, m->p,
 	 m->offset,
 	 m->dev_major, m->dev_minor, m->inode,
-	 m->filename,
-	 isStack(m) ? "(stack)" : "",
-	 isDumpable(m) ? "*" : ""
+	 m->w == 'w' ? 0 : checksum((unsigned char *)m->from, m->to - m->from),
+	 m->filename
 	 );
 }
 
-void printmaps(map *m) {
-  if (m) for (; *m; m++) printmap(*m);
+static void dumpmap(int fd, map m) {
+  write(fd, m->from, m->to-m->from);
 }
 
-static char *strperm(char *s) {
-  unsigned int n = strlen(s);
-  char *t = GC_MALLOC_ATOMIC(n+1);
-  if (t == NULL) {
-    fprintf(stderr,"out of memory");
-    exit(1);
-  }
-  strcpy(t,s);
-  return t;
-}
-
-map *getmaps() {
-  static char *filename = "/proc/self/maps";
-  FILE *maps = fopen(filename,"r");
-  if (maps == NULL) {
-    fprintf(stderr,"can't open %s\n",filename);
-    exit(1);
-  }
+void dumpdata(const char *dumpfilename) {
+  struct MAP map;
+  char fn[1000], buf[1000];
+  int fd = open(dumpfilename,O_WRONLY|O_CREAT|O_TRUNC,0666);
+  FILE *f  = fdopen(fd,"w");
+  static char *mapfilename = "/proc/self/maps";
+  long pos, n;
+  FILE *maps = fopen(mapfilename,"r");
+  if (maps == NULL) { fprintf(stderr,"can't open %s\n",mapfilename); exit(1); }
   while (TRUE) {
-    struct MAP n;
-    char fn[1000], buf[1000];
     if (NULL == fgets(buf,sizeof buf,maps)) break;
     fn[0] = 0;
     if (0 == sscanf(buf,"%x-%x %c%c%c%c %x %x:%x %d %1000s",
-	   (int *)&n.from, (int *)&n.to, &n.r, &n.w, &n.x, &n.p,
-	   &n.offset, &n.dev_major, &n.dev_minor, &n.inode, 
+	   (int *)&map.from, (int *)&map.to, &map.r, &map.w, &map.x, &map.p,
+	   &map.offset, &map.dev_major, &map.dev_minor, &map.inode, 
 	   fn)) break;
-    n.filename = strperm(fn);
-    printmap(&n);
+    map.filename = fn;
+    if (isStack(&map)) continue;
+    printmap(f,&map);
   }
-  return 0;
+  pos = ftell(f);
+  n = ((pos + EXEC_PAGESIZE - 1)/EXEC_PAGESIZE) * EXEC_PAGESIZE - pos;
+  while (0 < n--) putc('\n',f);
+  fflush(f);
+  rewind(maps);
+  while (TRUE) {
+    if (NULL == fgets(buf,sizeof buf,maps)) break;
+    fn[0] = 0;
+    if (0 == sscanf(buf,"%x-%x %c%c%c%c %x %x:%x %d %1000s",
+	   (int *)&map.from, (int *)&map.to, &map.r, &map.w, &map.x, &map.p,
+	   &map.offset, &map.dev_major, &map.dev_minor, &map.inode, 
+	   fn)) break;
+    map.filename = fn;
+    if (!isDumpable(&map)) continue;
+    dumpmap(fd,&map);
+  }
+  fclose(maps);
+  close(fd);
 }
