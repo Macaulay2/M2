6b6e445b3c772ffbd4aae06a1346a1966b1ae179
Daniel Grayson <dan@math.uiuc.edu>
1999-01-16 07:13:34 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@366 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/M2lib.c
+++ Macaulay2/d/M2lib.c
@@ -357,6 +357,8 @@ char **argv;
 	argc = 5;
 #endif
 
+     GC_stackbottom = dummy;
+
      ONSTACK(saveenvp);
 
 #if defined(_WIN32)
@@ -455,6 +457,7 @@ char **argv;
      if (0 != sigsetjmp(loaddata_jump,TRUE)) {
 	  char **environ0;
      	  GC_free_space_divisor = 4;
+	  GC_stackbottom = &dummy;
 	  old_collections = GC_gc_no;
 #if !defined(__MWERKS__)
      	  environ = saveenvp;	/* environ is a static variable that points
