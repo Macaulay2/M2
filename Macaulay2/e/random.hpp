4304ab5fe69a80a022aafb69063385cd6f2dedd2
Daniel Grayson <dan@math.uiuc.edu>
2001-08-02 20:05:07 +0000

    make random number generator portable
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@987 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/random.hpp
+++ Macaulay2/e/random.hpp
@@ -1,23 +1,27 @@
 // Copyright 1997 by Michael Stillman
 
+// Mike, I replaced all uses of "long" by "int32", defined in targettypes.h, to make it work
+// on machines where long can be longer.
+#include "../c2/targettypes.h"
+
 #define Random RandomFoo
 
 class RingElement;
 
 class Random
 {
-  static long seed;
+  static int32 seed;
   static RingElement maxint;
-  static long maxlong;		// At the moment, this is the max bignum random
+  static int32 maxint32;		// At the moment, this is the max bignum random
 				// number that can be generated...
 public:
   static void i_random();
-  static void set_seed(long s);
+  static void set_seed(int32 s);
   static void set_max_int(RingElement a);
   static RingElement get_max_int();
 
-  static long random0();	// Return a random number in range 0..2^31-2
-  static long random0(long r);
+  static int32 random0();	// Return a random number in range 0..2^31-2
+  static int32 random0(int32 r);
   static RingElement random();
 };
 
