fecd6b698b5a321c7cb1966e6774c62d93dad4e3
Daniel Grayson <dan@math.uiuc.edu>
2003-05-28 03:03:33 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@1580 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/nash/nash.c
+++ M2/Macaulay2/nash/nash.c
@@ -5,14 +5,29 @@
  Hopefully we can use it to make gdb more transparent.
 */
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
+#define TRUE 1
+#define FALSE 0
 
-int argc;
-char **argv, *args;
+int argc, xargc;
+char **argv, **xargv;
 
-usage() {
+void parse(char *args) {
+  xargc = 1;
+  xargv = malloc((xargc + 1) * sizeof (*xargv));
+  if (xargv == NULL) {
+    fprintf(stderr,"%s: out of memory\n",argv[0]);
+    exit(1);
+  }
+  xargv[0] = args;
+  xargv[xargc] = NULL;
+}
+
+void usage() {
   fprintf(stderr,"usage: %s -c \"exec command arg1 arg2 ...\"\n",argv[0]);
   exit(1);
 }
@@ -20,6 +35,9 @@ usage() {
 int main (int _argc, char **_argv) {
   argc = _argc;
   argv = _argv;
-  if (argc != 2 || 0 != strcmp("-c",argv[1]) || 0 != strncmp("exec ",argv[2],5)) usage();
-  args = &argv[2] + 5;
+  if (argc != 3 || 0 != strcmp("-c",argv[1]) || 0 != strncmp("exec ",argv[2],5)) usage();
+  parse(argv[2] + 5);
+  execvp(xargv[0],xargv);
+  fprintf(stderr,"%s: failed to exec \"%s\"\n", argv[0], xargv[0]);
+  return 1;
 }
