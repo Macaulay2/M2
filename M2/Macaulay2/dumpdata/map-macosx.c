12c35eccb59ec2b88bc386eb0ab1c0679c5332f8
Daniel Grayson <dan@math.uiuc.edu>
2003-06-07 15:21:57 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@1608 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/dumpdata/map-macosx.c
+++ M2/Macaulay2/dumpdata/map-macosx.c
@@ -1,3 +1,5 @@
+/* dumpdata for mac os x doesn't work yet, and may never work */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h> 
@@ -16,6 +18,8 @@ int haveDumpdata() {
 }
 
 #define count(x) (sizeof(x)/sizeof(int))
+#define nest(info) info.is_submap
+#define record(info) (!(info.share_mode == SM_COW || info.share_mode == SM_PRIVATE_ALIASED || info.share_mode == SM_EMPTY))
 
 int printmaps() {
   int i=0, nesting_depth=0;
@@ -26,21 +30,23 @@ int printmaps() {
     int infoCnt = count(info);
     if (vm_region_recurse_64(mach_task_self(),&address,&size,&nesting_depth,(int *)&info,&infoCnt) != 0) 
       return ERROR;
-    if (info.is_submap) {
-      nesting_depth++;
-    }
+    if (nest(info)) nesting_depth++;
     else {
-      printf("%d: %08x-%08x [%dK] %c%c%c/%c%c%c nesting_depth=%d share_mode=%d\n",i,address,address+size,size/1024,
-	     (info.protection & VM_PROT_READ) ? 'r' : '-',
-	     (info.protection & VM_PROT_WRITE) ? 'w' : '-',
-	     (info.protection & VM_PROT_EXECUTE) ? 'x' : '-',
-	     (info.max_protection & VM_PROT_READ) ? 'r' : '-',
-	     (info.max_protection & VM_PROT_WRITE) ? 'w' : '-',
-	     (info.max_protection & VM_PROT_EXECUTE) ? 'x' : '-',
-	     nesting_depth,
-	     info.share_mode
-	     );
-      i++;
+      if (record(info)) {
+	i++;
+	printf("%d: %08x-%08x [%dK] %c%c%c/%c%c%c nesting_depth=%d share_mode=%d is_submap=%c\n",
+	       i,address,address+size,size/1024,
+	       (info.protection & VM_PROT_READ) ? 'r' : '-',
+	       (info.protection & VM_PROT_WRITE) ? 'w' : '-',
+	       (info.protection & VM_PROT_EXECUTE) ? 'x' : '-',
+	       (info.max_protection & VM_PROT_READ) ? 'r' : '-',
+	       (info.max_protection & VM_PROT_WRITE) ? 'w' : '-',
+	       (info.max_protection & VM_PROT_EXECUTE) ? 'x' : '-',
+	       nesting_depth,
+	       info.share_mode,
+	       info.is_submap ? 'y' : 'n'
+	       );
+      }
       address += size;
     }
   }
@@ -62,11 +68,9 @@ int nummaps () {
     int infoCnt = count(info);
     if (vm_region_recurse_64(mach_task_self(),&address,&size,&nesting_depth,(int *)&info,&infoCnt) != 0) 
       return ERROR;
-    if (info.is_submap) {
-      nesting_depth++;
-    }
+    if (nest(info)) nesting_depth++;
     else {
-      if (!(info.share_mode == SM_COW || info.share_mode == SM_PRIVATE_ALIASED || info.share_mode == SM_EMPTY)) i++;
+      if (record(info)) i++;
       address += size;
     }
   }
@@ -82,11 +86,9 @@ int getmaps(int nmaps, struct MAP maps[nmaps]) {
     int infoCnt = count(info);
     if (vm_region_recurse_64(mach_task_self(),&address,&size,&nesting_depth,(int *)&info,&infoCnt) != 0) 
       return ERROR;
-    if (info.is_submap) {
-      nesting_depth++;
-    }
+    if (nest(info)) nesting_depth++;
     else {
-      if (!(info.share_mode == SM_COW || info.share_mode == SM_PRIVATE_ALIASED || info.share_mode == SM_EMPTY)) {
+      if (record(info)) {
 	assert(i < nmaps);
 	maps[i].from = (void *)address;
 	maps[i].to = (void *)(address + size);
