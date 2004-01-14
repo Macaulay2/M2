962b7b2265e2815780ef99513614113995fb0331
Daniel Grayson <dan@math.uiuc.edu>
2004-01-14 21:36:41 +0000

    fixed a bug in equal(x:HashTable,y:HashTable):Expr -- it wasn't considering cache tables to be equal
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@1985 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/objects.d
+++ M2/Macaulay2/d/objects.d
@@ -121,17 +121,13 @@ export ancestor(o:HashTable,p:HashTable):bool := (
 	  o = o.parent;
 	  ));
 equal(x:HashTable,y:HashTable):Expr := (
-     if x==y then return True;
+     if x == y then return True;
      if x.hash != y.hash then return False;
-     if x.mutable 
-     || y.mutable
-     || x.numEntries != y.numEntries
-     || length(x.table) != length(y.table)
-     || x.class != y.class -- && False == equal(x.class,y.class) 
-     || x.parent != y.parent -- && False == equal(x.parent,y.parent) 
-     then return False;
-     if x.hash == 0					    -- cache tables has hash code 0
-     && ancestor(x.class,cacheTableClass) then return (True);
+     if x.class != y.class || x.parent != y.parent then return False;
+     if x.hash == 0					    -- cache tables have hash code 0
+     && ancestor(x.class,cacheTableClass) then return True;
+     if x.mutable || y.mutable then return False;
+     if x.numEntries != y.numEntries || length(x.table) != length(y.table) then return False;
      foreach a at i in x.table do (
 	  p := a;
 	  q := y.table.i;
