07d151d3f17b15f70ea70256d1e677b9c7054806
Daniel Grayson <dan@math.uiuc.edu>
2004-01-29 21:24:36 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@2100 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/test/engine/factor-init.c
+++ Macaulay2/test/engine/factor-init.c
@@ -1,20 +1,17 @@
-#define INTERIOR_POINTERS 0	/* would add only 0.04 sec */
-#define GMP_USES_GC 0		/* adds 0.1 sec */
-#define FACTORY_USES_GC 0	/* the main problem! */
-
-#if 0
-
-all on: 0m12.000s
-
-#endif
-
+#define INTERIOR_POINTERS 1	/* would add only 0.04 sec */
+#define GMP_USES_GC 1		/* adds 0.1 sec */
+#define GMP_TESTS_RETURN_VALUE_FROM_GC 0
+#define FACTORY_USES_GC 1	/* adds 4.7 sec to 7.5 second running time */
+#define FACTORY_TESTS_RETURN_VALUE_FROM_GC 0
+#define REPLACE_GETBLOCK 1
+#define REPLACE_MALLOC_BY_GC_IN_GETBLOCK 1
 
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <gmp.h>
 #include <gc.h>
-
 
 extern void outofmem();
 extern void IM2_initialize();
@@ -27,89 +24,129 @@ void arginits(int argc, char **argv) {
 
 static void init_gc(void) {
      GC_all_interior_pointers = INTERIOR_POINTERS;
-     GC_free_space_divisor = 2;	/* this was intended to be used only when we are about to dump data */
+     GC_free_space_divisor = 2;
      GC_init();
-     if (getenv("GC_free_space_divisor")) {
-	  GC_free_space_divisor = atoi(getenv("GC_free_space_divisor"));
-	  if (GC_free_space_divisor <= 0) {
-	       fprintf(stderr, "%s: non-positive GC_free_space_divisor value, %ld\n", 
-		    progname, GC_free_space_divisor);
-	       exit (1);
-	       }
-	  }
-     if (getenv("GC_enable_incremental") && atoi(getenv("GC_enable_incremental"))==1) {
-	  GC_enable_incremental();
-	  fprintf(stderr,"GC_enable_incremental()\n");
-	  }
-     if (getenv("GC_expand_hp")) {
-	  GC_expand_hp(atoi(getenv("GC_expand_hp")));
-	  }
+     // GC_enable_incremental();
      }
 
-/* we test gc to whether it properly marks pointers found in registers */
-static void uniq(void *p, ...) {
-  va_list a;
-  void *q[100];
-  int n = 0, i, j;
-  q[n++] = p;
-  va_start(a,p);
-  for (;(q[n] = va_arg(a,void *));n++) ;
-  va_end(a);
-  for (i=0; i<n; i++) for (j=0; j<i; j++) if (q[i] == q[j]) {
-    fprintf(stderr,
-	    "%s: error: gc library doesn't find all the active pointers!\n"
-	    "           Perhaps GC_push_regs was configured incorrectly.\n",
-	    progname
-	    );
-    exit(1);
-  }
-}
-static void test_gc (void) {
-  uniq(
-       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
-       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
-       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
-       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
-       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
-       (void *)0);
-}
-
+#if GMP_USES_GC
+static void GC_free2 (void *s, size_t old) { GC_FREE(s); }
+#if GMP_TESTS_RETURN_VALUE_FROM_GC
 static void *GC_malloc1 (size_t size_in_bytes) {
      void *p;
      p = GC_MALLOC_UNCOLLECTABLE(size_in_bytes);
      if (p == NULL) outofmem();
      return p;
      }
-
-static void GC_free2 (void *s, size_t old) {
-     GC_FREE(s);
-     }
-
 static void *GC_realloc3 (void *s, size_t old, size_t new) {
      void *p = GC_REALLOC(s,new);
      if (p == NULL) outofmem();
      return p;
      }
+#else
+static void *GC_malloc1 (size_t size_in_bytes) { return GC_MALLOC_UNCOLLECTABLE(size_in_bytes); }
+static void *GC_realloc3(void *s, size_t old, size_t new) { return GC_REALLOC(s,new); }
+#endif
+#endif
 
 static void init_gmp(void) {
 #if GMP_USES_GC
+#if GMP_TESTS_RETURN_VALUE_FROM_GC
   mp_set_memory_functions(GC_malloc1,GC_realloc3,GC_free2);
+#else
+  mp_set_memory_functions(GC_malloc,GC_realloc3,GC_free);
+#endif
 #endif
 }
 
 #if FACTORY_USES_GC
-void*     getBlock ( size_t size                                  ) { return GC_malloc1(size);                   }
+#if FACTORY_TESTS_RETURN_VALUE_FROM_GC
+void*     getBlock ( size_t size                                  ) { return GC_malloc1(size); }
 void* reallocBlock ( void * block, size_t oldsize, size_t newsize ) { return GC_realloc3(block,oldsize,newsize); }
-void     freeBlock ( void * block, size_t size                    ) { return GC_free2(block, size);              }
+void     freeBlock ( void * block, size_t size                    ) { return GC_free2(block, size); }
+#else
+void*     getBlock ( size_t size                                  ) { return GC_MALLOC(size); }
+void* reallocBlock ( void * block, size_t oldsize, size_t newsize ) { return GC_REALLOC(block,newsize); }
+void     freeBlock ( void * block, size_t size                    ) { return GC_FREE(block); }
+#endif
+#else
+#if REPLACE_GETBLOCK
+# if REPLACE_MALLOC_BY_GC_IN_GETBLOCK
+#  define malloc GC_MALLOC_UNCOLLECTABLE
+#  define free GC_FREE
+#  define realloc GC_REALLOC
+# endif
+    typedef struct dummy_le { struct dummy_le* next; } listentry;
+    static listentry * blocklist[7] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+#   define GETBLOCK( list, size ) { if ( blocklist[list] ) { \
+					  listentry* retval = blocklist[list]; \
+					  blocklist[list] = retval->next; \
+					  return (void*)retval; \
+				      } else { \
+					  char* retval = (char*)malloc( size ); \
+					  *((int*)retval) = size-4; \
+					  retval += 4; \
+					  return (void*)retval; \
+				      } }
+#   define FREEBLOCK( list, block ) { \
+					 listentry* dummy = (listentry*)block; \
+					 dummy->next = blocklist[list]; \
+					 blocklist[list] = dummy; \
+				     }
+
+    void* getBlock ( size_t size )
+    {
+	if ( size <= 4 ) GETBLOCK( 0, 8 )
+	else if ( size <= 12 ) GETBLOCK( 1, 16 )
+	else if ( size <= 28 ) GETBLOCK( 2, 32 )
+	else if ( size <= 60 ) GETBLOCK( 3, 64 )
+	else if ( size <= 124 ) GETBLOCK( 4, 128 )
+	else if ( size <= 252 ) GETBLOCK( 5, 256 )
+	else if ( size <= 508 ) GETBLOCK( 6, 512 )
+	else {
+	    char* retval = (char*)malloc( size+4 );
+	    *((int*)retval) = size;
+	    retval += 4;
+	    return retval;
+	}
+    }
+
+    void freeBlock ( void* block, size_t size )
+    {
+	char* dum = (char*)block;
+	if ( block == NULL ) return;
+	dum -= 4;
+	size = *((int*)dum);
+	if ( size == 4 ) FREEBLOCK( 0, block )
+	else if ( size == 12 ) FREEBLOCK( 1, block )
+	else if ( size == 28 ) FREEBLOCK( 2, block )
+	else if ( size == 60 ) FREEBLOCK( 3, block )
+	else if ( size == 124 ) FREEBLOCK( 4, block )
+	else if ( size == 252 ) FREEBLOCK( 5, block )
+	else if ( size == 508 ) FREEBLOCK( 6, block )
+	else free( dum );
+    }
+
+    void* reallocBlock ( void* block, size_t oldsize, size_t newsize )
+    {
+	void* dummy = getBlock( newsize );
+	memcpy( dummy, block, (newsize < oldsize) ? newsize : oldsize );
+	freeBlock( block, oldsize );
+	return dummy;
+    }
+
+#undef malloc
+#undef free
+#undef realloc
+
+#endif
 #endif
 
-int M2inits_run;
-
 void M2inits(void) {
+  static int M2inits_run;
   extern void M2inits1(void), M2inits2(void);
   if (M2inits_run) return;
   init_gc();
-  // test_gc();
   init_gmp();
   // IM2_initialize();
   M2inits_run = 1;
