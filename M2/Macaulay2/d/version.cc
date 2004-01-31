9ef361b7e3abb4238e7464cc981d6109cd8f9c8c
Daniel Grayson <dan@math.uiuc.edu>
2004-01-31 17:05:21 +0000

    new option --disable-factory, macro variable "FACTORY"
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@2110 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/version.cc
+++ M2/Macaulay2/d/version.cc
@@ -1,5 +1,20 @@
+#include "config.h"
+#ifdef FACTORY
 #include <factor.h>		/* to get version number of libfac */
+#endif
 
 extern "C" {
-  const char *get_libfac_version() { return libfac_version; }
+  const char *get_libfac_version() { 
+#ifdef FACTORY
+    return libfac_version; 
+#else
+    return "library 'libfac' not installed";
+#endif
+  }
 }
+
+//
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/d"
+// End:
+//
