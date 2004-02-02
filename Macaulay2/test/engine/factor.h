febdc3df0f6a6e9f9f1cb7f59143e1c5805a063a
Daniel Grayson <dan@math.uiuc.edu>
2004-02-02 00:37:59 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@2118 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/test/engine/factor.h
+++ Macaulay2/test/engine/factor.h
@@ -1,5 +1,7 @@
 /* 
   basic running time, all options below set to 0 : 7.56, 7.58, 7.57
+  now it's risen to 7.84, 7.84, 7.83 for no reason I know
+  hmm, if I compile factory with -O3 instead of -O2 I get 8.17, 8.22, 8.24 -- that's counterintuitive
 */
 
 /* without -lcfmem (see Makefile.in) we use the native operator new/delete: 8.66, 8.67, 8.67 */
@@ -13,17 +15,17 @@
 */
 
 /* for factor.cc */
-#define NEW_USES_GC 0		/* adds 3.77 sec : 8.87, 8.86, 8.85 (GC_MALLOC) ; 9.08, 9.06, 9.06 (GC_MALLOC_UNCOLLECTABLE) */
+#define NEW_USES_GC 0		/* adds 1.77 sec : 8.87, 8.86, 8.85 (GC_MALLOC) ; 9.08, 9.06, 9.06 (GC_MALLOC_UNCOLLECTABLE) */
 #define NEW_TESTS_RETURN_VALUE_FROM_GC 0
 
 /* for factor-init.c */
-#define INTERIOR_POINTERS 0	/* */
-#define GMP_USES_GC 0		/* adds 0.05 : 7.12, 7.13, 7.14, 7.12, 7.10, 7.10 */
-#define GMP_TESTS_RETURN_VALUE_FROM_GC 0 /* subtracts 0.05, so leave it in */
+#define INTERIOR_POINTERS 1	/* 7.63, 7.64, no change */
+#define GMP_USES_GC 1		/* adds 0.05 : 7.12, 7.13, 7.14, 7.12, 7.10, 7.10, but now it's 7.52, 7.55 ??? */
+#define GMP_TESTS_RETURN_VALUE_FROM_GC 1 /* subtracts 0.05, so leave it in, now 7.63, 7.64 */
 #define FACTORY_USES_GC_DIRECTLY 0	/* adds 5.20 sec */
 #define FACTORY_TESTS_RETURN_VALUE_FROM_GC 0
-#define REPLACE_GETBLOCK 0	/* no change, our code is identical */
-#define REPLACE_MALLOC_BY_GC_IN_GETBLOCK 0 /* 7.06, 7.11, 7.09, 7.08 -- speeds it up! */
+#define REPLACE_GETBLOCK 1	/* improved realloc algorithm, notices when we have enough space, but it slows down: 8.82, 8.83 ???? */
+#define REPLACE_MALLOC_BY_GC_IN_GETBLOCK 1 /* 7.06, 7.11, 7.09, 7.08 -- speeds it up!  But now it's 7.73, 7.72 ??? */
 #define REPLACE_GC_FREE_BY_NOP 0 /* 7.63, 7.64, 7.62 */
 
 /* consequences */
@@ -36,7 +38,7 @@
 #endif
 
 /*
-// Local Variables:
-// compile-command: "make -C $M2BUILDDIR/Macaulay2/test/engine benchmark"
-// End:
+ Local Variables:
+ compile-command: "make -C $M2BUILDDIR/Macaulay2/test/engine benchmark"
+ End:
 */
