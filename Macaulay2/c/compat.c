e5a48dffd20d178e4ee6ba80337a684c8f4ef8a8
Daniel Grayson <dan@math.uiuc.edu>
2004-02-15 02:50:57 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@2153 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/c/compat.c
+++ Macaulay2/c/compat.c
@@ -1,15 +1,15 @@
 /*		Copyright 1993 by Daniel R. Grayson		*/
 
 #ifdef MPWC
-char posfmt[] = "    File \"%s\"; Line %d # Column %d:";
+char posfmt[] = "    File \"%s\"; Line %d # Column %d: Loaddepth %d: ";
 char errfmt[] = "    File \"%s\"; Line %d # Column %d: %s";
 char errfmtnc[] = "    File \"%s\"; Line %d # %s";
 #elif defined(_WIN32) && !defined(__CYGWIN32__)
-char posfmt[] = "%s(%d) : column %d :";
+char posfmt[] = "%s(%d) : column %d : loaddepth %d : ";
 char errfmt[] = "%s(%d) : column %d : %s";
 char errfmtnc[] = "%s(%d) : %s";
 #else
-char posfmt[] = "%s:%d:%d:";
+char posfmt[] = "%s:%d:%d:(%d):";
 char errfmt[] = "%s:%d:%d: %s";
 char errfmtnc[] = "%s:%d: %s";
 #endif
