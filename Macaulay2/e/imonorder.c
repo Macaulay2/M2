54008a2fd2ea444e234283270f25617e64ba8d77
Mike Stillman <mike@math.cornell.edu>
2004-05-25 02:49:06 +0000

    some files for monomial handling
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@2543 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ Macaulay2/e/imonorder.c
@@ -0,0 +1,624 @@
+#include "imonorder.h"
+#include "../d/M2mem.h"
+/* TODO:
+   -- negative exponent versions need to be included (at least for MO_LEX)
+   -- non-commutative blocks should be added in
+*/
+
+static int ntmpexp = 0;
+static int *tmpexp = 0; /* Set to an array 0..ntmpexp-1 of ints */
+
+static void mo_block_revlex(struct mo_block *b, int nvars)
+{
+  b->typ = MO_REVLEX;
+  b->nvars = nvars;
+  b->nslots = nvars;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_grevlex(struct mo_block *b, int nvars)
+{
+  b->typ = MO_GREVLEX;
+  b->nvars = nvars;
+  b->nslots = nvars;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_grevlex2(struct mo_block *b, int nvars)
+{
+  b->typ = MO_GREVLEX2;
+  b->nvars = nvars;
+  b->nslots = (nvars+1)/2; /* 2 per word */
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_grevlex4(struct mo_block *b, int nvars)
+{
+  b->typ = MO_GREVLEX4;
+  b->nvars = nvars;
+  b->nslots = (nvars + 3)/4;  /* 4 per word */
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_grevlex_wts(struct mo_block *b, int nvars)
+{
+  b->typ = MO_GREVLEX_WTS;
+  b->nvars = nvars;
+  b->nslots = nvars;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = nvars;
+  b->weights = 0; /* will be set later */
+}
+
+static void mo_block_grevlex2_wts(struct mo_block *b, int nvars)
+{
+  b->typ = MO_GREVLEX2_WTS;
+  b->nvars = nvars;
+  b->nslots = (nvars+1)/2; /* 2 per word */
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = nvars;
+  b->weights = 0; /* will be set later */
+}
+
+static void mo_block_grevlex4_wts(struct mo_block *b, int nvars)
+{
+  b->typ = MO_GREVLEX4_WTS;
+  b->nvars = nvars;
+  b->nslots = (nvars + 3)/4;  /* 4 per word */
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = nvars;
+  b->weights = 0; /* will be set later */
+}
+
+static void mo_block_lex(struct mo_block *b, int nvars)
+{
+  b->typ = MO_LEX;
+  b->nvars = nvars;
+  b->nslots = nvars;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_lex2(struct mo_block *b, int nvars)
+{
+  b->typ = MO_LEX2;
+  b->nvars = nvars;
+  b->nslots = (nvars+1)/2; /* 2 per word */
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_lex4(struct mo_block *b, int nvars)
+{
+  b->typ = MO_LEX4;
+  b->nvars = nvars;
+  b->nslots = (nvars + 3)/4;  /* 4 per word */
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_group_lex(struct mo_block *b, int nvars)
+{
+  b->typ = MO_LAURENT;
+  b->nvars = nvars;
+  b->nslots = nvars;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_group_revlex(struct mo_block *b, int nvars)
+{
+  b->typ = MO_LAURENT_REVLEX;
+  b->nvars = nvars;
+  b->nslots = nvars;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_position_up(struct mo_block *b)
+{
+  b->typ = MO_POSITION_UP;
+  b->nvars = 0;
+  b->nslots = 0;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_position_down(struct mo_block *b)
+{
+  b->typ = MO_POSITION_DOWN;
+  b->nvars = 0;
+  b->nslots = 0;
+  b->first_exp = 0; /* will be set later */
+  b->first_slot = 0; /* will be set later */
+  b->nweights = 0;
+  b->weights = 0;
+}
+
+static void mo_block_wt_function(struct mo_block *b, int nvars, double *wts)
+{
+  b->typ = MO_WEIGHTS;
+  b->nvars = 0;
+  b->nslots = 1;
+  b->first_exp = 0;
+  b->first_slot = 0; /* will be set later */
+  b->nweights = nvars;
+  b->weights = wts;
+}
+
+MonomialOrder *monomialOrderMake(const MonomialOrdering *mo)
+{
+  MonomialOrder *result;
+  int i,j,nv,this_block;
+  double *wts;
+  /* Determine the number of variables, the number of blocks, and the location
+     of the component */
+  int nblocks = 0;
+  int nvars = 0;
+  int hascomponent = 0;
+  int isnoncomm = 0;
+  for (i=0; i<mo->len; i++)
+    {
+      struct mon_part_rec_ *t = mo->array[i];
+      nblocks++;
+      if (t->type == MO_POSITION_DOWN || t->type == MO_POSITION_UP)
+	hascomponent++;
+      else if (t->type == MO_NC_LEX)
+	isnoncomm = 1;
+      if (t->type != MO_WEIGHTS)
+	nvars += t->nvars;
+    }
+  nblocks -= hascomponent;
+
+  /* Now create the blocks, and fill them in. Also fill in the deg vector */
+  result = (MonomialOrder *) getmem(sizeof(MonomialOrder));
+  result->nvars = nvars;
+  result->nslots = 0;
+  result->nblocks = nblocks;
+  result->blocks = (struct mo_block *) getmem(nblocks * sizeof(struct mo_block));
+  result->degs = (double *) getmem_atomic(nvars * sizeof(double));
+  if (hascomponent == 0)
+    result->nblocks_before_component = nblocks;
+
+  this_block = 0;
+  nvars = 0;
+  for (i=0; i<mo->len; i++, this_block++)
+    {
+      struct mon_part_rec_ *t = mo->array[i];
+      if (t->type != MO_WEIGHTS)
+	{
+	  if (t->wts == 0)
+	    for (j=0; j<t->nvars; j++)
+	      result->degs[nvars++] = 1;
+	  else
+	    for (j=0; j<t->nvars; j++)
+	      result->degs[nvars++] = t->wts[j];
+	}
+      else
+	{
+	  wts = (double *) getmem_atomic(t->nvars * sizeof(double));
+	  for (j=0; j<t->nvars; j++)
+	    wts[j] = t->wts[j];
+	}
+      switch (t->type) {
+      case MO_REVLEX:
+	mo_block_revlex(result->blocks + this_block, t->nvars);
+	break;
+      case MO_GREVLEX:
+	mo_block_grevlex(result->blocks + this_block, t->nvars);
+	break;
+      case MO_GREVLEX2:
+	mo_block_grevlex2(result->blocks + this_block, t->nvars);
+	break;
+      case MO_GREVLEX4:
+	mo_block_grevlex4(result->blocks + this_block, t->nvars);
+	break;
+      case MO_GREVLEX_WTS:
+	mo_block_grevlex_wts(result->blocks + this_block, t->nvars);
+	break;
+      case MO_GREVLEX2_WTS:
+	mo_block_grevlex2_wts(result->blocks + this_block, t->nvars);
+	break;
+      case MO_GREVLEX4_WTS:
+	mo_block_grevlex4_wts(result->blocks + this_block, t->nvars);
+	break;
+      case MO_LEX:
+	mo_block_lex(result->blocks + this_block, t->nvars);
+	break;
+      case MO_LEX2:
+	mo_block_lex2(result->blocks + this_block, t->nvars);
+	break;
+      case MO_LEX4:
+	mo_block_lex4(result->blocks + this_block, t->nvars);
+	break;
+      case MO_WEIGHTS:
+	mo_block_wt_function(result->blocks + this_block, t->nvars, wts);
+	break;
+      case MO_LAURENT:
+	mo_block_group_lex(result->blocks + this_block, t->nvars);
+	break;
+      case MO_LAURENT_REVLEX:
+	mo_block_group_revlex(result->blocks + this_block, t->nvars);
+	break;
+      case MO_NC_LEX:
+	/* MES */
+	break;
+      case MO_POSITION_UP:
+	if (--hascomponent == 0)
+	  {
+	    // Set the information about the component
+	    result->component_up = 1;
+	    result->nblocks_before_component = this_block;
+	  }
+	//  mo_block_position_up(result->blocks + this_block);
+	break;
+      case MO_POSITION_DOWN:
+	if (--hascomponent == 0)
+	  {
+	    // Set the information about the component
+	    result->component_up = 0;
+	    result->nblocks_before_component = this_block;
+	  }
+	//  mo_block_position_down(result->blocks + this_block);
+	break;
+      }
+    }
+
+  /* Go back and fill in the 'slots' information */
+  /* Now fix the first_exp, first_slot values, and also result->{nslots,nvars}; */
+  nv = 0;
+  result->nslots = 0;
+  for (i=0; i<nblocks; i++)
+    {
+      enum MonomialOrdering_type typ = result->blocks[i].typ;
+
+
+      result->blocks[i].first_exp = nv;
+      result->blocks[i].first_slot = result->nslots;
+      nv += result->blocks[i].nvars;
+      result->nslots += result->blocks[i].nslots;
+
+      if (typ == MO_WEIGHTS)
+	{
+	  result->blocks[i].first_exp = 0;
+
+	  /* divide the wt vector by the degree vector */
+	  for (j=0; j<result->blocks[i].nvars; j++)
+	    result->blocks[i].weights[j] /= result->degs[j];
+	}
+      else if (typ == MO_GREVLEX_WTS || typ == MO_GREVLEX2_WTS || typ == MO_GREVLEX4_WTS)
+	{
+	  result->blocks[i].weights = result->degs + result->blocks[i].first_exp;
+	}
+
+      if (i == result->nblocks_before_component-1)
+	{
+	  result->nslots_before_component = result->nslots;
+	}
+    }
+
+#if 0
+  result->component_slot = -1;
+  for (i=0; i<nblocks; i++)
+    if (result->blocks[i].typ == MO_POSITION_DOWN ||
+	result->blocks[i].typ == MO_POSITION_UP)
+      {
+	result->component_slot = result->blocks[i].first_slot;
+	result->blocks[i].first_exp = result->nvars;
+	break;
+      }
+#endif
+
+  /* Set is_laurent */
+  result->is_laurent = (int *) getmem_atomic(result->nvars * sizeof(int));
+  for (i=0; i<result->nvars; i++) result->is_laurent[i] = 0;
+
+  for (i=0; i<result->nblocks; i++)
+    if (result->blocks[i].typ == MO_LAURENT
+	|| result->blocks[i].typ == MO_LAURENT_REVLEX)
+      {
+	for (j=0; j < result->blocks[i].nvars; j++)
+	  result->is_laurent[result->blocks[i].first_exp + j] = 1;
+      }
+
+  /* Set tmpexp */
+  if (result->nvars >= ntmpexp)
+    {
+      ntmpexp = result->nvars+1;
+      tmpexp = (int *) getmem_atomic(ntmpexp *sizeof(int));
+    }
+  return result;
+}
+
+extern void monomialOrderFree(MonomialOrder *mo)
+{
+}
+
+union pack4 {
+  long i;
+  struct {
+    unsigned char a;
+    unsigned char b;
+    unsigned char c;
+    unsigned char d;
+  } ch;
+};
+	  
+#if 0
+static void MO_pack4(int nvars, const int *expon, int *slots)
+{
+  union pack4 w;
+  while (nvars > 0)
+    {
+      w.i = 0;
+      if (--nvars >= 0) {
+	w.ch.a = *expon++;
+	if (--nvars >= 0) {
+	  w.ch.b = *expon++;
+	  if (--nvars >= 0) {
+	    w.ch.c = *expon++;
+	    if (--nvars >= 0) w.ch.d = *expon++;
+	  }}}
+      *slots++ = w.i;
+    }
+}
+#endif
+
+static void MO_pack4(int nvars, const int *expon, int *slots)
+{
+  union pack4 w;
+  while (nvars > 0)
+    {
+      w.i = 0;
+      if (--nvars >= 0) {
+	w.ch.d = *expon++;
+	if (--nvars >= 0) {
+	  w.ch.c = *expon++;
+	  if (--nvars >= 0) {
+	    w.ch.b = *expon++;
+	    if (--nvars >= 0) w.ch.a = *expon++;
+	  }}}
+      *slots++ = w.i;
+    }
+}
+
+#if 0
+static void MO_unpack4(int nvars, const int *slots, int *expon)
+{
+  union pack4 w;
+  while (nvars > 0)
+    {
+      w.i = *slots++;
+      if (--nvars >= 0) {
+	*expon++ = w.ch.a;
+	if (--nvars >= 0) {
+	  *expon++ = w.ch.b;
+	  if (--nvars >= 0) {
+	    *expon++ = w.ch.c;
+	    if (--nvars >= 0) *expon++ = w.ch.d;
+	  }}}
+    }
+}
+#endif
+static void MO_unpack4(int nvars, const int *slots, int *expon)
+{
+  union pack4 w;
+  while (nvars > 0)
+    {
+      w.i = *slots++;
+      if (--nvars >= 0) {
+	*expon++ = w.ch.d;
+	if (--nvars >= 0) {
+	  *expon++ = w.ch.c;
+	  if (--nvars >= 0) {
+	    *expon++ = w.ch.b;
+	    if (--nvars >= 0) *expon++ = w.ch.a;
+	  }}}
+    }
+}
+
+void monomialOrderEncode(const MonomialOrder *mo, 
+			    const_exponents expon, 
+			    monomial result_psums)
+     /* Given 'expon', compute the encoded partial sums value */
+{
+  int i,j,nvars,s;
+  int *p1;
+  struct mo_block *b = mo->blocks;
+  int nblocks = mo->nblocks;
+  const int *e = expon;
+  int *p = result_psums;
+  for (i=nblocks; i>0; --i, b++)
+    switch (b->typ) {
+    case MO_LEX:
+    case MO_LAURENT:
+      nvars = b->nvars;
+      for (j=0; j<nvars; j++)
+	*p++ = *e++;
+      break;
+    case MO_REVLEX:
+    case MO_LAURENT_REVLEX:
+      nvars = b->nvars;
+      for (j=0; j<nvars; j++)
+	*p++ = - *e++;
+      break;
+    case MO_GREVLEX:
+    case MO_GREVLEX_WTS:
+      nvars = b->nvars;
+      p += b->nslots;
+      p1 = p;
+      *--p1 = *e++;
+      for (j=1; j<nvars; j++)
+	{
+	  --p1;
+	  *p1 = *e++ + p1[1];
+	}
+      break;
+    case MO_GREVLEX4:
+    case MO_GREVLEX4_WTS:
+      nvars = b->nvars;
+      p1 = tmpexp + b->nvars;
+      *--p1 = *e++;
+      for (j=1; j<nvars; j++)
+	{
+	  --p1;
+	  *p1 = *e++ + p1[1];
+	}
+      MO_pack4(nvars,p1,p);
+      p += b->nslots;
+      break;
+    case MO_GREVLEX2:
+    case MO_GREVLEX2_WTS:
+      /* handle this case yet */
+      break;
+    case MO_LEX4:
+      /* handle this case yet */
+      break;
+    case MO_LEX2:
+      /* handle this case yet */
+      break;
+    case MO_WEIGHTS:
+      s = 0;
+      for (j=0; j<b->nweights; j++)
+	s += b->weights[j] * expon[j];
+      *p++ = s;
+      break;
+    case MO_POSITION_UP:
+    case MO_POSITION_DOWN:
+      /* nothing to do here */
+      break;
+    case MO_NC_LEX:
+      /* nothing to do here */
+      break;
+    }
+}
+
+int monomialOrderFromActualExponents(const MonomialOrder *mo, 
+				      const_exponents expon, 
+				      exponents result_exp)
+/* Sets result_exp[i] = expon[i] * deg[i], i=0..nvars-1.
+   If a variable is negative, and shouldn't be, 0 is returned.
+   Otherwise 1 is returned. */
+{
+  int i;
+  int result = 1;
+  for (i=0; i<mo->nvars; i++)
+    {
+      result_exp[i] = expon[i] * mo->degs[i];
+      if (expon[i] < 0 && !mo->is_laurent[i])
+	result = 0;
+    }
+  return result;
+}
+
+int monomialOrderToActualExponents(const MonomialOrder *mo, 
+				      const_exponents expon, 
+				      exponents result_exp)
+{
+  int i;
+  for (i=0; i<mo->nvars; i++)
+    {
+      result_exp[i] = expon[i] / mo->degs[i];
+    }
+  return 1;
+}
+
+void monomialOrderDecode(const MonomialOrder *mo, const_monomial psums, exponents expon)
+{
+
+  int i,j,nvars;
+  struct mo_block *b = mo->blocks;
+  int nblocks = mo->nblocks;
+  int *e = expon;
+  const int *p = psums;
+  for (i=nblocks; i>0; --i, b++)
+    switch (b->typ) {
+    case MO_LEX:
+    case MO_LAURENT:
+      nvars = b->nvars;
+      p = psums + b->first_slot;
+      e = expon + b->first_exp;
+      for (j=0; j<nvars; j++)
+	*e++ = *p++;
+      break;
+    case MO_REVLEX:
+    case MO_LAURENT_REVLEX:
+      nvars = b->nvars;
+      p = psums + b->first_slot;
+      e = expon + b->first_exp;
+      for (j=0; j<nvars; j++)
+	*e++ = - *p++;
+      break;
+    case MO_GREVLEX:
+    case MO_GREVLEX_WTS:
+      nvars = b->nvars;
+      p = psums + b->first_slot + nvars - 1;
+      e = expon + b->first_exp;
+      *e++ = *p--;
+      for (j=nvars-1; j>=1; --j, --p)
+	*e++ = *p - p[1];
+      break;
+    case MO_GREVLEX4:
+    case MO_GREVLEX4_WTS:
+      nvars = b->nvars;
+      MO_unpack4(nvars, psums + b->first_slot, tmpexp);
+      p = tmpexp + nvars - 1;
+      e = expon + b->first_exp;
+      *e++ = *p--;
+      for (j=nvars-1; j>=1; --j, --p)
+	*e++ = *p - p[1];
+      break;
+    case MO_GREVLEX2:
+    case MO_GREVLEX2_WTS:
+      /* handle this case yet */
+      break;
+    case MO_LEX4:
+      /* handle this case yet */
+      break;
+    case MO_LEX2:
+      /* handle this case yet */
+      break;
+    case MO_WEIGHTS:
+      break;
+    case MO_POSITION_UP:
+    case MO_POSITION_DOWN:
+      /* should not occur, but do nothing in any case */
+      break;
+    case MO_NC_LEX:
+      /* nothing to do here */
+      break;
+    }
+}
+
+/*
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
+// End:
+*/
---
+++ Macaulay2/e/imonorder.h
@@ -0,0 +1,72 @@
+#ifndef __imonorder_h_
+#define __imonorder_h_
+
+/* This is the internal form of the monomial ordering */
+/* Used in monomial encoding/decoding/comparison */
+
+#include "engine.h"
+#include "monordering.h"
+
+typedef int * exponents;
+typedef int * monomial;
+
+typedef const int * const_exponents;
+typedef const int * const_monomial;
+
+struct mo_block {
+  enum MonomialOrdering_type typ;
+  int nvars;
+  int nslots;
+  int first_exp;
+  int first_slot;
+  int nweights;
+  double *weights;
+};
+
+struct MonomialOrder_rec {
+  int nvars;
+  int nslots;
+  int nblocks;
+  int nblocks_before_component;
+  int nslots_before_component;
+  int component_up; /* bool */
+#if 0
+  int component_slot; /* < 0 means that there is no component (or, it is assumed to be 0) */
+#endif
+  double *degs; /* 0..nvars: heuristic degree of each variable.  degs[nvars] = 1.
+		   Assumption: degs[i] >= 1, for all i, and should be an integer.
+		   Any graded rev lex block assumes graded wrt these degrees. */
+  struct mo_block *blocks; /* 0..nblocks-1 with each entry a struct mo_block */
+  int *is_laurent; /* 0..nvars-1: 0 or 1: 1 means negative exponents allowed */
+};
+
+typedef struct MonomialOrder_rec MonomialOrder;
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+  MonomialOrder *monomialOrderMake(const MonomialOrdering *mo);
+  void monomialOrderFree(MonomialOrder *mo);
+  void monomialOrderEncode(const MonomialOrder *mo, const_exponents a, monomial b);
+  void monomialOrderDecode(const MonomialOrder *mo, const_monomial a, exponents b);
+
+  int monomialOrderFromActualExponents(const MonomialOrder *mo, 
+				       const_exponents expon, 
+				       exponents result_exp);
+
+  int monomialOrderToActualExponents(const MonomialOrder *mo, 
+				     const_exponents expon, 
+				     exponents result_exp);
+  
+#if defined(__cplusplus)
+}
+#endif
+
+
+#endif
+
+/*
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
+// End:
+*/
