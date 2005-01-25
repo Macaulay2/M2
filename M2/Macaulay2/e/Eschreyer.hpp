dc5169c37953f1480db8f8401727e1dd4092168e
Mike Stillman <mike@math.cornell.edu>
2005-01-25 01:16:07 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@2932 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/Eschreyer.hpp
+++ M2/Macaulay2/e/Eschreyer.hpp
@@ -54,6 +54,9 @@ class GBKernelComputation : public object
   int find_divisor(const MonomialIdeal * mi, const int *exponents, int &result);
   // Returns the index of the least element in the monomial order which divides.
 
+  void GBKernelComputation::wipe_unneeded_terms(gbvector * & f);
+  // removes every term of f which is not a lead term of some element of gb.
+
   gbvector * s_pair(gbvector * syz) const;
   void reduce(gbvector * &g, gbvector * &gsyz);  // Reduces g to zero.  gsyz is real result.
 public:
