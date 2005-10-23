51f1885d6dc094be0fcd3921dc1af9c5fd83508f
Mike Stillman <mike@math.cornell.edu>
2005-10-23 11:39:45 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@3568 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/linalgGB/monsort.cpp
+++ M2/Macaulay2/e/linalgGB/monsort.cpp
@@ -2,30 +2,32 @@
 
 #include "monsort.hpp"
 
-long MonomialSorter::sort_partition(long lo, long hi)
+template<typename Sorter>
+long QuickSorter<Sorter>::sort_partition(long lo, long hi)
 {
-  monomial pivot = monoms[lo];
+  value pivot = elems[lo];
   long i = lo-1;
   long j = hi+1;
   for (;;)
     {
       do { j--; }
-      while (M->compare_grevlex(monoms[j], pivot) < 0);
+      while (M->compare(elems[j], pivot) < 0);
       do { i++; }
-      while (M->compare_grevlex(monoms[i], pivot) > 0);
+      while (M->compare(elems[i], pivot) > 0);
 
       if (i < j)
 	{
-	  monomial tmp = monoms[j];
-	  monoms[j] = monoms[i];
-	  monoms[i] = tmp;
+	  value tmp = elems[j];
+	  elems[j] = elems[i];
+	  elems[i] = tmp;
 	}
       else
 	return j;
     }
 }
 
-void MonomialSorter::sort(long lo, long hi)
+template<typename Sorter>
+void QuickSorter<Sorter>::sort(long lo, long hi)
 {
   if (lo < hi)
     {
@@ -35,23 +37,49 @@ void MonomialSorter::sort(long lo, long hi)
     }
 }
 
-MonomialSorter::MonomialSorter(MonomialInfo *M0, monomial *monoms0, long len0)
-  : M(M0),
-    monoms(monoms0),
-    len(len0)
-{
+
+/*****   macros create functional code   *****/
+#define pivot_index() (begin+(end-begin)/2)
+#define swap(a,b,t) ((t)=(a),(a)=(b),(b)=(t))
+
+template<typename Sorter>
+void QuickSorter<Sorter>::sort2(long begin, long end) {
+   /*** Use of static here will reduce memory footprint, but will make it thread-unsafe ***/
+   static long pivot;
+   static long t;     /* temporary variable for swap */
+   if (end > begin) {
+      long l = begin + 1;
+      long r = end;
+      swap(elems[begin], elems[pivot_index()], t); /*** choose arbitrary pivot ***/
+      pivot = elems[begin];
+      while(l < r) {
+         if (M->compare(elems[l],pivot) <= 0) {
+            l++;
+         } else {
+            while(l < --r && M->compare(elems[r], pivot) >= 0)/*** skip superfluous swaps ***/
+               ;
+            swap(elems[l], elems[r], t); 
+         }
+      }
+      l--;
+      swap(elems[begin], elems[l], t);
+      sort2(begin, l);
+      sort2(r, end);
+   }
 }
 
-MonomialSorter::~MonomialSorter()
+#undef swap
+#undef pivot_index
+
+
+template<typename Sorter>
+void QuickSorter<Sorter>::sort(Sorter *M0, value *elems0, long len0)
 {
+  QuickSorter S(M0,elems0,len0);
+  S.sort2(0,len0-1);
 }
 
-void MonomialSorter::sortMonomials(MonomialInfo *M0, monomial *monoms0, long len0)
-{
-  MonomialSorter S(M0,monoms0,len0);
-  S.sort(0,len0-1);
-}
-
+template class QuickSorter<MSorter>;
 // Local Variables:
 // compile-command: "make -C $M2BUILDDIR/Macaulay2/e/linalgGB "
 // End:
