276d82e21337056a171d4d385ef4109a970b8dcc
Mike Stillman <mike@math.cornell.edu>
2005-10-23 11:39:45 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@3568 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/linalgGB/monsort.hpp
+++ Macaulay2/e/linalgGB/monsort.hpp
@@ -5,22 +5,61 @@
 
 #include "moninfo.hpp"
 
-class MonomialSorter
+template<typename Sorter>
+// Sorter S, S::value_type
+// int S.compare(S::value_type a, S::value_type b)
+class QuickSorter
 {
-  typedef MonomialInfo::monomial monomial;
-  MonomialInfo *M;
-  monomial *monoms;
+  typedef typename Sorter::value value;
+  Sorter *M;
+  value *elems;
   long len;
 
   long sort_partition(long lo, long hi);
   void sort(long lo, long hi);
+  void sort2(long lo, long hi);
 
-  MonomialSorter(MonomialInfo *M0, monomial *monoms0, long len0);
-  ~MonomialSorter();
+  QuickSorter(Sorter *M0, value *elems0, long len0)
+    : M(M0), elems(elems0), len(len0) {}
+  ~QuickSorter() {}
 public:
-  static void sortMonomials(MonomialInfo *M0, monomial *monoms0, long len0);
+  static void sort(Sorter *M0, value *elems0, long len0);
 };
 
+class MSorter
+{
+public:
+  typedef MonomialInfo::value monomial;
+  typedef long value;
+private:
+  MonomialInfo *M;
+  monomial *monoms;
+  long *indices;
+  long len;
+  long ncmps;
+public:
+  int compare(value a, value b)
+  {
+    ncmps ++;
+    return M->compare_grevlex(monoms[a],monoms[b]);
+  }
+
+  MSorter(MonomialInfo *M0, monomial *monoms0, long *indices0, long len0)
+    : M(M0), monoms(monoms0), indices(indices0), len(len0), ncmps(0) {}
+
+  ~MSorter() {} 
+
+  void sort() { 
+    QuickSorter<MSorter>::sort(this,indices,len); 
+    fprintf(stderr, "sort: nelements %ld ncompares %ld\n",len,ncmps);
+  }
+
+  static void sort(MonomialInfo *M0, monomial *monoms0, long *indices0, long len0) {
+    MSorter S(M0,monoms0,indices0,len0);
+    S.sort();
+  }
+
+};
 #endif
 
 // Local Variables:
