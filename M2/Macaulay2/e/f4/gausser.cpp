c0ca0cfbad2e55573a91790c8882d3839ac23a68
Mike Stillman <mike@math.cornell.edu>
2006-01-19 01:42:01 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@3597 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ M2/Macaulay2/e/f4/gausser.cpp
@@ -0,0 +1,23 @@
+// Copyright 2005 Michael E. Stillman.
+
+#include "gausser.hpp"
+
+Gausser::Gausser(const Ring *K)
+{
+}
+
+F4CoefficientArray Gausser::from_ringelem_array(int len, ring_elem *elems) const
+{
+}
+
+void Gausser::to_ringelem_array(int len, F4CoefficientArray, ring_elem *result) const
+{
+}
+
+void Gausser::remove_array(F4CoefficientArray a) const
+{
+}
+
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
+// End:
