a19e6fd14c18eb4c8d908f5c75aae13b595964e5
Daniel Grayson <dan@math.uiuc.edu>
2006-05-10 07:11:54 +0000

    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@3684 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/M2inits.h
+++ M2/Macaulay2/d/M2inits.h
@@ -6,10 +6,6 @@ extern void M2inits(), M2inits1(), M2inits2();
 extern void enterFactory(), enterM2();
 extern int M2inits_run;
 
-  /*extern void *	(*__gmp_allocate_func) (unsigned);*/
-  /*extern void *	(*__gmp_reallocate_func) (void *, unsigned, unsigned);*/
-  /*extern void	(*__gmp_free_func) (void *, unsigned);*/
-
 extern void *	(*__gmp_allocate_func) (size_t);
 extern void *	(*__gmp_reallocate_func) (void *, size_t, size_t);
 extern void	(*__gmp_free_func) (void *, size_t);
