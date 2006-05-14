affdf1aecb5812edbac461defad49b6f694d7792
Daniel Grayson <dan@math.uiuc.edu>
2006-05-14 18:02:22 +0000

    *** empty log message ***
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@3716 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/gc_test.c
+++ M2/Macaulay2/d/gc_test.c
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 
 static int had_error = 0;
+static char *msg;
 
 /* we test gc to see whether it properly marks pointers found in registers */
 static void uniq(void *p, ...) {
@@ -14,20 +15,15 @@ static void uniq(void *p, ...) {
   va_start(a,p);
   for (;(q[n] = va_arg(a,void *));n++) ;
   va_end(a);
-  for (i=0; i<n; i++) for (j=0; j<i; j++) if (q[i] == q[j]) {
-    fprintf(stderr,
-	    "%s:%d: error: gc library doesn't find all the active pointers!\n"
-	    "           Perhaps GC_push_regs was configured incorrectly.\n",
-	    __FILE__, __LINE__
-	    );
-    had_error = 1;
-  }
+  for (i=0; i<n; i++) for (j=0; j<i; j++) if (q[i] == q[j]) fprintf(stderr,msg, __FILE__, __LINE__ ), had_error = 1;
 }
 
 static void *x[100];
 
 int main() {
   int i=0;
+  msg = "%s:%d: error: gc library doesn't find all the active pointers in registers or on the stack!\n"
+        "           Perhaps GC_push_regs was configured incorrectly.\n";
   uniq(
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
@@ -35,6 +31,8 @@ int main() {
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        (void *)0);
+  msg = "%s:%d: error: gc library doesn't find all the active pointers in static memory!\n"
+        "           Perhaps GC_add_roots needs to be told about static memory.\n";
   uniq(
        x[i++] = GC_malloc(12), x[i++] = GC_malloc(12), x[i++] = GC_malloc(12), (GC_gcollect(),x[i++] = GC_malloc(12)),
        x[i++] = GC_malloc(12), x[i++] = GC_malloc(12), x[i++] = GC_malloc(12), (GC_gcollect(),x[i++] = GC_malloc(12)),
