b37e8ff59a2716f4dfbe35481173702ac8b3286e
Daniel Grayson <dan@math.uiuc.edu>
2006-05-14 17:50:43 +0000

    add a test for whether the gc garbage collector scans pointers found in static memory
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@3714 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/gc_test.c
+++ Macaulay2/d/gc_test.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+static int had_error = 0;
+
 /* we test gc to see whether it properly marks pointers found in registers */
-static void uniq(void *p, ...) {
+static void uniq1(void *p, ...) {
   va_list a;
   void *q[100];
   int n = 0, i, j;
@@ -18,24 +20,45 @@ static void uniq(void *p, ...) {
 	    "           Perhaps GC_push_regs was configured incorrectly.\n",
 	    __FILE__, __LINE__
 	    );
-    exit(1);
+    had_error = 1;
   }
 }
 
-static void test_gc (void) {
-  uniq(
+/* we test gc to see whether it properly marks pointers found in static memory */
+static void uniq2(void *p, ...) {
+  va_list a;
+  static void *q[100];
+  int n = 0, i, j;
+  q[n++] = p;
+  va_start(a,p);
+  for (;(q[n] = va_arg(a,void *));n++) ;
+  va_end(a);
+  for (i=0; i<n; i++) for (j=0; j<i; j++) if (q[i] == q[j]) {
+    fprintf(stderr,
+	    "%s:%d: error: gc library doesn't find all the active pointers!\n"
+	    "           Perhaps GC_add_roots needs to be told about static memory areas.\n",
+	    __FILE__, __LINE__
+	    );
+    had_error = 1;
+  }
+}
+
+int main() {
+  uniq1(
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
        (void *)0);
-}
-
-
-int main() {
-  test_gc();
-  return 0;
+  uniq2(
+       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
+       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
+       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
+       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
+       GC_malloc(12), GC_malloc(12), GC_malloc(12), (GC_gcollect(),GC_malloc(12)),
+       (void *)0);
+  return had_error;
 }
 
 /*
