0a768d65c6ebc3e171582f51ece703e932bbc7ff
Daniel Grayson <dan@math.uiuc.edu>
2007-01-07 15:18:00 +0000

    array.hpp: improve bounds checking, use our_new_delete
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5055 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/array.hpp
+++ M2/Macaulay2/e/array.hpp
@@ -6,16 +6,25 @@
 
 template <class T> class array;
 
-const int init_array_size = 16;
-
 template<class T>
-class array
+class array : public our_new_delete
 {
   T    *entries;
+  static const int init_array_size = 16;
   int  len;
   int  max;
+  void expand(int newtop) {
+      int newlen = (len > 1 ? len : 1);
+      for (; newtop>=newlen; newlen *= 2);
+      T *tmp = newarray(T ,newlen);
+      engine_alloc(newlen * sizeof(T));
 
-  void expand(int newtop);
+      for (int j = 0; j<max; j++) tmp[j] = entries[j];
+      engine_dealloc(len * sizeof(T));
+      deletearray(entries);
+      entries = tmp;
+      len = newlen;
+      }
    
 public:
   array(int i_size = init_array_size) 
@@ -39,12 +48,16 @@ public:
 
   int  length() const { return max; }
 
-  void shrink(int newmax) { if (newmax < max) max = newmax; }
+  void shrink(int newmax) { 
+       assert( newmax >= 0 );
+       if (newmax < max) max = newmax;
+  }
 
   T * get_raw_array() { return entries; }
 
   T &operator[](int i)
     {
+      assert(i >= 0);
       if (i < max) return entries[i];
       if (i >= len) expand(i);
       max = i+1;
@@ -53,14 +66,24 @@ public:
 
   const T &operator[](int i) const
     {
+      assert(i >= 0);
       assert(i < max);
       return entries[i];
     }
 
   T &rawelem(int i)
-    { assert(i < max); return entries[i]; }
+    { 
+	 assert(i >= 0);
+	 assert(i < max);
+	 return entries[i]; 
+    }
+
   const T &rawelem(int i) const
-    { assert(i < max); return entries[i]; }
+    { 
+	 assert(i >= 0);
+	 assert(i < max); 
+	 return entries[i];
+    }
 
   void append(const T &t) 
     { 
@@ -83,24 +106,10 @@ public:
     }
 };
 
-template <class T> 
-void array<T>::expand(int newtop)
-{
-  int newlen = (len > 1 ? len : 1);
-  for (; newtop>=newlen; newlen *= 2);
-  T *tmp = newarray(T ,newlen);
-  engine_alloc(newlen * sizeof(T));
-
-  for (int j = 0; j<max; j++) tmp[j] = entries[j];
-  engine_dealloc(len * sizeof(T));
-  deletearray(entries);
-  entries = tmp;
-  len = newlen;
-}
-
 template <class T>
-class array_class {
+class array_class : public our_new_delete {
   T    *entries;
+  static const int init_array_size = 16;
   int  len;
   int  max;
   void expand(int newtop)
@@ -121,12 +130,16 @@ public:
 
   int  length() const { return max; }
 
-  void shrink(int newmax) { if (newmax < max) max = newmax; }
+  void shrink(int newmax) { 
+       assert( newmax >= 0 );
+       if (newmax < max) max = newmax;
+  }
 
   T * get_raw_array() { return entries; }
 
   T &operator[](int i)
     {
+      assert(i >= 0);
       if (i < max) return entries[i];
       if (i >= len) expand(i);
       max = i+1;
@@ -135,14 +148,24 @@ public:
 
   const T &operator[](int i) const
     {
+      assert(i >= 0);
       assert(i < max);
       return entries[i];
     }
 
   T &rawelem(int i)
-    { assert(i < max); return entries[i]; }
+    { 
+	 assert(i >= 0);
+	 assert(i < max); 
+	 return entries[i]; 
+    }
+
   const T &rawelem(int i) const
-    { assert(i < max); return entries[i]; }
+    { 
+	 assert(i >= 0);
+	 assert(i < max); 
+	 return entries[i];
+    }
 
   void append(const T &t) 
     { 
