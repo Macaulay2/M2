bbd2c2f11d9a7a95e48f050425f85a1ec7dafed3
Daniel Grayson <dan@math.uiuc.edu>
2007-01-10 18:46:38 +0000

    overflow tests
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5171 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/overflow-test.cpp
+++ M2/Macaulay2/e/overflow-test.cpp
@@ -1,13 +1,25 @@
+#include <stdio.h>
+#undef TRYLONGLONG
 #include "overflow.hpp"
 #include "assert.h"
 
-#define TRYLONGLONG
-#define BENCHMARK
+#define e8 10000000
+#define outer (e8)
+#define inner 300
+
+int s;
+volatile int x;
 
 int main () {
-     int i,j,k;
-     for (i=10000; i>0; i--) {
-	  for (j=0; j<10; j++)
-	       for (k=0; k<10; k++)
-     }
+     int i,j;
+     printf("%Ld repetitions\n",(long long)inner*outer);
+     // safe::add(0x7fffffff,0x7fffffff);
+     for (i=outer; i>0; i--)
+	  for (j=0; j<inner; j++)
+	       // x = j+s;
+	       x = safe::add(j,s);
 }
+
+// Local Variables:
+// compile-command: "cd $M2BUILDDIR/Macaulay2/e && make DEPENDS=no overflow-test-demangled.s overflow-test && time ./overflow-test"
+// End:
