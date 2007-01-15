321dd37d972f4572578fa2f721629f3af780cfd4
Daniel Grayson <dan@math.uiuc.edu>
2007-01-15 15:35:44 +0000

    pi update
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@5244 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/pi.cc
+++ Macaulay2/e/pi.cc
@@ -1,9 +1,9 @@
 #include "pi.h"
 
-template class pi<int32_t,7>;
-template class pi<int32_t,8>;
-template class piv<int32_t,7,10>;
+template class pui<int32_t,7>;
+template class pui<int32_t,8>;
+template class puiv<int32_t,7,10>;
 
 // Local Variables:
-// compile-command: "make -C $M2BUILDDIR/Macaulay2/e pi-demangled.s"
+// compuile-command: "make -C $M2BUILDDIR/Macaulay2/e pui-demangled.s"
 // End:
--- Macaulay2/e/pi.h
+++ Macaulay2/e/pi.h
@@ -1,6 +1,12 @@
 /* pi.cc -- routines for packed integers and monomials */
 // pi = packed integer
-// piv = packed integers
+// piv = packed integer vector
+// pui = packed unsigned integer
+// puiv = packed unsigned integer vector
+// pgi = packed integer, with one extra guard bit per field
+// pgiv = packed integer vector, with one extra guard bit per field
+// pgui = packed unsigned integer, with one extra guard bit per field
+// pguiv = packed unsigned integer vector, with one extra guard bit per field
 // bits = number of bits per subfield
 // T = type of int to pack them into
 #include "overflow.hpp"
@@ -12,8 +18,6 @@
 #define expect_true(x)  (x)
 #endif
 #include "pi-masks.h"
-#define numbits(T) (8 * sizeof(T))
-#define divides(a,b) ((b)%(a)==0)
 template <typename T> struct masks {
      static inline T lomask(int bits);
      static inline T himask(int bits);
@@ -22,39 +26,55 @@ template <> inline int32_t masks<int32_t>::lomask(int bits) { return lomask32[bi
 template <> inline int32_t masks<int32_t>::himask(int bits) { return himask32[bits]; }
 template <> inline int64_t masks<int64_t>::lomask(int bits) { return lomask64[bits]; }
 template <> inline int64_t masks<int64_t>::himask(int bits) { return himask64[bits]; }
-template <typename T, int bits> class pi { 
-     static inline T lomask() { return masks<T>::lomask(bits); }
-     static inline T himask() { return masks<T>::himask(bits); }
-     static T inline add(T x, T y, T &carries) {
+template <typename T, int bits> class pui {
+     static T bit_add(T x, T y) { return x^y; }
+     static T bit_sub(T x, T y) { return x^y; }
+     static T bit_eq(T x, T y) { return ~(x^y); }
+     static T bit_neq(T x, T y) { return x^y; }
+     static T bit_imp(T x, T y) { return (~x) | y; }
+     static T bit_nimp(T x, T y) { return x & ~y; }
+     static int numbits() { return 8 * sizeof(T); }
+     static int numfields() { return numbits() / bits; }
+     static int extrabits() { return numbits() - bits * numfields(); }
+     static T lomask() { return masks<T>::lomask(bits); }
+     static T himask() { return masks<T>::himask(bits); }
+     static T add(T x, T y, T &oflows) {
 	  T sum = x + y;
-	  T diffs = x ^ y;
-	  carries |= diffs ^ sum;
-	  if expect_false (divides(bits,numbits(T)) && diffs > 0 && (sum^x)<0) safe::ov("overflow: pi + pi");
+	  oflows |= bit_nimp(sum, bit_neq(x,y)); /* fix... */
 	  return sum;
      }
-     static void inline add_cleanup(T &carries) {
-	  if expect_false (0 != (carries & lomask())) safe::ov("overflow: pi + pi");
+     static void add_final(T &oflows) {
+	  if expect_false (0 != (oflows & himask())) safe::ov("overflow: pui - pui");
+     }
+     static T sub(T x, T y, T &oflows) {
+	  T dif = x - y;
+	  oflows |= bit_neq(bit_sub(x,y),dif);
+	  if expect_false (extrabits() == 0 && x<y) safe::ov("overflow: pui - pui");
+	  return dif;
+     }
+     static void sub_final(T &oflows) {
+	  if expect_false (0 != (oflows & lomask())) safe::ov("overflow: pui - pui");
      }
 public:
-     static T inline add(T x, T y) {
-	  T carries = 0;
-	  T res = add(x,y,carries);
-	  add_cleanup(carries);
+     static T sub(T x, T y) {
+	  T oflows = 0;
+	  T res = sub(x,y,oflows);
+	  sub_final(oflows);
 	  return res;
      }
-     static void inline add(int len, T res[], T x[], T y[]) {
-	  T carries = 0;
-	  for (int j=0; j<len; j++) res[j] = add(x[j], y[j], carries);
-	  add_cleanup(carries);
+     static void sub(int len, T res[], T x[], T y[]) {
+	  T oflows = 0;
+	  for (int j=0; j<len; j++) res[j] = sub(x[j], y[j], oflows);
+	  sub_final(oflows);
      }
 };
-template <typename T, int bits, int len> class piv {
-     static void inline add(T res[], T x[], T y[]) {
-	  pi<T,bits>::add(len,res,x,y);
+template <typename T, int bits, int len> class puiv {
+     static void sub(T res[], T x[], T y[]) {
+	  pui<T,bits>::sub(len,res,x,y);
      }
 };     
 
 
 // Local Variables:
-// compile-command: "make -C $M2BUILDDIR/Macaulay2/e pi-demangled.s"
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e pui-demangled.s"
 // End:
