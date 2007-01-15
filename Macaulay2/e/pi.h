16b85829cf76566b4268bd346722ad9687ed6618
Daniel Grayson <dan@math.uiuc.edu>
2007-01-15 04:10:42 +0000

    pi update
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@5242 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/pi.h
+++ Macaulay2/e/pi.h
@@ -12,40 +12,49 @@
 #define expect_true(x)  (x)
 #endif
 #include "pi-masks.h"
-
+#define numbits(T) (8 * sizeof(T))
+#define divides(a,b) ((b)%(a)==0)
 template <typename T> struct masks {
      static inline T lomask(int bits);
      static inline T himask(int bits);
 };
-
 template <> inline int32_t masks<int32_t>::lomask(int bits) { return lomask32[bits]; }
 template <> inline int32_t masks<int32_t>::himask(int bits) { return himask32[bits]; }
 template <> inline int64_t masks<int64_t>::lomask(int bits) { return lomask64[bits]; }
 template <> inline int64_t masks<int64_t>::himask(int bits) { return himask64[bits]; }
-
-template <typename T> int f();
-template <> int f<int>() { return 1; }
-
 template <typename T, int bits> class pi { 
-     T i;
+     static inline T lomask() { return masks<T>::lomask(bits); }
+     static inline T himask() { return masks<T>::himask(bits); }
+     static T inline add(T x, T y, T &carries) {
+	  T sum = x + y;
+	  T diffs = x ^ y;
+	  carries |= diffs ^ sum;
+	  if expect_false (divides(bits,numbits(T)) && diffs > 0 && (sum^x)<0) safe::ov("overflow: pi + pi");
+	  return sum;
+     }
+     static void inline add_cleanup(T &carries) {
+	  if expect_false (0 != (carries & lomask())) safe::ov("overflow: pi + pi");
+     }
 public:
-     pi(T i0) : i(i0) {}
-     operator T() { return i; }
-     pi operator+(pi &y) {
-	  T s = i + y.i;
-	  if expect_false (0 != (s & (masks<T>::himask(bits)))) safe::ov("overflow: pi + pi");
-	  return s; }
-     pi operator+=(pi &y) {
-	  T s = i += y.i;
-	  if expect_false (0 != (s & (masks<T>::himask(bits)))) safe::ov("overflow: pi += pi");
-	  return s; }
+     static T inline add(T x, T y) {
+	  T carries = 0;
+	  T res = add(x,y,carries);
+	  add_cleanup(carries);
+	  return res;
+     }
+     static void inline add(int len, T res[], T x[], T y[]) {
+	  T carries = 0;
+	  for (int j=0; j<len; j++) res[j] = add(x[j], y[j], carries);
+	  add_cleanup(carries);
+     }
 };
-template <typename T,int bits,int len> class piv { 
-     T i[len];
-public:
-     piv &operator += (piv &y) {
-	  T o=0;
-	  for (int j=0; j<len; j++) o |= i[j] += y.i[j];
-	  if expect_false(0 != (o & (masks<T>::himask(bits)))) safe::ov("overflow: piv += piv");
-	  return *this; }
-};
+template <typename T, int bits, int len> class piv {
+     static void inline add(T res[], T x[], T y[]) {
+	  pi<T,bits>::add(len,res,x,y);
+     }
+};     
+
+
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e pi-demangled.s"
+// End:
