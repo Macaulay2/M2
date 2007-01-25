5c802e73e052324b9c5d93b4fd13841529b0a773
Daniel Grayson <dan@math.uiuc.edu>
2007-01-25 15:51:07 +0000

    packed int monomial development
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5268 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/pi.cc
+++ M2/Macaulay2/e/pi.cc
@@ -1,9 +1,9 @@
 #include "pi.h"
 
-template class pui<int32_t,7>;
-template class pui<int32_t,8>;
-template class puiv<int32_t,7,10>;
+template class pui<int32_t,int,7>;
+template class pui<int32_t,int,8>;
+template class puiv<int32_t,int,7,10>;
 
 // Local Variables:
-// compile-command: "make -C $M2BUILDDIR/Macaulay2/e pui-demangled.s"
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e pi-demangled.s"
 // End:
