fb47e2ab51bc45cb4485c8e4788c347184a728e5
Daniel Grayson <dan@math.uiuc.edu>
2007-01-25 16:59:29 +0000

    packed monomial stuff
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@5270 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/pi.h
+++ Macaulay2/e/pi.h
@@ -11,10 +11,10 @@
 #define expect_true(x)  (x)
 #endif
 #include "pi-masks.h"
-template <typename T> struct masks { static inline T lomask(int nbits), himask(int nbits); };
-template <> inline int32_t masks<int32_t>::lomask(int nbits) { return lomask32[nbits]; }
+template <typename T> struct masks { static inline T ovmask(int nbits), himask(int nbits); };
+template <> inline int32_t masks<int32_t>::ovmask(int nbits) { return ovmask32[nbits]; }
 template <> inline int32_t masks<int32_t>::himask(int nbits) { return himask32[nbits]; }
-template <> inline int64_t masks<int64_t>::lomask(int nbits) { return lomask64[nbits]; }
+template <> inline int64_t masks<int64_t>::ovmask(int nbits) { return ovmask64[nbits]; }
 template <> inline int64_t masks<int64_t>::himask(int nbits) { return himask64[nbits]; }
 template <typename T> static T bool_add(T x, T y) { return x^y; }
 template <typename T> static T bool_sub(T x, T y) { return x^y; }
@@ -23,13 +23,13 @@ template <typename T> static T bool_neq(T x, T y) { return x^y; }
 template <typename T> static T bool_imp(T x, T y) { return (~x) | y; }
 template <typename T> static T bool_nimp(T x, T y) { return x & ~y; }
 template <typename T, typename U, int nbits> class pui { // U is an unsigned integer type holding at least nbits; we pack them into the integer type T
-     static int numbits() { return 8 * sizeof(T); }
-     static int extrabits() { return numbits() - nbits * numfields(); }
-     static T lomask() { return masks<T>::lomask(nbits); }
+     static int bits_per_bin() { return 8 * sizeof(T); }
+     static int extrabits() { return bits_per_bin() - nbits * flds_per_bin(); }	// to determine whether the overflow mask includes a bit for the highest field
+     static T ovmask() { return masks<T>::ovmask(nbits); }
      static T himask() { return masks<T>::himask(nbits); }
      static U field_mask() { return ((U)1 << nbits) - 1; }
-     static U field       (T t, int i) { return (U)(t >> (nbits * (numfields() - 1 - i))) & field_mask(); }
-     static U field_at_bit(T t, int i) { return (U)(t >> i                              ) & field_mask(); }
+     static U field       (T t, int i) { return (U)(t >> (nbits * (flds_per_bin() - 1 - i))) & field_mask(); }
+     static U field_at_bit(T t, int i) { return (U)(t >> i                                 ) & field_mask(); }
      static U checkfit(U u) { if (0 != (u & field_mask())) safe::ov("overflow: pui"); return u ; }
      static T add(T x, T y, T &carries) {
 	  T sum = x + y;
@@ -37,7 +37,7 @@ template <typename T, typename U, int nbits> class pui { // U is an unsigned int
 	  if expect_false (extrabits() == 0 && sum<x) safe::ov("overflow: pui + pui");
 	  return sum; }
      static void add_final(T &carries) {
-	  T oflows = carries & lomask();
+	  T oflows = carries & ovmask();
 	  if expect_false (0 != oflows) safe::ov("overflow: pui + pui"); }
      static T sub(T x, T y, T &borrows) {
 	  T dif = x - y;
@@ -45,15 +45,15 @@ template <typename T, typename U, int nbits> class pui { // U is an unsigned int
 	  if expect_false (extrabits() == 0 && x<y) safe::ov("overflow: pui - pui");
 	  return dif; }
      static void sub_final(T &borrows) {
-	  T oflows = borrows & lomask();
+	  T oflows = borrows & ovmask();
 	  if expect_false (0 != oflows) safe::ov("overflow: pui - pui"); }
 public:
-     static int numfields() { return numbits() / nbits; }
+     static int flds_per_bin() { return bits_per_bin() / nbits; }
      static void pack(T *dest, U *src, int numfields) {
 	  if (numfields == 0) return;
 	  while (1) {
 	       T t = 0;
-	       for (int j = (numfields()-1)*nbits; j >= 0; j -= nbits) {
+	       for (int j = (flds_per_bin()-1)*nbits; j >= 0; j -= nbits) {
 		    t |= checkfit(*src++) << j;
 		    if expect_false (--numfields == 0) {
 			 *dest++ = t;
@@ -67,7 +67,7 @@ public:
 	  if (numfields == 0) return;
 	  while (1) {
 	       T t = *src++;
-	       for (int bit = (numfields()-1)*nbits; bit >= 0; bit -= nbits) {
+	       for (int bit = (flds_per_bin()-1)*nbits; bit >= 0; bit -= nbits) {
 		    *dest++ = field_at_bit(t,bit);
 		    if expect_false (--numfields == 0) return;
 	       }
@@ -78,7 +78,7 @@ public:
 	  while (1) {
 	       T a = *x++;
 	       T b = *y++;
-	       for (int j = (numfields()-1)*nbits; j >= 0; j -= nbits) {
+	       for (int j = (flds_per_bin()-1)*nbits; j >= 0; j -= nbits) {
 		    int r = f(field_at_bit(a,j), field_at_bit(b,j));
 		    if expect_false (r != 0) return r;
 		    if expect_false (--numfields == 0) return 0;
@@ -106,7 +106,7 @@ public:
 	  if expect_true (extrabits() == 0 && x<y) return false;
 	  T dif = x - y;
 	  T borrows = bool_neq(bool_sub(x,y),dif);
-	  T oflows = borrows & lomask();
+	  T oflows = borrows & ovmask();
 	  if expect_true (0 != oflows) return false;
 	  return true;
      }
