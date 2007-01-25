5a54840f97e0ddf49d75cf43046f255412268b5d
Daniel Grayson <dan@math.uiuc.edu>
2007-01-25 16:45:01 +0000

    packed int monomials
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5269 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/pi.cc
+++ M2/Macaulay2/e/pi.cc
@@ -1,5 +1,7 @@
 #include "pi.h"
 
+int64_t (*g)(int64_t *, int64_t *, int) = pui<int64_t,int,7>::cmp_lex;
+
 template class pui<int32_t,int,7>;
 template class pui<int32_t,int,8>;
 template class puiv<int32_t,int,7,10>;
