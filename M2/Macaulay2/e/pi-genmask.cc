dc21f1daf6557b58f1231e0754fc81b8cd7ea62e
Daniel Grayson <dan@math.uiuc.edu>
2007-01-25 18:34:16 +0000

    packed monomial stuff
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5273 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/pi-genmask.cc
+++ M2/Macaulay2/e/pi-genmask.cc
@@ -1,20 +1,13 @@
 #include "integertypes.h"
 #include <stdio.h>
-template <typename T> T ovmask(int bits) { if (bits==0) return 0; T n=1, r=0; n<<=bits  ; while(n&~r) {r|=n; n<<=bits;} return r; }
 template <typename T> T himask(int bits) { if (bits==0) return 0; T n=1, r=0; n<<=bits-1; while(n&~r) {r|=n; n<<=bits;} return r; }
 
 int main() {
 
-     printf("const static uint32_t ovmask32[] = {\n");
-     for (int bits=0; bits<=32; bits++) printf("  0x%08x,\n",ovmask<uint32_t>(bits));
-     printf("};\n");
      printf("const static uint32_t himask32[] = {\n");
      for (int bits=0; bits<=32; bits++) printf("  0x%08x,\n",himask<uint32_t>(bits));
      printf("};\n");
 
-     printf("const static uint64_t ovmask64[] = {\n");
-     for (int bits=0; bits<=64; bits++) printf("  0x%016LxLL,\n",ovmask<uint64_t>(bits));
-     printf("};\n");
      printf("const static uint64_t himask64[] = {\n");
      for (int bits=0; bits<=64; bits++) printf("  0x%016LxLL,\n",himask<uint64_t>(bits));
      printf("};\n");
