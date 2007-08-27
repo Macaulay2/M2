1402a6dde0788655e7e84efa2875af6fdb03d977
Daniel Grayson <dan@math.uiuc.edu>
2007-08-27 20:00:29 +0000

    working on link order for constructors, maybe ready
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5827 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/factory_init2.cc
+++ M2/Macaulay2/d/factory_init2.cc
@@ -1,25 +1,16 @@
 #include "M2inits.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 void factory_init2(){}
-
 int factory_init2_run = 0;
-
-static void f() __attribute__ ((constructor));
-static void f() { 
-  if (!factory_init1_run) {
-    fprintf(stderr, "internal error: factory_init1 not initialized before factory_init2\n");
-    exit(1);
-  }
-  enterM2();
-  factory_init2_run = 1;
-}
-
-static void g() __attribute__ ((destructor));
-static void g() { enterFactory(); }
-
 static struct C {
-      C () { enterM2(); }
+     C () { 
+	  if (!factory_init1_run) {
+	       fprintf(stderr, "internal error: factory_init1 not initialized before factory_init2\n");
+	       exit(1);
+	  }
+	  enterM2();
+	  factory_init2_run = 1;
+     }
      ~C () { enterFactory(); }
 } x;
