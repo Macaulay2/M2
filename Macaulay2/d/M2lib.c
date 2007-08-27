efc4090eaa281c3ceb57e78f53d3de6e2b0d3287
Daniel Grayson <dan@math.uiuc.edu>
2007-08-27 20:54:24 +0000

    do not stop on interrupt, even for non-tty
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@5829 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/M2lib.c
+++ Macaulay2/d/M2lib.c
@@ -244,9 +244,9 @@ static void interrupt_handler(int sig)
 	  if (system_interruptShield) system_interruptPending = TRUE;
 	  else {
 	       extern void evaluate_setInterruptFlag();
-	       if (tokens_stopIfError || !isatty(STDIN)) {
+	       if (tokens_stopIfError) {
 		    int interruptExit = 2;	/* see also interp.d */
-		    fprintf(stderr,"interrupted, stopping%s",NEWLINE);
+		    fprintf(stderr,"interrupted, stopping\n");
 		    exit(interruptExit);
 	       }
 	       evaluate_setInterruptFlag();
