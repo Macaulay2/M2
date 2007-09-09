c6e5f4e692a3340e155df9f69f2aedd97aba786d
Mike Stillman <mike@math.cornell.edu>
2007-09-09 23:50:59 +0000

    working on generic Groebner walk implementation
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5882 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/gb-walk.cpp
+++ M2/Macaulay2/e/gb-walk.cpp
@@ -3,29 +3,125 @@
 #include "gb-walk.hpp"
 #include "matrix.hpp"
 #include "monordering.h"
+#include "comp-gb-declared.hpp"
+
+class MonomialOrderMatrix
+{
+  int nvars;
+  long **order;
+  
+public:
+  MonomialOrderMatrix(const MonomialOrdering *mo);
+  ~MonomialOrderMatrix();
+  
+  const long *part(int i) const { assert(i < nvars); return order[i]; }
+  int compare(long *m1, long *m2) const;
+
+  int minpart(long *m) const;
+  // returns the smallest integer i such that part(j) . m = 0 for j < i
+
+  long value(int i, long *monom) const;
+  // return part(i) . monom
+
+  MonomialOrdering *toMonomialOrdering() const;// TODO
+
+// Routines needed: 
+  int facet_compare(long *mon1, long *mon2); // TODO
+  
+};
+
+
+////////////////////////////////
+
+GBWalker::GBWalker(MarkedGB *G0,
+		   long **order1,
+		   long **order2)
+  :  R(G0->get_gb_ring()),
+     F(G0->get_ambient_FreeModule()),
+     G(G0),
+     monorder1(order1), // or create this from the monomial order?
+     monorder2(order2),
+     ww(0)
+{
+
+  // TODO: need to set what else?
+  // 
+}
+
 
 GBWalker::GBWalker(const Matrix *gb_under_order1,
-			    const MonomialOrdering *order1)
+		   const MonomialOrdering *order1)
 {
 }
 
 GBWalker * GBWalker::create(const Matrix *gb_under_order1,
 			    const MonomialOrdering *order1)
 {
-  // MES: TO WRITE
+  // TODO MES: TO WRITE
   return new GBWalker(gb_under_order1, order1);
 }
 
 GBWalker::~GBWalker()
 {
-  // MES: TO WRITE
+  // TODO MES: TO WRITE
 }
 
 bool GBWalker::stop_conditions_ok()
 {
-  // MES: TO WRITE
+  // TODO MES: TO WRITE
   return true;
 }
+
+GBComputation * GBWalker::make_gb(const Matrix *M) const
+  // return the GB of g, keep = 0 or 1.
+{
+  M2_arrayint weights = makearrayint(R->n_vars());
+  for (int i=0; i<R->n_vars(); i++) weights->array[i] = 1;
+
+  GBComputation *G0 = GBComputation::choose_gb(M,
+					      false, // collect syz
+					      -1,
+					      weights,
+					      false,
+					      -1,
+					      0,
+					      0
+					      /* , max_reduction_count */
+					      );
+  G0->set_stop_conditions(false,
+			 NULL,
+			 -1,
+			 -1, // syzygy limit
+			 -1,
+			 -1,
+			 -1,
+			 false,
+			 NULL);
+  return G0;
+}
+
+void GBWalker::initialize()
+{
+  // Initialize the local variables of the computation
+  state = STATE_compute_w;
+  ww = 0;
+  inwwG = 0;
+  gb_inwwG = 0;
+  next_to_reduce = 0;
+  // G is already set
+  G1 = 0;
+}
+
+bool GBWalker::compute_next_w()
+// Uses the value of w in the class, and looks at every term of every poly
+// in the marked GB which is > the marked term (in order2) trying to find
+// the next w to use.
+// Returns: true if a w is found
+{
+  // TODO MES: TO WRITE
+  return true;
+}
+
 
 //////////////////////////////////////////////////////
 // GBComputation and Computation inherited routines //
@@ -35,9 +131,51 @@ void GBWalker::remove_gb()
   // MES: TO WRITE
 }
 
+
 void GBWalker::start_computation()
 {
-  // MES: TO WRITE
+  if (stop_.always_stop) return; // don't change status
+
+  for (;;)
+    switch (state) {
+    case STATE_compute_w:
+      if (!compute_next_w())
+	{
+	  // We are done!
+	  state = STATE_done;
+	  set_status(COMP_DONE);
+	  return;
+	}
+      inwwG = G->get_parallel_lead_terms(ww);
+      gb_inwwG = make_gb(inwwG);
+      state = STATE_do_gb;
+    case STATE_do_gb:
+      // Now compute the GB object.  If not interrupted, go on:
+      gb_inwwG->start_computation();
+      if (gb_inwwG->status() == COMP_INTERRUPTED)
+	{
+	  set_status(COMP_INTERRUPTED);
+	  return;
+	}
+      next_to_reduce = 0;
+      state = STATE_reduce;
+    case STATE_reduce:
+      while (next_to_reduce < 0) // TODO: consider the top of the loop
+	{
+	  H =  G->matrix_remainder(gb_inwwG->get_gb()); // Not quite: need to subtract...
+	  next_to_reduce++;
+	}
+      state = STATE_autoreduce;
+    case STATE_autoreduce:
+      G->remove_gb();
+      delete G;
+      G1 = static_cast<MarkedGB *>(GBDeclared::create(gb_inwwG->get_initial(-1), H, H, 0, 0));
+      state = STATE_compute_w;
+    case STATE_done:
+      set_status(COMP_DONE);
+      return;
+    }
+
 }
 
 const PolynomialRing *GBWalker::get_ring() 
--- M2/Macaulay2/e/lapack.hpp
+++ M2/Macaulay2/e/lapack.hpp
@@ -6,269 +6,269 @@
 /* Lapack routines */
 /* Compute solutions x to Ax = b for square matrix A and a matrix b */
 extern "C" {
-void dgesv_(int *n,    // number of rows in A
-		       int *nrhs, // number of right hand sides
-		       double *a, // n by n matrix A, on exit L&U from A=PLU
-		       int *lda,  // n
-		       int *ipiv, // indices defining permutation P
-		       double *b, // right-hand-side, on exit the solution matrix
-		       int *ldb,  // n
-		       int *info);// error info
+int dgesv_(int *n,    // number of rows in A
+	   int *nrhs, // number of right hand sides
+	   double *a, // n by n matrix A, on exit L&U from A=PLU
+	   int *lda,  // n
+	   int *ipiv, // indices defining permutation P
+	   double *b, // right-hand-side, on exit the solution matrix
+	   int *ldb,  // n
+	   int *info);// error info
 
-void dgeev_(char *n,    // whether to compute left eigenvectors
-		       char *n2,   // whether to compute right eigenvectors
-		       int *size,  // rows
-		       double *M,  // input matrix A
-		       int *size1, // rows
-		       double *E,  // real components of eigenvalues
-		       double *E2, // imaginar components of eigenvalues
-		       double *,   // left eigenvectors
-		       int *,      // rows
-		       double *,   // right eigenvectors
-		       int *,      // rows
-		       double *,   // workspace
-		       int *,      // size of workspace
-		       int *);     // error info
+int dgeev_(char *n,    // whether to compute left eigenvectors
+	   char *n2,   // whether to compute right eigenvectors
+	   int *size,  // rows
+	   double *M,  // input matrix A
+	   int *size1, // rows
+	   double *E,  // real components of eigenvalues
+	   double *E2, // imaginar components of eigenvalues
+	   double *,   // left eigenvectors
+	   int *,      // rows
+	   double *,   // right eigenvectors
+	   int *,      // rows
+	   double *,   // workspace
+	   int *,      // size of workspace
+	   int *);     // error info
+  
+int dsyev_(char *n,      // whether to compute eigenvectors
+	   char *n2,     // how to store A (upper or lower)
+	   int *size,    // rows
+	   double *M,    // symmetric matrix A
+	   int *lda,     // rows
+	   double *eig,  // becomes eigenvalues
+	   double *work, // workspace
+	   int *wsize,   // size of workspace
+	   int *info);   // error info
+  
+int dgetrf_(int *rows, // rows 
+	    int *cols, // columns
+	    double *A, // input matrix, on exit L & U from A=PLU.
+	    int *ld,   // rows
+	    int *ipiv, // becomes permutation indices of P
+	    int *info);// error info
 
-void dsyev_(char *n,      // whether to compute eigenvectors
-		       char *n2,     // how to store A (upper or lower)
-		       int *size,    // rows
-		       double *M,    // symmetric matrix A
-		       int *lda,     // rows
-		       double *eig,  // becomes eigenvalues
-		       double *work, // workspace
-		       int *wsize,   // size of workspace
-		       int *info);   // error info
-		       
-void dgetrf_(int *rows, // rows 
-			int *cols, // columns
-			double *A, // input matrix, on exit L & U from A=PLU.
-			int *ld,   // rows
-			int *ipiv, // becomes permutation indices of P
-			int *info);// error info
+int dgesvd_(char* jobU,    // amount of U to return
+	    char* jobV,    // amount of V to return
+	    int* rows,     // rows
+	    int* cols,     // columns
+	    double *A,     // input matrix for SVD
+	    int *ldA,      // rows
+	    double *Sigma, // singular values
+	    double *U,     // U
+	    int *ldU,      // rows
+	    double *VT,    // V transpose
+	    int *ldVT,     // cols
+	    double *work,  // workspace
+	    int *lwork,    // size of workspace
+	    int *info);    // error info
 
-void dgesvd_(char* jobU,    // amount of U to return
-			char* jobV,    // amount of V to return
-			int* rows,     // rows
-			int* cols,     // columns
-			double *A,     // input matrix for SVD
-			int *ldA,      // rows
-			double *Sigma, // singular values
-			double *U,     // U
-			int *ldU,      // rows
-			double *VT,    // V transpose
-			int *ldVT,     // cols
-			double *work,  // workspace
-			int *lwork,    // size of workspace
-			int *info);    // error info
-
-void dgesdd_(char* jobU,    // amount of U to return
-			int* rows,     // rows
-			int* cols,     // columns
-			double *A,     // input matrix for SVD
-			int *ldA,      // rows
-			double *Sigma, // singular values
-			double *U,     // U
-			int *ldU,      // rows
-			double *VT,    // V transpose
-			int *ldVT,     // cols
-			double *work,  // workspace
-			int *lwork,    // size of workspace
-			int *iwork,    // integer workspace
-			int *info);    // error info
-
-void dgels_(char* job,     // type of least squares problem
-		       int* rows,     // rows
-		       int* cols,     // columns
-		       int* nhrs,     // number right hand sides
-		       double *A,     // input matrix for least squares
-		       int *ldA,      // rows
-		       double *b,     // matrix of right hand side vectors
-		       int *ldb,      // rows of right hand side
-		       double *work,  // workspace
-		       int *lwork,    // size of workspace
-		       int *info);    // error info
-
-void dgelss_(int* rows,     // rows
-			int* cols,     // columns
-			int* nhrs,     // number right hand sides
-			double *A,     // input matrix for least squares
-			int *ldA,      // rows
-			double *b,     // matrix of right hand side vectors
-			int *ldb,      // rows of right hand side
-			double *Sigma, // singular values
-			double *rcond, // used to determine if singular value is 0
-			int *rank,     // rank of the matrix on output
-			double *work,  // workspace
-			int *lwork,    // size of workspace
-			int *info);    // error info
+int dgesdd_(char* jobU,    // amount of U to return
+	    int* rows,     // rows
+	    int* cols,     // columns
+	    double *A,     // input matrix for SVD
+	    int *ldA,      // rows
+	    double *Sigma, // singular values
+	    double *U,     // U
+	    int *ldU,      // rows
+	    double *VT,    // V transpose
+	    int *ldVT,     // cols
+	    double *work,  // workspace
+	    int *lwork,    // size of workspace
+	    int *iwork,    // integer workspace
+	    int *info);    // error info
+  
+int dgels_(char* job,     // type of least squares problem
+	   int* rows,     // rows
+	   int* cols,     // columns
+	   int* nhrs,     // number right hand sides
+	   double *A,     // input matrix for least squares
+	   int *ldA,      // rows
+	   double *b,     // matrix of right hand side vectors
+	   int *ldb,      // rows of right hand side
+	   double *work,  // workspace
+	   int *lwork,    // size of workspace
+	   int *info);    // error info
+  
+int dgelss_(int* rows,     // rows
+	    int* cols,     // columns
+	    int* nhrs,     // number right hand sides
+	    double *A,     // input matrix for least squares
+	    int *ldA,      // rows
+	    double *b,     // matrix of right hand side vectors
+	    int *ldb,      // rows of right hand side
+	    double *Sigma, // singular values
+	    double *rcond, // used to determine if singular value is 0
+	    int *rank,     // rank of the matrix on output
+	    double *work,  // workspace
+	    int *lwork,    // size of workspace
+	    int *info);    // error info
 
 /* cblas routines */
 // computes "ax + y"
 void cblas_daxpy(const int n,     // length of vectors
-			    const double a,  // scalar alpha
-			    const double* x, // vector x
-			    const int incx,  // increment of x
-			    double* y,       // vector y
-			    const int incy); // increment of y
+		 const double a,  // scalar alpha
+		 const double* x, // vector x
+		 const int incx,  // increment of x
+		 double* y,       // vector y
+		 const int incy); // increment of y
 
 // computes ax
 void cblas_dscal(const int n,     // length of vectors
-			    const double a,  // scalar alpha
-			    const double* x, // vector x
-			    const int incx); // increment of x
-
+		 const double a,  // scalar alpha
+		 const double* x, // vector x
+		 const int incx); // increment of x
+  
 // computes "alpha AB + beta C"
 // NOTE: first 3 args should formally be ENUMS, not ints.
 //       Problem? e.g., what if enums change?
 void cblas_dgemm(const int Order,     // how matrices are stored, by column or row.
-			    const int TransA,    // whether to transform A, e.g. take transpose
-			    const int TransB,    // whether to transform B
-			    const int M,         // rows of A
-			    const int N,         // columns of B
-			    const int K,         // columns of A, which must = rows of B
-			    const double alpha,  // scalar alpha
-			    const double *A,     // matrix A
-			    const int lda,       // rows of A
-			    const double *B,     // matrix B
-			    const int ldb,       // rows of B
-			    const double beta,   // scalar bet
-			    double *C,           // matrix C; on output, alphaAB+betaC
-			    const int ldc);      // rows of C
+		 const int TransA,    // whether to transform A, e.g. take transpose
+		 const int TransB,    // whether to transform B
+		 const int M,         // rows of A
+		 const int N,         // columns of B
+		 const int K,         // columns of A, which must = rows of B
+		 const double alpha,  // scalar alpha
+		 const double *A,     // matrix A
+		 const int lda,       // rows of A
+		 const double *B,     // matrix B
+		 const int ldb,       // rows of B
+		 const double beta,   // scalar bet
+		 double *C,           // matrix C; on output, alphaAB+betaC
+		 const int ldc);      // rows of C
+  
+int zgesv_(int *n,    // number of rows in A
+	   int *nrhs, // number of right hand sides
+	   double *a, // n by n matrix A, on exit L&U from A=PLU
+	   int *lda,  // n
+	   int *ipiv, // indices defining permutation P
+	   double *b, // right-hand-side
+	   int *ldb,  // n
+	   int *info);// error info
 
-void zgesv_(int *n,    // number of rows in A
-		       int *nrhs, // number of right hand sides
-		       double *a, // n by n matrix A, on exit L&U from A=PLU
-		       int *lda,  // n
-		       int *ipiv, // indices defining permutation P
-		       double *b, // right-hand-side
-		       int *ldb,  // n
-		       int *info);// error info
+int zgeev_(char *n,        // whether to compute left eigenvectors
+	   char *n2,       // whether to compute right eigenvectors
+	   int *size,      // rows
+	   double *M,  // n by n input matrix
+	   int *size1,     // rows
+	   double *E,  // eigenvalues, on output
+	   double *l,  // left eigenvectors, on output
+	   int *lsize,     // rows
+	   double *r,  // right eigenvectors, on output
+	   int *rsize,     // rows
+	   double *w,  // workspace
+	   int *wsize,     // size of workspace
+	   double *rwork,  // another workspace
+	   int *info);     // error info
 
-void zgeev_(char *n,        // whether to compute left eigenvectors
-		       char *n2,       // whether to compute right eigenvectors
-		       int *size,      // rows
-		       double *M,  // n by n input matrix
-		       int *size1,     // rows
-		       double *E,  // eigenvalues, on output
-		       double *l,  // left eigenvectors, on output
-		       int *lsize,     // rows
-		       double *r,  // right eigenvectors, on output
-		       int *rsize,     // rows
-		       double *w,  // workspace
-		       int *wsize,     // size of workspace
-		       double *rwork,  // another workspace
-		       int *info);     // error info
+int zheev_(char *n,       // whether to compute eigenvectors
+	   char *n2,      // how to store A (upper or lower)
+	   int *size,     // rows
+	   double *M, // hermitian matrix A
+	   int *lda,      // rows
+	   double *eig,   // becomes eigenvalues
+	   double *w, // workspace
+	   int *wsize,    // size of workspace
+	   double *rwork, // another workspace
+	   int *info);    // error info
 
-void zheev_(char *n,       // whether to compute eigenvectors
-		       char *n2,      // how to store A (upper or lower)
-		       int *size,     // rows
-		       double *M, // hermitian matrix A
-		       int *lda,      // rows
-		       double *eig,   // becomes eigenvalues
-		       double *w, // workspace
-		       int *wsize,    // size of workspace
-		       double *rwork, // another workspace
-		       int *info);    // error info
+int zgetrf_(int *rows, // rows 
+	    int *cols, // columns
+	    double *M, // input matrix, on exit L & U from A=PLU.
+	    int *ld,   // rows
+	    int *ipiv, // becomes permutation indices of P
+	    int *info);// error info
 
-void zgetrf_(int *rows, // rows 
-			int *cols, // columns
-			double *M, // input matrix, on exit L & U from A=PLU.
-			int *ld,   // rows
-			int *ipiv, // becomes permutation indices of P
-			int *info);// error info
+int zgesvd_(char* jobU,     // amount of U to return
+	    char* jobV,     // amount of V to return
+	    int* rows,      // rows
+	    int* cols,      // columns
+	    double *A,  // input matrix for SVD
+	    int *ldA,       // rows
+	    double *Sigma,  // singular values
+	    double *U,  // U
+	    int *ldU,       // rows
+	    double *VT, // V transpose
+	    int *ldVT,      // cols
+	    double *w,  // workspace
+	    int *lwork,     // size of workspace
+	    double *rwork,  // another workspace
+	    int *info);     // error info
 
-void zgesvd_(char* jobU,     // amount of U to return
-			char* jobV,     // amount of V to return
-			int* rows,      // rows
-			int* cols,      // columns
-			double *A,  // input matrix for SVD
-			int *ldA,       // rows
-			double *Sigma,  // singular values
-			double *U,  // U
-			int *ldU,       // rows
-			double *VT, // V transpose
-			int *ldVT,      // cols
-			double *w,  // workspace
-			int *lwork,     // size of workspace
-			double *rwork,  // another workspace
-			int *info);     // error info
+int zgesdd_(char* jobU,     // amount of U to return
+	    int* rows,      // rows
+	    int* cols,      // columns
+	    double *A,  // input matrix for SVD
+	    int *ldA,       // rows
+	    double *Sigma,  // singular values
+	    double *U,  // U
+	    int *ldU,       // rows
+	    double *VT, // V transpose
+	    int *ldVT,      // cols
+	    double *w,  // workspace
+	    int *lwork,     // size of workspace
+	    double *rwork,  // another workspace
+	    int *iwork,     // integer workspace
+	    int *info);     // error info
 
-void zgesdd_(char* jobU,     // amount of U to return
-			int* rows,      // rows
-			int* cols,      // columns
-			double *A,  // input matrix for SVD
-			int *ldA,       // rows
-			double *Sigma,  // singular values
-			double *U,  // U
-			int *ldU,       // rows
-			double *VT, // V transpose
-			int *ldVT,      // cols
-			double *w,  // workspace
-			int *lwork,     // size of workspace
-			double *rwork,  // another workspace
-			int *iwork,     // integer workspace
-			int *info);     // error info
-
-void zgels_(char* job,     // type of least squares problem
-		       int* rows,     // rows
-		       int* cols,     // columns
-		       int* nhrs,     // number right hand sides
-		       double *A,     // input matrix for least squares
-		       int *ldA,      // rows
-		       double *b,     // matrix of right hand side vectors
-		       int *ldb,      // rows of right hand side
-		       double *work,  // workspace
-		       int *lwork,    // size of workspace
-		       int *info);    // error info
-
-void zgelss_(int* rows,     // rows
-			int* cols,     // columns
-			int* nhrs,     // number right hand sides
-			double *A,     // input matrix for least squares
-			int *ldA,      // rows
-			double *b,     // matrix of right hand side vectors
-			int *ldb,      // rows of right hand side
-			double *Sigma, // singular values
-			double *rcond, // used to determine if singular value is 0
-			int *rank,     // rank of the matrix on output
-			double *work,  // workspace
-			int *lwork,    // size of workspace
-			double *rwork,  // workspace
-			int *info);    // error info
+int zgels_(char* job,     // type of least squares problem
+	   int* rows,     // rows
+	   int* cols,     // columns
+	   int* nhrs,     // number right hand sides
+	   double *A,     // input matrix for least squares
+	   int *ldA,      // rows
+	   double *b,     // matrix of right hand side vectors
+	   int *ldb,      // rows of right hand side
+	   double *work,  // workspace
+	   int *lwork,    // size of workspace
+	   int *info);    // error info
+  
+int zgelss_(int* rows,     // rows
+	    int* cols,     // columns
+	    int* nhrs,     // number right hand sides
+	    double *A,     // input matrix for least squares
+	    int *ldA,      // rows
+	    double *b,     // matrix of right hand side vectors
+	    int *ldb,      // rows of right hand side
+	    double *Sigma, // singular values
+	    double *rcond, // used to determine if singular value is 0
+	    int *rank,     // rank of the matrix on output
+	    double *work,  // workspace
+	    int *lwork,    // size of workspace
+	    double *rwork,  // workspace
+	    int *info);    // error info
 
 /* cblas routines */
 // computes "ax + y"
 void cblas_daxpy(const int n,     // length of vectors
-			    const double a,  // scalar alpha
-			    const double* x, // vector x
-			    const int incx,  // increment of x
-			    double* y,       // vector y
-			    const int incy); // increment of y
+		 const double a,  // scalar alpha
+		 const double* x, // vector x
+		 const int incx,  // increment of x
+		 double* y,       // vector y
+		 const int incy); // increment of y
 
 // computes ax
 void cblas_dscal(const int n,     // length of vectors
-			    const double a,  // scalar alpha
-			    const double* x, // vector x
-			    const int incx); // increment of x
+		 const double a,  // scalar alpha
+		 const double* x, // vector x
+		 const int incx); // increment of x
 
 // computes "alpha AB + beta C"
 // NOTE: first 3 args should formally be ENUMS, not ints.
 //       Problem? e.g., what if enums change?
 void cblas_zgemm(const int Order,   // how matrices are stored, by column or row.
-			    const int TransA,  // whether to transform A, e.g. take transpose
-			    const int TransB,  // whether to transform B
-			    const int M,       // rows of A
-			    const int N,       // columns of B
-			    const int K,       // columns of A, which must = rows of B
-			    const void *alpha, // scalar alpha
-			    const void *A,     // matrix A
-			    const int lda,     // rows of A
-			    const void *B,     // matrix B
-			    const int ldb,     // rows of B
-			    const void *beta,  // scalar bet
-			    void *C,           // matrix C; on output, alphaAB+betaC
-			    const int ldc);    // rows of C
+		 const int TransA,  // whether to transform A, e.g. take transpose
+		 const int TransB,  // whether to transform B
+		 const int M,       // rows of A
+		 const int N,       // columns of B
+		 const int K,       // columns of A, which must = rows of B
+		 const void *alpha, // scalar alpha
+		 const void *A,     // matrix A
+		 const int lda,     // rows of A
+		 const void *B,     // matrix B
+		 const int ldb,     // rows of B
+		 const void *beta,  // scalar bet
+		 void *C,           // matrix C; on output, alphaAB+betaC
+		 const int ldc);    // rows of C
 };
 
 class Lapack {
