c1ff2be055ac2e6b65501cf5396eb4f965ad0693
Daniel Grayson <dan@math.uiuc.edu>
2007-09-28 14:29:39 +0000

    new public interface to packed monomial routines, initial steps
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@5948 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ Macaulay2/e/pm.hpp
@@ -0,0 +1,25 @@
+// this is the public interface to the packed monomial routines
+// see pi-readme.txt
+// Copyright, Daniel R. Grayson, 2007
+
+int pm_sign = 1;
+
+template <typename EXP, typename BIN>
+struct pm {
+  int numvars;
+  int numbins;
+  virtual void encode(BIN *&dest, EXP *&);
+  virtual void decode(EXP *&dest, BIN *&);
+  virtual int compare(BIN *&dest, BIN *&);
+  virtual void mult(BIN *&dest, BIN *&, BIN *&);
+  virtual void unsafe_mult(BIN *&dest, BIN *&, BIN *&);
+  virtual void div(BIN *&dest, BIN *&, BIN *&);
+  virtual void unsafe_div(BIN *&dest, BIN *&, BIN *&);
+  virtual int divides(BIN *&, BIN *&);
+  virtual void lcm(BIN *&dest, BIN *&fac1, BIN *&fac2, BIN *&, BIN *&);
+  virtual void gcd(BIN *&dest, BIN *&fac1, BIN *&fac2, BIN *&, BIN *&);
+};
+
+// Local Variables:
+// compile-command: "g++ -Wall -c -x c++ pm.hpp -o /dev/null"
+// End:
