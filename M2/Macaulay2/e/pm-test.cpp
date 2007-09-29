d5aa8bfe8a014f1ea389b70f64dc29474ebc989a
Daniel Grayson <dan@math.uiuc.edu>
2007-09-29 00:41:10 +0000

    add packed monomial test file
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@5966 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ M2/Macaulay2/e/pm-test.cpp
@@ -0,0 +1,10 @@
+#include "pm.hpp"
+
+pmonomial<int> m = { 0 };
+pm<int,int> xxxx(0,0,m);
+pmonomial<long> n = { 0 };
+pm<int,long> yyyy(0,0,n);
+
+// Local Variables:
+// compile-command: "cd $M2BUILDDIR/Macaulay2/e && make pm-test.o"
+// End:
