5b1295f2e46b468f5dd6a98e7de5336763a3d4f7
Daniel Grayson <dan@math.uiuc.edu>
2008-01-20 23:58:57 +0000

    nope, we wanted the heft co-vector afer all, revert!
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@6401 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/monoid.cpp
+++ Macaulay2/e/monoid.cpp
@@ -293,7 +293,7 @@ void Monoid::text_out(buffer &o) const
       o << "}";
     }
 
-  o << "," << newline << "  HeftValues => {";
+  o << "," << newline << "  Heft => {";
   for (i=0; i<heftvals_->len; i++)
     {
       if (i != 0) o << ", ";
