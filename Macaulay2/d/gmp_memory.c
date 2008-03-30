2d93de4b407c3022ed7e7e5f698bff3ca00a21be
Daniel Grayson <dan@math.uiuc.edu>
2008-03-30 22:32:17 +0000

    rename our initial (error siognaling) default gmp memory allocation functions
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@6789 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/gmp_memory.c
+++ Macaulay2/d/gmp_memory.c
@@ -3,20 +3,20 @@
 #include <string.h>
 #define STDERR 2
 
-void *	__gmp_default_allocate (unsigned n) {
-     char *msg = "__gmp_default_allocate called\n";
+static void *our_gmp_default_allocate (unsigned n) {
+     char *msg = "our_gmp_default_allocate called\n";
      write(STDERR,msg,strlen(msg));
      abort();
 }
 
-void *	__gmp_default_reallocate (void *p, unsigned n, unsigned m) {
-     char *msg = "__gmp_default_reallocate called\n";
+static void *our_gmp_default_reallocate (void *p, unsigned n, unsigned m) {
+     char *msg = "our_gmp_default_reallocate called\n";
      write(STDERR,msg,strlen(msg));
      abort();
 }
 
-void	__gmp_default_free (void *p , unsigned n) {
-     char *msg = "__gmp_default_free called\n";
+static void our_gmp_default_free (void *p , unsigned n) {
+     char *msg = "our_gmp_default_free called\n";
      write(STDERR,msg,strlen(msg));
      abort();
 }
@@ -28,9 +28,15 @@ extern void	(*__gmp_free_func) (void *, unsigned);
 
 static void gmp_memory_init_function(void) __attribute__ ((constructor));
 static void gmp_memory_init_function(void) {
+#if 1
+     __gmp_allocate_func = our_gmp_default_allocate;
+     __gmp_reallocate_func = our_gmp_default_reallocate;
+     __gmp_free_func = our_gmp_default_free;
+#else
      __gmp_allocate_func = __gmp_default_allocate;
      __gmp_reallocate_func = __gmp_default_reallocate;
      __gmp_free_func = __gmp_default_free;
+#endif
 }
 
 /*
