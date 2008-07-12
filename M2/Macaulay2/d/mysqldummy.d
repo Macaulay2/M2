72a1635da4ae0608895b48f362c0643542138bb1
Daniel Grayson <dan@math.uiuc.edu>
2008-07-12 18:11:27 +0000

    mysql stuff
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@7259 a8012f80-5f13-0410-8dc0-eb77bd940c0d

---
+++ M2/Macaulay2/d/mysqldummy.d
@@ -0,0 +1,14 @@
+--		Copyright 2008 by Daniel R. Grayson
+
+use common;
+use tokens;
+
+err(e:Expr):Expr := buildErrorPacket("mysql library not available");
+setupfun("mysqlRealConnect",err);
+setupfun("mysqlRealQuery",err);
+setupfun("mysqlQuery",err);
+setupfun("mysqlListDbs",err);
+
+-- Local Variables:
+-- compile-command: "make -C $M2BUILDDIR/Macaulay2/d "
+-- End:
