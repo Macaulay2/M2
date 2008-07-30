d30c8abe3202d0ae5abba78b3b31ac0aa2151d74
Anton Leykin <leykin@math.gatech.edu>
2008-07-30 16:45:29 +0000

    NAG changes
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@7313 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/e/NAG.hpp
+++ Macaulay2/e/NAG.hpp
@@ -4,13 +4,29 @@
 #define _nag_
 
 #include "matrix.hpp"
+#include "complex.h"
+
+// see ../packages/NAG.m2 for the description of the structure of SLPs
+
+#define slpCOPY 1
+#define slpMULTIsum 2
+#define slpPRODUCT 3
 
 class StraightLineProgram : public object
 {
+  M2_arrayint program;
+  M2_CCC* nodes; // array of CCs
+  int num_consts, num_inputs, num_outputs;
+  M2_CCC* output;
+  bool evaluated;
+  long precision;
+
   StraightLineProgram();
 public:
   static StraightLineProgram_OrNull *make(Matrix *consts, M2_arrayint program);
   virtual ~StraightLineProgram() {}
+  
+  void text_out(buffer& o) const;
 
   Matrix *evaluate(const Matrix *vals);
 };
