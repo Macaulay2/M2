Macaulay2/M2

commit 7e91507b580a0cfa0f8f1f51bfccb797533d6581

SimpleDoc should load Text package for the user; improve doc; add bug report
git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@7869 a8012f80-5f13-0410-8dc0-eb77bd940c0d

Macaulay2/m2/html.m2
@@ -12,7 +12,7 @@ Macaulay2HomePage := () -> "http://www.math.uiuc.edu/Macaulay2/index-" | version
 -- we've turned off checking for existence of files...
 
 local prefix; local topNodeButton
-local nullButton; local masterIndexButton; local tocButton; local homeButton; local myHomeButton;
+local nullButton; local masterIndexButton; local tocButton; local homeButton; local directoryButton;
 local NEXT; local PREV; local UP; local tableOfContents; local linkTable; local SRC
 local nextButton; local prevButton; local upButton; local backwardButton; local forwardButton
 local masterIndex
@@ -202,16 +202,9 @@ html HTML := t -> concatenate(
 
 buttonBar := (tag) -> ButtonTABLE {{ 
 	  DIV splice {
-     	       forward tag,
-	       backward tag,
-	       next tag,
-	       prev tag,
-	       up tag,
+     	       forward tag, backward tag, next tag, prev tag, up tag,
      	       (if tag =!= topDocumentTag then topNodeButton else topNodeButton#-1, " | "),
-     	       masterIndexButton, " | ",
-     	       tocButton, " | ",
-     	       homeButton, " | ",
-     	       myHomeButton
+     	       masterIndexButton, " | ", tocButton, " | ", directoryButton, " | ", homeButton
 	       }}}
 
 upAncestors := tag -> reverse (
@@ -361,8 +354,8 @@ assembleTree := (pkg,nodes) -> (
 setupButtons := () -> (
      topNodeButton = HREF {htmlDirectory|topFileName, "top" };
      tocButton = HREF {htmlDirectory|tocFileName, "toc"};
-     homeButton = HREF {Macaulay2HomePage (), "M2 home"};
-     myHomeButton = HREF { "file://" | externalPath | applicationDirectory() | "index.html", "my home"};
+     homeButton = HREF {Macaulay2HomePage (), "Macaulay 2 web site"};
+     directoryButton = HREF { "file://" | externalPath | applicationDirectory() | "index.html", "directory"};
      nullButton = "";
      masterIndexButton = HREF {htmlDirectory|indexFileName,"index"};
      forwardButton = "next";
@@ -395,7 +388,7 @@ makeMasterIndex := (keylist,verbose) -> (
      r := HTML {
 	  HEAD splice { TITLE title, links() },
 	  BODY {
-	       DIV { topNodeButton, " | ", tocButton, " | ", homeButton, " | ", myHomeButton },
+	       DIV { topNodeButton, " | ", tocButton, " | ", directoryButton, " | ", homeButton },
 	       HR{},
 	       HEADER1 title,
 	       DIV between(LITERAL "&nbsp;&nbsp;&nbsp;",apply(alpha, c -> HREF {"#"|c, c})), 
@@ -417,7 +410,7 @@ maketableOfContents := (verbose) -> (
      << html HTML {
 	  HEAD splice { TITLE title, links() },
 	  BODY {
-	       DIV { topNodeButton, " | ", masterIndexButton, " | ", homeButton, " | ", myHomeButton },
+	       DIV { topNodeButton, " | ", masterIndexButton, " | ", directoryButton, " | ", homeButton },
 	       HR{},
 	       HEADER1 title,
 	       toDoc tableOfContents
@@ -452,7 +445,7 @@ runFile := (inf,inputhash,outf,tmpf,desc,pkg,announcechange,rundir,usermode) -> 
 	  ulimit = utest " -t 80" | utest " -m 200000"| utest " -v 200000";
 	  );
      tmpf << "-- -*- M2-comint -*- {* hash: " << inputhash << " *}" << endl << close;
-     cmd := ulimit | "cd " | rundir | "; time " | cmdname | " " | args | " <" | format inf | " >>" | format tmpf | " 2>&1";
+     cmd := ulimit | "cd " | rundir | "; " | cmdname | " " | args | " <" | format inf | " >>" | format tmpf | " 2>&1";
      stderr << cmd << endl;
      r := run cmd;
      if r == 0 then (
@@ -1108,8 +1101,7 @@ makePackageIndex List := path -> (
 	  BODY { 
 	       -- buttonBar tag, HR{},
 	       PARA {
-		    "This is the top level documentation page for Macaulay 2 and its packages, stored in 
-		    your application directory.
+		    "This is the directory for Macaulay 2 and its packages.
 		    Bookmark this page for future reference, or run the viewHelp command in Macaulay 2
 		    to open up your browser on this page."
 		    },

Macaulay2/packages/SimpleDoc.m2
@@ -1,4 +1,5 @@
 -- -*- coding: utf-8 -*-
+needsPackage "Text";
 newPackage(
 	"SimpleDoc",
     	Version => "1.0", 

Macaulay2/bugs/dan/0-info-files
@@ -2,6 +2,10 @@ links don't quite work:
 
   - in a package Foo, links to Foo should be redirected to Top
 
+  - in the node for Ideal, the link to "content" is broken,
+    probably because it's redirected to "content_lpRingElement_rp".
+    It needs to be a more complex link.
+
 This menu item is bad because there is a colon on the left side:
 
     * Parser : Analyzer: Parser _co Analyzer.  combine a parser with a lexical

Macaulay2/packages/Classic.m2
@@ -33,8 +33,10 @@ arrayPolyP = toList % seqP_";" listPolyP
 export poly ; poly = method()
 poly String :=  RingElement => polyP : nonspaceAnalyzer
 ideal String := Ideal => ideal % listPolyP : nonspaceAnalyzer
-monomialIdeal String := MonomialIdeal => monomialIdeal % listPolyP : nonspaceAnalyzer
-matrix String := Matrix => opts -> matrix_opts % arrayPolyP : nonspaceAnalyzer
+monomialIdeal String := MonomialIdeal => 
+                 monomialIdeal % listPolyP : nonspaceAnalyzer
+matrix String := Matrix => opts -> 
+                  matrix_opts % arrayPolyP : nonspaceAnalyzer
 -- end --
 
 beginDocumentation()

Macaulay2/packages/Macaulay2Doc/doc3.m2
@@ -309,10 +309,13 @@ document {
 document {
      Key => Keyword,
      Headline => "the class of all keywords",
-     "Keywords are symbols that are treated specially by the system while parsing user input.  Some of them,
-     such as ", TO "and", ", consist of alphanumeric characters and look just like
-     ordinary symbols.  Others, such as ", TO "==>", ", consist of special characters
-     and are called operators."
+     PARA {
+     	  "Keywords are symbols that are treated specially by the system while parsing user input.  Some of them,
+     	  such as ", TO "and", ", consist of alphanumeric characters and look just like
+     	  ordinary symbols.  Others, such as ", TO "==>", ", consist of special characters
+     	  and are called operators."
+	  },
+     SeeAlso => {"precedence of operators"}
      }
 
 document {

commit a94676f67f0033576bc3eecdea7d821ee53eeb04

fix a bug introduced in r7869 that prevented startup
git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@7870 a8012f80-5f13-0410-8dc0-eb77bd940c0d

Macaulay2/m2/html.m2
@@ -64,7 +64,6 @@ searchPrefixPath = url -> (				    -- entries on the prefixPath may have differe
 	       )
 	  );
      if debugLevel > 5 then stderr << "--file " << url << " not found in " << prefixPath << endl;
-     if debugLevel > 10 then error "debug me";
      )
 
 toURL := path -> (

Macaulay2/packages/SimpleDoc.m2
@@ -1,5 +1,4 @@
 -- -*- coding: utf-8 -*-
-needsPackage "Text";
 newPackage(
 	"SimpleDoc",
     	Version => "1.0", 
@@ -352,3 +351,7 @@ toDoc ///
     matrix
     (directSum,List)
 ///
+
+endPackage "SimpleDoc"
+-- this needs to go at the end:
+needsPackage "Text"