Macaulay2/M2

commit 5d0b9320516207c32c7900eb35488d0ea56abcdb

fix "M2 ../foo.m2" so it works; makefiles; undo -r7890 so minimizeFilename, relativizeFilename, and toAbsolutePath work as before, textually
git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@7892 a8012f80-5f13-0410-8dc0-eb77bd940c0d

M2/Macaulay2/m2/html.m2
@@ -626,7 +626,7 @@ installPackage Package := opts -> pkg -> (
      	  
 	  -- copy package source subdirectory
 	  srcDirectory := replace("PKG",pkg#"title",layout#"package");
-	  dn := realpath(currentSourceDir|buildPackage);
+	  dn := realpath currentSourceDir | buildPackage;
 	  if isDirectory dn
 	  then (
 	       if not (options pkg).AuxiliaryFiles
@@ -717,7 +717,7 @@ installPackage Package := opts -> pkg -> (
 		    ));
 
 	  -- make example output files, or else copy them from old package directory tree
-	  exampleDir' := realpath(currentSourceDir|buildPackage|"/examples") | "/";
+	  exampleDir' := realpath currentSourceDir | buildPackage | "/examples" | "/";
 	  outfn' := fkey -> exampleDir'|toFilename fkey|".out";
 	  gethash := outf -> (
 	       f := get outf;

M2/BUILD/dan/Makefile.include
@@ -4,15 +4,18 @@ include builds/$(DIRECTORY)/include/configuration
 .PHONY : everything world configure all install check-links distclean clean arch-chk install-it rebuild-world default
 all:; $(MAKE) -C builds/$(DIRECTORY) $@ $(BUILDOPTIONS)
 arch-chk:
-check-links:; $(MAKE) -C builds/$(DIRECTORY) -C Macaulay2/html-check-links check
+everything:; $(MAKE) -C builds/$(DIRECTORY) all RemakeAllDocumentation=true RemakePackages=true RerunExamples=true
+check-links:; $(MAKE) -C builds/$(DIRECTORY)/Macaulay2/html-check-links check
 check:; $(MAKE) -C builds/$(DIRECTORY) -k $@
 configure builds/$(DIRECTORY)/include/configuration:; mkdir -p builds/$(DIRECTORY) && cd builds/$(DIRECTORY) && $(PWD)/../../configure $(CONFIGOPTIONS)
 default: arch-chk all install check-links
-distclean:; $(MAKE) -C builds/$(DIRECTORY) $@ && find . -type f
+distclean: distclean1 distclean2
+distclean1:; $(MAKE) -C builds/$(DIRECTORY) distclean
+distclean2:; find builds/$(DIRECTORY) -type f | sed 's/^/=== /'
 install-it:; $(MAKE) -C builds/$(DIRECTORY) install -o all $(INSTALLOPTIONS)
 install:pre-install install-it post-install
 pre-install post-install:
-rebuild-world: arch-chk distclean world
+rebuild-world: arch-chk distclean configure everything install check
 world: arch-chk configure all install check
 CONFIGOPTIONS += --enable-encap
 CONFIGOPTIONS += --enable-common-staging-area

M2/BUILD/dan/Makefile.ubuntu64
@@ -1,5 +1,5 @@
 #-*- Makefile -*-
-BUILDOPTIONS += RemakeAllDocumentation=true RemakePackages=true
+#BUILDOPTIONS += RemakeAllDocumentation=true RemakePackages=true
 #BUILDOPTIONS += -s
 #INSTALLOPTIONS += -s
 

M2/Macaulay2/bugs/dan/0-abs-paths
@@ -1,0 +1,3 @@
+error messages in output files should not use relative paths
+there should be a way to turn that on
+runFile in html.m2 should use it

M2/Macaulay2/d/actors4.d
@@ -1367,15 +1367,6 @@ locate(e:Expr):Expr := (
 setupfun("locate",locate);
 
 export bar := 1;
-
--- codePosition(e:Expr):Expr := (
---      when e is c:CodeClosure do (
--- 	  p := codePosition(c.code);
--- 	  Expr(Sequence(
--- 	       verifyMinimizeFilename(p.filename),toInteger(int(p.line)),toInteger(int(p.column)),toInteger(int(p.line)),toInteger(int(p.column)))))
---      else WrongArg("pseudocode"));
--- setupfun("codePosition", codePosition);
-
 export storeInHashTableWithCollisionHandler(x:HashTable,key:Expr,value:Expr,handler:Expr):Expr := (
      if !x.mutable then return buildErrorPacket("attempted to modify an immutable hash table");
      h := hash(key);

M2/Macaulay2/d/actors5.d
@@ -1343,7 +1343,13 @@ changeDirectory(e:Expr):Expr := (
      else WrongArgString());
 setupfun("changeDirectory",changeDirectory);
 
-realpathfun(e:Expr):Expr := when e is filename:string do Expr(realpath(filename)) else WrongArgString();
+realpathfun(e:Expr):Expr := (
+     when e is f:string do (
+     	  when realpath(f)
+     	  is null do buildErrorPacket(syscallErrorMessage("realpath"))
+     	  is p:string do Expr(p)
+     	  )
+     else WrongArgString());
 setupfun("realpath",realpathfun);
 
 setupconst("typicalValues", Expr(typicalValues));

M2/Macaulay2/d/scclib.c
@@ -379,7 +379,7 @@ M2_string system_realpath(M2_string filename) {
   char buf[PATH_MAX];
   char *r = realpath(fn,buf);
   GC_FREE(fn);
-  return r == NULL ? filename : tostring(buf);
+  return r == NULL ? NULL : tostring(buf);
 }
 
 M2_string system_errfmt(M2_string filename, int lineno, int colno, int loaddepth) {

M2/Macaulay2/d/stdiop.d
@@ -18,7 +18,7 @@ use ctype;
 export Position := {filename:string, line:ushort, column:ushort, loadDepth:uchar};
 export (s:Position) === (t:Position) : bool := s == t || s.filename === t.filename && s.line == t.line && s.column == t.column;
 export dummyPosition := Position("{*dummy file name*}",ushort(0),ushort(0),uchar(loadDepth));
-shorten(s:string):string := (
+shorten(s:string):string := (				    -- purely textual
      -- shorten filenames like "/a/b/c/../d////e/f" to "/a/b/d/e/f"
      while true do (
 	  i := 0;
@@ -53,18 +53,21 @@ shorten(s:string):string := (
 	       );
 	  );
      );
-isAbsolutePath(s:string):bool := (
+isAbsolutePath(s:string):bool := (                         			    -- purely textual
      -- eventually make this happen only in MSDOS
      length(s) >= 1 && s.0 == '/' ||
      length(s) >= 3 && s.1 == ':' && s.2 == '/' ||
      s === "stdio"
      );
-absoluteFilename(filename:string):string := (
-     if !isAbsolutePath(filename) then filename = getcwd() + filename;
-     -- old way was: shorten(filename), purely textual, could get into trouble with symbolic links
-     f := realpath(filename);				    -- new way looks at the file system
-     if length(filename) == 0 || filename.(length(filename)-1) == '/' && length(f)>0 && f.(length(f)-1) != '/' then f = f + '/';
-     f);
+absoluteFilename(filename:string):string := (					    -- purely textual
+     if !isAbsolutePath(filename)
+     then filename = getcwd() + filename;		    -- using getcwd() instead of getenv("PWD"), which is what emacs thinks about
+     shorten(filename)
+     -- if we wanted to take real paths into account we would do it like this instead:
+     -- f := realpath(filename);
+     -- if length(filename) == 0 || filename.(length(filename)-1) == '/' && length(f)>0 && f.(length(f)-1) != '/' then f = f + '/';
+     -- f
+     );
 export relativizeFilename(cwd:string,filename:string):string := (
      cwd = absoluteFilename(cwd);
      if length(cwd) == 0 || cwd.(length(cwd)-1) != '/' then cwd = cwd + '/';
@@ -87,12 +90,22 @@ export minimizeFilename(filename:string):string := (
      if length(c) == 0 then c = "./";
      c);
 export verifyMinimizeFilename(filename:string):string := (
+     p := when realpath(filename) is null do return filename is s:string do s;
      f := minimizeFilename(filename);
      if isAbsolutePath(f) then return f;
+     if !(length(f) >= 3 && f.0 == '.' && f.1 == '.' && f.2 == '/') then (
+	  -- we assume realpath(getenv("PWD")) and getcwd() would be the same, so the only problem for
+	  -- emacs would be if the minimized path begins with ../
+	  return f;
+	  );
      pwd := getenv("PWD")+"/";
      if pwd === getcwd() then return f;
-     h := realpath(getcwd()+f);			       -- this is the right way
-     k := realpath(shorten(pwd+f)); -- this simulates what emacs does, textually, which might be wrong
+     h := when
+     realpath(getcwd()+f)			       -- this is correct
+     is null do return filename is s:string do s;
+     k := when
+     realpath(shorten(pwd+f))                    -- this simulates what emacs does, textually, which might be wrong
+     is null do return filename is s:string do s;
      if k === h then f else h);
 export tostring(w:Position) : string := (
      if w == dummyPosition 

commit 0f80e7a87c20f4f58535fae3330236bb034b9840

add slashes after realpath, fixing a problem introduced in r7892
git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@7897 a8012f80-5f13-0410-8dc0-eb77bd940c0d

M2/Macaulay2/m2/html.m2
@@ -626,7 +626,7 @@ installPackage Package := opts -> pkg -> (
      	  
 	  -- copy package source subdirectory
 	  srcDirectory := replace("PKG",pkg#"title",layout#"package");
-	  dn := realpath currentSourceDir | buildPackage;
+	  dn := realpath currentSourceDir | "/" | buildPackage;
 	  if isDirectory dn
 	  then (
 	       if not (options pkg).AuxiliaryFiles
@@ -717,7 +717,7 @@ installPackage Package := opts -> pkg -> (
 		    ));
 
 	  -- make example output files, or else copy them from old package directory tree
-	  exampleDir' := realpath currentSourceDir | buildPackage | "/examples" | "/";
+	  exampleDir' := realpath currentSourceDir | "/" | buildPackage | "/examples" | "/";
 	  outfn' := fkey -> exampleDir'|toFilename fkey|".out";
 	  gethash := outf -> (
 	       f := get outf;