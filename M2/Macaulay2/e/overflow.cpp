13ec93569c16614cfa0c8e2170cd5715dc45c7fc
Daniel Grayson <dan@math.uiuc.edu>
2008-12-29 05:45:06 +0000

    use libgc also for the engine_error class
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@7915 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/overflow.cpp
+++ M2/Macaulay2/e/overflow.cpp
@@ -3,3 +3,7 @@
 namespace safe {
      void ov(const char *msg) { throw(exc::overflow_error(msg)); }
 }
+
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e overflow.ss"
+// End:
