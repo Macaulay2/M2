ca2033977785d130f192cb820b805d59d6d0f0e0
Daniel Grayson <dan@math.uiuc.edu>
2009-03-14 01:31:15 +0000

    python stuff
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@8541 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/python-c.c
+++ M2/Macaulay2/d/python-c.c
@@ -67,3 +67,26 @@ PyObject *python_ObjectType(PyObject *o) {
   return PyObject_Type(o);
 }
 
+/* see http://docs.python.org/extending/extending.html for this example */
+
+static PyObject * spam_system(PyObject *self, PyObject *args) {
+  const char *command;
+  int sts;
+  if (!PyArg_ParseTuple(args, "s", &command)) return NULL;
+  sts = system(command);
+  return Py_BuildValue("i", sts);
+}
+static PyObject *SpamError;
+static PyMethodDef SpamMethods[] = {
+  {"system",  spam_system, METH_VARARGS, "Execute a shell command."},
+  {NULL, NULL, 0, NULL}
+};
+void python_initspam() {
+  static char name[] = "spam.error";
+  PyObject *m;
+  m = Py_InitModule("spam", SpamMethods);
+  if (m == NULL) return;
+  SpamError = PyErr_NewException(name, NULL, NULL);
+  Py_INCREF(SpamError);
+  PyModule_AddObject(m, "error", SpamError);
+}
