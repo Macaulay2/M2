b5f2c7fe497a0d90bb04e65af63563af4a6a2f47
Daniel Grayson <dan@math.uiuc.edu>
2009-07-15 03:14:51 +0000

    XML
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@8809 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/xml-c.c
+++ M2/Macaulay2/d/xml-c.c
@@ -3,6 +3,7 @@
 #include <libxml/tree.h>
 #include <string.h>
 #include "M2mem.h"
+#include "M2types.h"
 
 static char *copystring(const char *s) {
   char *p = (char *)getmem(strlen(s)+1);
@@ -13,6 +14,25 @@ static char *copystring(const char *s) {
 void initxml() __attribute__ ((constructor));
 void initxml() {
   xmlGcMemSetup(freemem,(void *(*)(size_t))getmem,(void *(*)(size_t))getmem_atomic,(void *(*)(void *,size_t))getmoremem1,copystring);
+}
+
+static void display(xmlNode *u) {
+  xmlNode *t = u;
+  if (t == NULL) return;
+  for (; t; t = t->next) if (t->type == XML_ELEMENT_NODE) printf("%s\n", t->name);
+  display(u->children);
+}
+
+void xml_parse(M2_string p) {
+  xmlDocPtr doc = xmlReadMemory(p->array,p->len,"a string", NULL, 0);
+  if (doc == NULL) {
+    printf("xml syntax error\n");
+  }
+  else {
+    display(xmlDocGetRootElement(doc));
+    printf("\n");
+    xmlFreeDoc(doc);
+  }
 }
 
 /*
