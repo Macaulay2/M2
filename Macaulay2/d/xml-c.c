32038302070bc29291656e5fc5743d892f276153
Daniel Grayson <dan@math.uiuc.edu>
2009-07-15 15:25:22 +0000

    XML
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@8816 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/xml-c.c
+++ Macaulay2/d/xml-c.c
@@ -16,16 +16,57 @@ void initxml() {
   xmlGcMemSetup(freemem,(void *(*)(size_t))getmem,(void *(*)(size_t))getmem_atomic,(void *(*)(void *,size_t))getmoremem1,copystring);
 }
 
-static void display(xmlDocPtr doc,xmlNodePtr u) {
-  xmlNodePtr t = u;
-  if (t == NULL) return;
-  for (; t; t = t->next) 
-    if (t->type == XML_ELEMENT_NODE) {
-      printf("%s: ", t->name);
-      xmlElemDump(stdout,doc,t);
-      printf("\n");
+static int level = 0;
+static void indent() { int i; for (i=2*level; i; i--) putchar(' '); }
+static void display(xmlDocPtr doc,xmlNodePtr t) {
+  level++;
+  for (;; t = t->next) {
+    if (t == NULL) {
+      level--;
+      return;
     }
-  display(doc,u->children);
+    indent();
+    switch (t->type) {
+    case XML_ELEMENT_NODE: {
+      /* xmlElementPtr e = (xmlElementPtr)t; */
+      xmlAttrPtr a = t->properties;
+      level++;
+      printf("XML_ELEMENT_NODE: "); 
+      printf("%s:", t->name);
+      for (;a;a=a->next) {
+	printf("\n");
+	indent();
+	printf("%s=:\n",a->name);
+	display(doc,a->children);
+      }
+      level--;
+      break;
+    }
+    case XML_ATTRIBUTE_NODE: printf("XML_ATTRIBUTE_NODE: "); break;
+    case XML_TEXT_NODE: printf("XML_TEXT_NODE: "); break;
+    case XML_CDATA_SECTION_NODE: printf("XML_CDATA_SECTION_NODE: "); break;
+    case XML_ENTITY_REF_NODE: printf("XML_ENTITY_REF_NODE: "); break;
+    case XML_ENTITY_NODE: printf("XML_ENTITY_NODE: "); break;
+    case XML_PI_NODE: printf("XML_PI_NODE: "); break;
+    case XML_COMMENT_NODE: printf("XML_COMMENT_NODE: "); break;
+    case XML_DOCUMENT_NODE: printf("XML_DOCUMENT_NODE: "); break;
+    case XML_DOCUMENT_TYPE_NODE: printf("XML_DOCUMENT_TYPE_NODE: "); break;
+    case XML_DOCUMENT_FRAG_NODE: printf("XML_DOCUMENT_FRAG_NODE: "); break;
+    case XML_NOTATION_NODE: printf("XML_NOTATION_NODE: "); break;
+    case XML_HTML_DOCUMENT_NODE: printf("XML_HTML_DOCUMENT_NODE: "); break;
+    case XML_DTD_NODE: printf("XML_DTD_NODE: "); break;
+    case XML_ELEMENT_DECL: printf("XML_ELEMENT_DECL: "); break;
+    case XML_ATTRIBUTE_DECL: printf("XML_ATTRIBUTE_DECL: "); break;
+    case XML_ENTITY_DECL: printf("XML_ENTITY_DECL: "); break;
+    case XML_NAMESPACE_DECL: printf("XML_NAMESPACE_DECL: "); break;
+    case XML_XINCLUDE_START: printf("XML_XINCLUDE_START: "); break;
+    case XML_XINCLUDE_END: printf("XML_XINCLUDE_END: "); break;
+    case XML_DOCB_DOCUMENT_NODE: printf("XML_DOCB_DOCUMENT_NODE: "); break;
+    default: printf("unknown node type");
+    }
+    printf("\n");
+    display(doc,t->children);
+  }
 }
 
 void xml_parse(M2_string p) {
@@ -34,8 +75,10 @@ void xml_parse(M2_string p) {
     printf("xml syntax error\n");
   }
   else {
-    display(doc,xmlDocGetRootElement(doc));
+    printf("document: ");
+    xmlDocDump(stdout,doc);
     printf("\n");
+    display(doc,xmlDocGetRootElement(doc));
     xmlFreeDoc(doc);
   }
 }
