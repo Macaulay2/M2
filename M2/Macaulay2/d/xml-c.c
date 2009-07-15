d4f5b099293e3d00d2eace2c624ef1760cd306f0
Daniel Grayson <dan@math.uiuc.edu>
2009-07-15 16:31:05 +0000

    XML
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@8821 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/d/xml-c.c
+++ M2/Macaulay2/d/xml-c.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
 #include <string.h>
 #include "M2mem.h"
 #include "M2types.h"
+#include "xml-c.h"
 
 static char *copystring(const char *s) {
   char *p = (char *)getmem(strlen(s)+1);
@@ -43,7 +42,7 @@ static void display(xmlDocPtr doc,xmlNodePtr t) {
       break;
     }
     case XML_ATTRIBUTE_NODE: printf("XML_ATTRIBUTE_NODE: "); break;
-    case XML_TEXT_NODE: printf("XML_TEXT_NODE: "); break;
+    case XML_TEXT_NODE: printf("XML_TEXT_NODE: %s",t->content); break;
     case XML_CDATA_SECTION_NODE: printf("XML_CDATA_SECTION_NODE: "); break;
     case XML_ENTITY_REF_NODE: printf("XML_ENTITY_REF_NODE: "); break;
     case XML_ENTITY_NODE: printf("XML_ENTITY_NODE: "); break;
@@ -69,6 +68,29 @@ static void display(xmlDocPtr doc,xmlNodePtr t) {
   }
 }
 
+static void show(xmlDocPtr doc,xmlNodePtr t) {
+  for (;; t = t->next) {
+    if (t == NULL) return;
+    switch (t->type) {
+    case XML_ELEMENT_NODE: {
+      xmlAttrPtr a = t->properties;
+      printf("<%s", t->name);
+      for (;a;a=a->next) {
+	printf(" ");
+	printf("%s=",a->name);
+	show(doc,a->children);
+      }
+      printf(">");
+      show(doc,t->children);
+      printf("</%s>", t->name);
+      break;
+    }
+    case XML_TEXT_NODE: printf("\"%s\"",t->content); break;
+    default:;
+    }
+  }
+}
+
 void xml_parse(M2_string p) {
   xmlDocPtr doc = xmlReadMemory(p->array,p->len,"a string", NULL, 0);
   if (doc == NULL) {
@@ -78,10 +100,76 @@ void xml_parse(M2_string p) {
     printf("document: ");
     xmlDocDump(stdout,doc);
     printf("\n");
-    display(doc,xmlDocGetRootElement(doc));
+    show(doc,xmlDocGetRootElement(doc));
+    printf("\n");
     xmlFreeDoc(doc);
   }
 }
+
+struct xml_node *xml_Parse(M2_string p) {
+  struct xml_node *r = (struct xml_node *)getmem(sizeof(*r));
+  r->doc = xmlReadMemory(p->array,p->len,"a string", NULL, 0);
+  r->node = xmlDocGetRootElement(r->doc);
+  return r;
+}
+
+struct xml_attr *xml_Attributes(struct xml_node *n) {
+  struct xml_attr *r;
+  if (n->node->type != XML_ELEMENT_NODE || n->node->properties == NULL) return NULL;
+  r = (struct xml_attr *)getmem(sizeof(*r));
+  r->doc = n->doc;
+  r->attr = n->node->properties;
+  return r;
+}
+
+int xml_isElement(struct xml_node *n){
+  return n->node->type == XML_ELEMENT_NODE;
+}
+
+int xml_isText(struct xml_node *n){
+  return n->node->type == XML_TEXT_NODE;
+}
+
+M2_string xml_getElementName(struct xml_node *n){
+  return tostring((char const *)n->node->name);
+}
+
+struct xml_node *nxml_getNextNode(struct xml_node *n){
+  struct xml_node *r;
+  if (n->node->next == NULL) return NULL;
+  r = (struct xml_node *)getmem(sizeof(*r));
+  r->doc = n->doc;
+  r->node = n->node->next;
+  return r;
+}
+
+struct xml_attr *axml_getNextAttr(struct xml_attr *a){
+  struct xml_attr *r;
+  if (a->attr->next == NULL) return NULL;
+  r = (struct xml_attr *)getmem(sizeof(*r));
+  r->doc = a->doc;
+  r->attr = a->attr->next;
+  return r;
+}
+
+struct xml_node *nxml_getNodeChildren(struct xml_node *n){
+  struct xml_node *r;
+  if (n->node->children == NULL) return NULL;
+  r = (struct xml_node *)getmem(sizeof(*r));
+  r->doc = n->doc;
+  r->node = n->node->children;
+  return r;
+}
+
+struct xml_node *nxml_getAttrChildren(struct xml_attr *a){
+  struct xml_node *r;
+  if (a->attr->children == NULL) return NULL;
+  r = (struct xml_node *)getmem(sizeof(*r));
+  r->doc = a->doc;
+  r->node = a->attr->children;
+  return r;
+}
+
 
 /*
  Local Variables:
