0baaf61f871b315c36f1dfa43ad8fcd605f1b920
Daniel Grayson <dan@math.uiuc.edu>
2010-01-29 18:00:59 +0000

    moving Bill's branches
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@10686 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/packages/development/msri2010/gfurnish/e/f4/f4-types.cpp
+++
@@ -1,5 +0,0 @@
-#include "f4-types.hpp"
-
-// Local Variables:
-// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
-// End:
--- M2/Macaulay2/packages/development/msri2010/gfurnish/e/int-bag.cpp
+++
@@ -1,9 +0,0 @@
-// (c) 1995  Michael E. Stillman
-
-#include "int-bag.hpp"
-
-
-
-// Local Variables:
-// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
-// End:
