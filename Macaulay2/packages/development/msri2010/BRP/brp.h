3eaa043b22ee745daadfb668a95f9f7b35b0cbf2
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2010-02-17 19:25:01 +0000

    more Buchberger optimization
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk/M2@10860 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/packages/development/msri2010/BRP/brp.h
+++ Macaulay2/packages/development/msri2010/BRP/brp.h
@@ -4,11 +4,15 @@
 #include <map>
 #include <string>
 
+//////// CAREFUL ////////
+// multiplication and addition are not const! they don't work as expected but
+//change one of the arguments!
+//////// CAREFUL ////////
+
 using namespace std;
 
 class BRP
 {
-  list<int> mylist;
 
   friend ostream& operator<< (ostream &out, BRP &self) {
     for(list<int>::iterator i=self.mylist.begin(); i != self.mylist.end(); ++i) {
@@ -18,24 +22,37 @@ class BRP
     return out;
   }
 
-  list<int> mod2(const list<int> &alist) {
+  static void mod2(list<int> &alist) {
     map<int,int> tally;
-    list<int> tmp;
     for (list<int>::const_iterator i=alist.begin(); i != alist.end(); ++i) {
       ++tally[*i];
     }
-    for(map<int,int>::iterator iter = tally.begin(); iter != tally.end();
-    ++iter) {
+    
+    list<int> tmp;
+    for(map<int,int>::iterator iter = tally.begin(); iter != tally.end(); ++iter) {
       if(iter->second % 2 == 1) {
         tmp.push_back(iter->first);
       }
     }
     tmp.sort();
     tmp.reverse();
-    return tmp;
+    alist = tmp;
   }
 
   public:
+  
+  list<int> mylist;
+
+  static bool isDivisibleBy(int a, int b) {
+    // check if a is divisible by b
+    int lcm = a | b;
+    return lcm == a;
+  }
+
+  static bool isRelativelyPrime(int a, int b) {
+    // check if a and b are relatively prime
+    return ( ((a | b) ^ a) == b);
+  }
 
   BRP();
 
@@ -57,15 +74,16 @@ class BRP
   int operator/(const BRP &other) const;
   int operator/(int other) const;
 
-  bool isDivisibleBy(const BRP &other) const;
-
   bool leadingIsRelativelyPrime(const BRP &other) const;
 
   bool isLeadingReducibleBy(const BRP &other) const;
+  bool isLeadingReducibleBy(int other) const;
   
   BRP remainder(const BRP &x) const;
 
   int LT() const;
+
+  void removeLeading();
 };
 
 
