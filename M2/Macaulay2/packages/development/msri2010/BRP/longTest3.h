d4fd2890bf0c114f591eb29c9ca3b1cccad92dc5
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2010-04-21 13:45:01 +0000

    removing BRP files
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@11119 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/packages/development/msri2010/BRP/longTest3.h
+++
@@ -1,6 +0,0 @@
-//This takes -- 
-// -- used 1.84039 seconds
-//  in M2
-
-map<int,BRP> testLongTest3Example();
-map<int,BRP> testLongTest3ExampleCorrect();
--- M2/Macaulay2/packages/development/msri2010/BRP/longTest5.h
+++
@@ -1,6 +0,0 @@
-//This takes -- 
-// -- used long seconds
-//  in M2
-
-map<int,BRP> testLongTest5Example();
-map<int,BRP> testLongTest5ExampleCorrect();
