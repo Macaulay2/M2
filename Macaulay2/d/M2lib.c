2e07d8d242dafb1693452cc8ec71ed5083dd4c67
Bill Gary Furnish <gfurnish@gfurnish.net>
2011-01-09 20:05:48 +0000

    Remove syscalls for cygwin compatability
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/branches/gfurnish/Parallel2@12701 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/M2lib.c
+++ Macaulay2/d/M2lib.c
@@ -121,7 +121,7 @@ void segv_handler2(int sig) {
 void stack_trace() {
      void (*old)(int) = signal(SIGSEGV,segv_handler2); /* in case traversing the stack below causes a segmentation fault */
      unblock(SIGSEGV);
-     fprintf(stderr,"-- stack trace, pid %ld:\n", (long)syscall(SYS_getpid));
+     fprintf(stderr,"-- stack trace, pid %ld:\n", (long)getpid());
      if (0 == sigsetjmp(stack_trace_jump,TRUE)) {
 #	  define D fprintf(stderr,"level %d -- return addr: 0x%08lx -- frame: 0x%08lx\n",i,(long)__builtin_return_address(i),(long)__builtin_frame_address(i))
 #	  define i 0
@@ -387,9 +387,6 @@ int register_fun(int *count, char *filename, int lineno, char *funname) {
 	#include <sys/types.h>
 	#endif
 	#include <stdlib.h>
-	#ifdef HAVE_SYSCALL_H
-	#include <syscall.h>
-	#endif
 	#include <time.h>
 	#include <stdio.h>
 	#include <unistd.h> // for sysconf
