4282b93c9ee5c5dad2954adbd78985024d2705d4
Bill Gary Furnish <gfurnish@gfurnish.net>
2011-01-10 03:49:24 +0000

    Apply fix for cygwin CPUTime
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/branches/gfurnish/Parallel2@12704 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- Macaulay2/d/M2lib.c
+++ Macaulay2/d/M2lib.c
@@ -381,19 +381,34 @@ int register_fun(int *count, char *filename, int lineno, char *funname) {
      return 0;
      }
 
-#ifdef HAVE_CLOCK_GETTIME
+#if defined(HAVE_CLOCK_GETTIME) && ( defined(CLOCK_THREAD_CPUTIME_ID) || defined(CLOCK_THREAD_CPUTIME) || defined(CLOCK_PROCESS_CPUTIME_ID) || defined(CLOCK_PROCESS_CPUTIME) )
 
 	#ifdef HAVE_SYS_TYPES_H
 	#include <sys/types.h>
 	#endif
 	#include <stdlib.h>
+	#ifdef HAVE_SYSCALL_H
+	#include <syscall.h>
+	#endif
 	#include <time.h>
 	#include <stdio.h>
 	#include <unistd.h> // for sysconf
 	static __thread double startTime;
 	double system_cpuTime(void) {
 	     struct timespec t;
-	     int err = clock_gettime(CLOCK_THREAD_CPUTIME_ID,&t);
+	     int err = clock_gettime(
+		  #if defined(CLOCK_THREAD_CPUTIME_ID)
+		  CLOCK_THREAD_CPUTIME_ID
+		  #elif defined(CLOCK_THREAD_CPUTIME)
+		  CLOCK_THREAD_CPUTIME
+		  #elif defined(CLOCK_PROCESS_CPUTIME_ID)
+		  CLOCK_PROCESS_CPUTIME_ID
+		  #elif defined(CLOCK_PROCESS_CPUTIME)
+		  CLOCK_PROCESS_CPUTIME
+		  #else
+		  #error "clock_gettime: no suitable argument for getting CPU time"
+		  #endif
+		  ,&t);
 	     if (err) return 0;		/* silent about error */
 	     double u = t.tv_sec + t.tv_nsec * 1e-9;
 	     return u - startTime;
