d5c751c6342adab25ba01c6d6edaadeab1b685c2
Florian Geiss <fg@math.uni-sb.de>
2011-07-28 23:58:37 +0000

    worked with Mike on ChineseRemainder engine routines
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@13629 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/e/cra.cpp
+++ M2/Macaulay2/e/cra.cpp
@@ -23,54 +23,50 @@ bool ChineseRemainder::computeMultipliers(mpz_t m, mpz_t n, mpz_t result_um, mpz
   return true;
 }
 
-void ChineseRemainder::CRA(mpz_t a, mpz_t b, mpz_t m, mpz_t n, mpz_t result)
+ring_elem ChineseRemainder::CRA(const PolyRing *R, ring_elem ff, ring_elem gg, mpz_t um, mpz_t vn, mpz_t mn)
 {
-  mpz_t um, vn, mn;
-  mpz_init(um);
-  mpz_init(vn);
-  mpz_init(mn);
-  computeMultipliers(m, n, um, vn, mn);
-  CRA0(a,b,um,vn,mn,result);
-  mpz_clear(um);
-  mpz_clear(vn);
-  mpz_clear(mn);
-}
-
-
-ring_elem ChineseRemainder::combine(const PolyRing *R, ring_elem ff, ring_elem gg, mpz_t m, mpz_t n)
-{
-  // Assumption: f and g are in a poly ring whose coeff ring is ZZ
-
-  mpz_t um, vn, mn;
-  mpz_init(um);
-  mpz_init(vn);
-  mpz_init(mn);
-  computeMultipliers(m, n, um, vn, mn);
-
   mpz_t result_coeff;
   mpz_init(result_coeff);
-
-  const Monoid *M = R->getMonoid();
-  const Ring *K = R->getCoefficientRing();
-
+  
   Nterm *f = ff;
   Nterm *g = gg;
   Nterm head;
   Nterm *result = &head;
-
+  
+  const Monoid *M = R->getMonoid();
+  const Ring *K = R->getCoefficientRing();
+  
   while (1)
+  {
+    if (g == NULL) 
     {
-      if (g == NULL) 
-	{
-	  // mult each term of g by n.
-	  break;
-	}
-      if (f == NULL) 
-	{
-	  // return 0
-	  break;
-	}
-      switch (M->compare(f->monom, g->monom)) {
+      // mult each term of f by n:
+      for ( ;f!=0; f=f->next)
+      {
+        result->next = R->new_term();
+        result = result->next;
+        result->next = 0;
+        M->copy(f->monom, result->monom);
+        mpz_mul(result_coeff, f->coeff.get_mpz(), um);
+        result->coeff = K->from_int(result_coeff);
+      }
+      break;
+    }
+    if (f == NULL) 
+    {
+      // mult each term of g by n:
+      for ( ;g!=0; g=g->next)
+      {
+        result->next = R->new_term();
+        result = result->next;
+        result->next = 0;
+        M->copy(g->monom, result->monom);
+        mpz_mul(result_coeff, g->coeff.get_mpz(), um);
+        result->coeff = K->from_int(result_coeff);
+      }
+      break;
+    }
+    switch (M->compare(f->monom, g->monom)) {
       case -1:
 	result->next = R->new_term();
 	result = result->next;
@@ -103,15 +99,97 @@ ring_elem ChineseRemainder::combine(const PolyRing *R, ring_elem ff, ring_elem g
 	result->next = t;
 	result = t;
 	break;
-      }
     }
-  
+  }
+
+  mpz_clear(result_coeff);
+  result->next = 0;
+  return head.next;
+}
+
+ring_elem ChineseRemainder::CRA(const PolyRing *R, ring_elem ff, ring_elem gg, mpz_t m, mpz_t n)
+{
+  // compute the multipliers
+  mpz_t um, vn, mn;
+  mpz_init(um);
+  mpz_init(vn);
+  mpz_init(mn);
+  computeMultipliers(m, n, um, vn, mn);  
+  // compute the chinese remainder with precomputed multipliers
+  ring_elem result = CRA(R, ff, gg, um, vn, mn);
   mpz_clear(um);
   mpz_clear(vn);
   mpz_clear(mn);
-  mpz_clear(result_coeff);
+  return result;
+}
+
+vec ChineseRemainder::CRA(const PolyRing *R, vec f, vec g, mpz_t um, mpz_t vn, mpz_t mn)
+{  
+  vecterm head;
+  vec result = &head;
+  
+  while (1)
+  {
+    if (g == NULL) 
+    {
+      // mult each term of f by n:
+      for ( ;f!=0; f=f->next)
+      {
+        result->next = R->new_vec();
+        result = result->next;
+        result->next = 0;
+        result->coeff=CRA(R,f->coeff,0,um,vn,mn);
+        result->comp=f->comp;
+      }
+      break;
+    }
+    if (f == NULL) 
+    {
+      // mult each term of g by n:
+      for ( ;g!=0; g=g->next)
+      {
+        result->next = R->new_vec();
+        result = result->next;
+        result->next = 0;
+        result->coeff=CRA(R,0,g->coeff,um,vn,mn);
+        result->comp=g->comp;      
+      }
+      break;
+    }
+    if (f->comp < g->comp)
+    {
+	result->next = R->new_vec();
+	result = result->next;
+	result->next = 0;
+        result->coeff=CRA(R,0,g->coeff,um,vn,mn);
+        result->comp=g->comp; 	
+        g = g->next;
+    } else if (f->comp > g->comp)
+    {
+      result->next = R->new_vec();
+      result = result->next;
+      result->next = 0;
+      result->coeff=CRA(R,f->coeff,0,um,vn,mn);
+      result->comp=f->comp; 	
+      f = f->next;
+    } else 
+    {
+      result->next = R->new_vec();
+      result = result->next;
+      result->next = 0;
+      result->coeff=CRA(R,f->coeff,g->coeff,um,vn,mn);
+      result->comp=f->comp; 	
+      f = f->next;
+      g = g->next;
+    }
+  }
+  result->next = 0;
   return head.next;
 }
+
+
+
+
 
 const RingElement * rawRingElementCRA(const RingElement *f, 
 				      const RingElement *g,
@@ -147,7 +225,7 @@ const RingElement * rawRingElementCRA(const RingElement *f,
 	{
 	  ring_elem rf = f->get_value();
 	  ring_elem rg = g->get_value();
-	  ring_elem result = ChineseRemainder::combine(P,rf,rg,m,n);
+	  ring_elem result = ChineseRemainder::CRA(P,rf,rg,m,n);
 	  return RingElement::make_raw(Rf, result);
 	}
       else
@@ -165,12 +243,81 @@ const Matrix * rawMatrixCRA(const Matrix *f,
 			    mpz_t m,
 			    mpz_t n)
 {
+  
+  const Ring *R = f->get_ring();
+
+  // Error handling:
+  if (f->get_ring() != g->get_ring())
+  {
+    ERROR("matrtices have different base rings");
+    return 0;
+  }
+  if (f->rows()->rank() != g->rows()->rank() 
+      || f->cols()->rank() != g->cols()->rank())
+  {
+    ERROR("matrixces have different shapes");
+    return 0;
+  }
+  
   // Assumption: f and g are either matrices over ZZ, or over a polynomial ring whose coeff
   // ring is ZZ.  The output is a matrix in the same ring.
-  ERROR("not written yet, d'oh");
+  
+  mpz_t um, vn, mn;
+  mpz_init(um);
+  mpz_init(vn);
+  mpz_init(mn);
+  ChineseRemainder::computeMultipliers(m, n, um, vn, mn);
+  mpz_t result_coeff;
+  mpz_init(result_coeff);
+  
+  //const Monoid *M = R->getMonoid();
+  //const Ring *K = R->getCoefficientRing();
+  
   return 0;
 }
 
+#if 0
+Matrix *Matrix::operator+(const Matrix &m) const
+{
+  if (get_ring() != m.get_ring())
+  {
+    ERROR("matrices have different base rings");
+    return 0;
+  }
+  if (rows()->rank() != m.rows()->rank()
+      || cols()->rank() != m.cols()->rank())
+  {
+    ERROR("matrices have different shapes");
+    return 0;
+  }
+  
+  const Ring *R = get_ring();
+  const FreeModule *F = rows();
+  const FreeModule *G = cols();
+  const int *deg;
+  
+  if (!rows()->is_equal(m.rows()))
+    F = R->make_FreeModule(n_rows());
+  
+  if (!cols()->is_equal(m.cols()))
+    G = R->make_FreeModule(n_cols());
+  
+  if (EQ == degree_monoid()->compare(degree_shift(), m.degree_shift()))
+    deg = degree_shift();
+  else
+    deg = degree_monoid()->make_one();
+  
+  MatrixConstructor mat(F,G,deg);
+  for (int i=0; i<n_cols(); i++)
+  {
+    vec v = R->copy_vec(elem(i));
+    vec w = R->copy_vec(m[i]);
+    R->add_vec_to(v,w);
+    mat.set_column(i, v);
+  }
+  return mat.to_matrix();
+}
+#endif
 
 
 ring_elem ChineseRemainder::reconstruct(const PolynomialRing *RQ, const Ring *R, ring_elem f, mpz_t m)
--- M2/Macaulay2/e/cra.hpp
+++ M2/Macaulay2/e/cra.hpp
@@ -20,11 +20,23 @@ public:
   static bool computeMultipliers(mpz_t m, mpz_t n, mpz_t result_um, mpz_t result_vn, mpz_t result_mn);
   // computes the multipliers um, vn, mn used in CRA0
 
-  static void CRA(mpz_t a, mpz_t b, mpz_t m, mpz_t n, mpz_t result);
-  // computes the unique integer 'result' < m*n s.t.
-  // result == a mod m, and == b mod n.
+  static ring_elem CRA(const PolyRing *R, const ring_elem f, const ring_elem g, mpz_t um, mpz_t vn, mpz_t mn);
+  // does the vector combination without error checking and with precomputed multipliers
+  
+  static vec CRA(const PolyRing *R, vec f, vec g, mpz_t um, mpz_t vn, mpz_t mn);
+  // does the vector combination without error checking and with precomputed multipliers
+  
+  static Matrix * CRA(const Matrix *f, const Matrix *g, mpz_t um, mpz_t vn, mpz_t mn);
+  // does the matrix combination without error checking and with precomputed multipliers
+  
+  static RingElement * CRA(const RingElement *f, const RingElement *g,  mpz_t um, mpz_t vn, mpz_t mn);
+  // does the ring element combination without error checking and with precomputed multipliers
 
-  static ring_elem combine(const PolyRing *R, ring_elem f, ring_elem g, mpz_t m, mpz_t n);
+  
+
+
+
+  static ring_elem CRA(const PolyRing *R, ring_elem f, ring_elem g, mpz_t m, mpz_t n);
   // Assumption: f and g are in a poly ring whose coeff ring is ZZ
 
   static ring_elem reconstruct(const PolynomialRing *RQ, const Ring *R, ring_elem f, mpz_t m);
