41d0fe2435c35d6b3767877fe64da3821fbdadb6
Daniel Grayson <dan@math.uiuc.edu>
2012-07-10 02:11:23 +0000

    increase stack size for each secondary thread from 0.5 MB to 8 MB
    git-svn-id: svn://svn.macaulay2.com/Macaulay2/trunk@14765 a8012f80-5f13-0410-8dc0-eb77bd940c0d

--- M2/Macaulay2/system/supervisor.cpp
+++ M2/Macaulay2/system/supervisor.cpp
@@ -382,7 +382,16 @@ SupervisorThread::SupervisorThread(int localThreadId):m_KeepRunning(true),m_Loca
 }
 void SupervisorThread::start()
 {
-  if(pthread_create(&m_ThreadId,NULL,SupervisorThread::threadEntryPoint,this))
+  const size_t min_stackSize = 8 * 1024 * 1024;
+  size_t stackSize = 0;
+  pthread_attr_t stackSizeAttribute;
+  if (pthread_attr_init(&stackSizeAttribute)) 
+    abort();
+  if (pthread_attr_getstacksize(&stackSizeAttribute, &stackSize))
+    abort();
+  if (stackSize < min_stackSize && pthread_attr_setstacksize (&stackSizeAttribute, min_stackSize))
+    abort();
+  if (pthread_create(&m_ThreadId,&stackSizeAttribute,SupervisorThread::threadEntryPoint,this))
     abort();
 }
 void SupervisorThread::threadEntryPoint()
