0f637ae20b24309c05be912f41a65f9781a452ec
Mike Stillman <mikestillman1@gmail.com>
2012-12-18 14:56:51 -0500

    oops: forgot to add interrupted.cpp

---
+++ M2/Macaulay2/e/interrupted.cpp
@@ -0,0 +1,8 @@
+#include "../system/supervisor.hpp"
+#include "../system/supervisorinterface.h"
+#define interrupted() test_Field(THREADLOCAL(interrupts_interruptedFlag,struct atomic_field))
+
+bool system_interrupted()
+{
+    return interrupted();
+}
