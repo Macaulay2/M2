b32470678b73634b822a09b706f3a5be9782a680
Mike Stillman <mikestillman1@gmail.com>
2012-12-26 08:58:10 -0500

    Added infrastructure so that e/unit-tests can test low level code in e dir.
    This is not yet complete.  In order to not need to link in everything from d
    and libfac, etc, some replacement code has been written (or will be, in the
    next commit(s)).  The most important is that global 'new' and 'delete'
    replacements from newdelete.cpp are NOT linked in.

---
+++ M2/Macaulay2/e/unit-tests/M2-cpp-replacement.cpp
@@ -0,0 +1,15 @@
+bool system_interrupted() { return false; }
+
+void factory_setup_1()
+{
+}
+
+class RingElement;
+
+const RingElement /* or null */ *rawExtendedGCDRingElement(const RingElement *f, 
+                                                           const RingElement *g, 
+                                                           const RingElement **A, 
+                                                           const RingElement **B)
+{
+    return 0;
+}
--- M2/Macaulay2/e/unit-tests/M2-replacement.c
+++ M2/Macaulay2/e/unit-tests/M2-replacement.c
@@ -11,6 +11,45 @@ M2_string M2_tostringn(char *s, int n)
     return p;
 }
 
+typedef struct RingElementrec *RingElement;
+
+  const RingElement /* or null */ *rawGCDRingElement(
+                                             const RingElement *f, const RingElement *g,
+                                             const RingElement *mipo, M2_bool inExtension
+                                             )
+{
+ /* connect to rawGCD */
+    return 0;
+}
+  const RingElement /* or null */ *rawExtendedGCDRingElement(
+                                                     const RingElement *f, const RingElement *g,
+                                                     const RingElement **A, const RingElement **B
+                                                     )
+{
+    /* connected to rawExtendedGCD */
+    return 0;
+}
+
+char * M2_tocharstar(M2_string s)
+{
+}
+
+M2_arrayint M2_makearrayint(int len)
+{
+}
+
+M2_string M2_join(M2_string s, M2_string t)
+{
+}
+M2_string M2_tostring(M2_constcharstarOrNull s)
+{
+}
+
+M2_string (*gmp_tonetCCparenpointer)(gmp_CC);
+M2_string (*gmp_tonetCCpointer)(gmp_CC);
+M2_string (*gmp_tostringRRpointer)(__mpfr_struct *);
+
+
 char newline[] = "\n";
 
 int M2_gbTrace = 0;
