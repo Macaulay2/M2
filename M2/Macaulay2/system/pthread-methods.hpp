f8add3ac86da9a668f7bd748ef7a454154c3951a
Daniel R. Grayson <dan@math.uiuc.edu>
2013-05-10 11:07:40 -0500

    mingw64 port: ntl, system

--- M2/Macaulay2/system/pthread-methods.hpp
+++ M2/Macaulay2/system/pthread-methods.hpp
@@ -1,9 +1,9 @@
 #ifdef __MINGW32__
   #define clearThread(t) t.p = NULL, t.x = 0
-  int operator==(pthread_t t, int zero) { return t.p == (void *)zero; }
-  int operator==(pthread_t t, pthread_t u) { return t.p == u.p && t.x == u.x; }
-  int operator!=(pthread_t t, pthread_t u) { return t.p != u.p || t.x != u.x; }
-  int operator< (pthread_t t, pthread_t u) { return t.p  < u.p || t.p == u.p && t.x < u.x; }
+  static inline int operator==(pthread_t t, int zero) { return t.p == (void *)zero; }
+  static inline int operator==(pthread_t t, pthread_t u) { return t.p == u.p && t.x == u.x; }
+  static inline int operator!=(pthread_t t, pthread_t u) { return t.p != u.p || t.x != u.x; }
+  static inline int operator< (pthread_t t, pthread_t u) { return t.p  < u.p || t.p == u.p && t.x < u.x; }
 #else
   #define clearThread(t) t = NULL
 #endif
