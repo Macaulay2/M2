bbfabd4552c2773f75d5c74d0ade3ac3159a1f23
Anton Leykin <antonleykin@gmail.com>
2013-10-01 10:32:14 -0400

    new files added to the previous commit

---
+++ M2/Macaulay2/e/aring-CC.cpp
@@ -0,0 +1,40 @@
+#include "aring-CC.hpp"
+
+namespace M2 {
+
+  void ARingCC::text_out(buffer &o) const
+  {
+    o << "ACC_53";
+  }
+
+  void ARingCC::elem_text_out(buffer &o,
+                              ElementType &ap,
+                              bool p_one,
+                              bool p_plus,
+                              bool p_parens) const
+  {
+    gmp_CC_struct g;
+    mpfr_init(g.re);
+    mpfr_init(g.im);
+    mpfr_set_d(g.re, ap.re, GMP_RNDN);
+    mpfr_set_d(g.im, ap.im, GMP_RNDN);
+    M2_string s = p_parens ? 
+      (*gmp_tonetCCparenpointer)(&g) : (*gmp_tonetCCpointer)(&g);
+    mpfr_clear(g.im);
+    mpfr_clear(g.re);
+
+    bool prepend_plus = p_plus && (s->array[0] != '-');
+    bool strip_last = !p_one && (
+				 (s->len == 1 && s->array[0] == '1')
+				 || (s->len == 2 && s->array[1] == '1' && s->array[0] == '-'));
+    
+    if (prepend_plus)
+      o << "+";
+    if (strip_last)
+      o.put(s->array, s->len-1);
+    else
+      o.put(s->array, s->len);
+    
+  }
+  
+}
