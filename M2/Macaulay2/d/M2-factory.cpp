b29e201e1b738c7e3d444e89012c9bd8a7621438
Daniel R. Grayson <dan@math.uiuc.edu>
2014-07-30 13:28:27 -0400

    max_align_t problem, part 2

--- M2/Macaulay2/d/M2-factory.cpp
+++ M2/Macaulay2/d/M2-factory.cpp
@@ -1,4 +1,8 @@
 #include <M2/config.h>
+#ifdef HAVE_STDDEF_H
+  /* this prevents a problem in Mac OS X, where 'cstddef' is loaded before 'stddef.h', and it causes a problem */
+  #include <stddef.h>
+#endif
 #include <factory/factory.h>		// to get definition of factoryseed()
 
 // extern "C" void actors5_setFactorySeed(int s) { 
