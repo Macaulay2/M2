2d78be6439f432b96d074d25595aa2e0b779b2a2
Mike Stillman <mikestillman1@gmail.com>
2015-03-13 12:42:54 -0400

    Added abstract functions, but only a little code so far.

--- M2/Macaulay2/e/NCAlgebra.cpp
+++ M2/Macaulay2/e/NCAlgebra.cpp
@@ -1,12 +1,13 @@
 #include "NCAlgebra.hpp"
 #include <vector>
 #include <string>
+#include <iostream>
+
 NCFreeAlgebra* NCFreeAlgebra::create(const Ring* K,
                       M2_ArrayString names)
 {
   M2_ASSERT(K != nullptr);
-  //return new NCFreeAlgebra(K, names);
-  return nullptr;
+  return new NCFreeAlgebra(K, names);
 }
 
 NCFreeAlgebra::NCFreeAlgebra(const Ring* K,
@@ -16,6 +17,126 @@ NCFreeAlgebra::NCFreeAlgebra(const Ring* K,
   for (auto i=0; i<names->len; i++)
     mVariableNames.push_back(std::string(names->array[i]->array, names->array[i]->len));
 }
+
+void NCFreeAlgebra::text_out(buffer &o) const
+{
+  mCoefficientRing.text_out(o);
+  o << "{";
+  for (int i=0; i<mVariableNames.size(); i++)
+    {
+      if (i > 0) o << ",";
+      o << mVariableNames[i];
+    }
+  o << "}";
+}
+unsigned int NCFreeAlgebra::computeHashValue(const ring_elem a) const
+{
+}
+
+ring_elem NCFreeAlgebra::from_long(long n) const
+{
+}
+
+ring_elem NCFreeAlgebra::from_int(mpz_ptr n) const
+{
+}
+
+ring_elem NCFreeAlgebra::from_rational(mpq_ptr q) const
+{
+}
+
+ring_elem NCFreeAlgebra::var(int v) const
+{
+  std::cout << "called var with v = " << v << std::endl;
+  NCPolynomial* result = new NCPolynomial;
+  result->mCoefficients.push_back(mCoefficientRing.from_long(1));
+  result->mMonomials.push_back(1);
+  result->mMonomials.push_back(v);
+  result->mMonomials.push_back(-1);  
+  return reinterpret_cast<Nterm*>(result);
+}
+
+bool NCFreeAlgebra::promote(const Ring *R, const ring_elem f, ring_elem &result) const
+{
+}
+
+bool NCFreeAlgebra::lift(const Ring *R, const ring_elem f, ring_elem &result) const
+{
+}
+
+bool NCFreeAlgebra::is_unit(const ring_elem f) const
+{
+}
+
+bool NCFreeAlgebra::is_zero(const ring_elem f) const
+{
+}
+
+bool NCFreeAlgebra::is_equal(const ring_elem f, const ring_elem g) const
+{
+}
+
+int NCFreeAlgebra::compare_elems(const ring_elem f, const ring_elem g) const
+{
+}
+
+ring_elem NCFreeAlgebra::copy(const ring_elem f) const
+{
+}
+
+void NCFreeAlgebra::remove(ring_elem &f) const
+{
+}
+
+ring_elem NCFreeAlgebra::negate(const ring_elem f) const
+{
+}
+
+ring_elem NCFreeAlgebra::add(const ring_elem f, const ring_elem g) const
+{
+}
+
+ring_elem NCFreeAlgebra::subtract(const ring_elem f, const ring_elem g) const
+{
+}
+
+ring_elem NCFreeAlgebra::mult(const ring_elem f, const ring_elem g) const
+{
+}
+
+ring_elem NCFreeAlgebra::invert(const ring_elem f) const
+{
+}
+
+ring_elem NCFreeAlgebra::divide(const ring_elem f, const ring_elem g) const
+{
+}
+
+void NCFreeAlgebra::syzygy(const ring_elem a, const ring_elem b,
+                      ring_elem &x, ring_elem &y) const
+{
+}
+
+void NCFreeAlgebra::elem_text_out(buffer &o,
+                             const ring_elem ff,
+                             bool p_one,
+                             bool p_plus,
+                             bool p_parens) const
+{
+  const NCPolynomial* f = reinterpret_cast<const NCPolynomial*>(ff.poly_val);
+  o << "[";
+  for (int i=0; i<f->mMonomials.size(); i++)
+    {
+      if (i>0) o << " ";
+      o << f->mMonomials[i];
+    }
+  o << "]";
+}
+
+ring_elem NCFreeAlgebra::eval(const RingMap *map, const ring_elem f, int first_var) const
+{
+}
+
 
 // Local Variables:
 // compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
--- M2/Macaulay2/e/NCAlgebra.hpp
+++ M2/Macaulay2/e/NCAlgebra.hpp
@@ -12,6 +12,17 @@ class Ring;
 /**
  * \ingroup polynomialrings
  */
+struct NCPolynomial
+{
+  std::vector<ring_elem> mCoefficients;
+  std::vector<int> mMonomials;
+  // each monomial is of the form:
+  //  <degree> <var1> <var2> ... <varn> <-1>
+  // e.g. xyxy: 4 0 1 0 1 -1
+  //   xy23x: 25 0 1 1 ... 1 0 -1
+  // 2 monomials: xzx, xy
+  //   3 0 2 1 -1 2 0 1 -1
+};
 class NCFreeAlgebra : public Ring
 {
   NCFreeAlgebra(const Ring* K,
@@ -20,6 +31,35 @@ public:
   static NCFreeAlgebra* create(const Ring* K,
                                M2_ArrayString names);
 
+  virtual void text_out(buffer &o) const;
+  virtual unsigned int computeHashValue(const ring_elem a) const;
+  virtual ring_elem from_long(long n) const;
+  virtual ring_elem from_int(mpz_ptr n) const;
+  virtual ring_elem from_rational(mpq_ptr q) const;
+  virtual ring_elem var(int v) const;
+  virtual bool promote(const Ring *R, const ring_elem f, ring_elem &result) const;
+  virtual bool lift(const Ring *R, const ring_elem f, ring_elem &result) const;
+  virtual bool is_unit(const ring_elem f) const;
+  virtual bool is_zero(const ring_elem f) const;
+  virtual bool is_equal(const ring_elem f, const ring_elem g) const;
+  virtual int compare_elems(const ring_elem f, const ring_elem g) const;
+  virtual ring_elem copy(const ring_elem f) const;
+  virtual void remove(ring_elem &f) const;
+  virtual ring_elem negate(const ring_elem f) const;
+  virtual ring_elem add(const ring_elem f, const ring_elem g) const;
+  virtual ring_elem subtract(const ring_elem f, const ring_elem g) const;
+  virtual ring_elem mult(const ring_elem f, const ring_elem g) const;
+  virtual ring_elem invert(const ring_elem f) const;
+  virtual ring_elem divide(const ring_elem f, const ring_elem g) const;
+  virtual void syzygy(const ring_elem a, const ring_elem b,
+                      ring_elem &x, ring_elem &y) const;
+  virtual void elem_text_out(buffer &o,
+                             const ring_elem f,
+                             bool p_one,
+                             bool p_plus,
+                             bool p_parens) const;
+  virtual ring_elem eval(const RingMap *map, const ring_elem f, int first_var) const;
+  
 private:
   std::vector<std::string> mVariableNames;
   const Ring& mCoefficientRing;
