bc2ca87640cb2cabc16cc747c9d9c07e1ddc5c51
Mike Stillman <mikestillman1@gmail.com>
2015-08-31 22:26:00 -0400

    Changes to build targets

--- M2/Macaulay2/e/free-resolutions/test-moninfo.cpp
+++ M2/Macaulay2/e/free-resolutions/test-moninfo.cpp
@@ -1,5 +1,14 @@
 #include <gtest/gtest.h>
 
+#include "stdinc.hpp"
+#include "moninfo.hpp"
+
 TEST(testingTest, first) {
   EXPECT_EQ(4, 4);
+}
+
+TEST(moninfo, create) {
+  MonomialOrdering mo;
+  MonomialInfo M(4, mo);
+  M.show();
 }
--- M2/Macaulay2/e/free-resolutions/testMain.cpp
+++ M2/Macaulay2/e/free-resolutions/testMain.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+int M2_gbTrace = 0;
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
