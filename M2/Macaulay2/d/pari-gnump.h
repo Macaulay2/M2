fceb7578cc3c035f86091aa47505113b18f62c2e
Guillem Blanco <guillem@wise.io>
2016-05-24 02:40:54 -0700

    adding pari-gnump URL

--- M2/Macaulay2/d/pari-gnump.h
+++ M2/Macaulay2/d/pari-gnump.h
@@ -17,6 +17,10 @@ You should have received a copy of the GNU General Public License
 along with Pari-gnump.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+ * http://www.multiprecision.org/index.php?prog=pari-gnump
+ */
+
 #include <stdio.h>
 #include <limits.h>
 #include <assert.h>
