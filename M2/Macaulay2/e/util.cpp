c85988395aad71eef8bb915c7ed7a7cc1925e81d
Mike Stillman <mikestillman1@gmail.com>
2016-06-26 20:23:03 -0400

    Clean up the use of stdvector_to_M2_arrayint.  Now is done via a template.

--- M2/Macaulay2/e/util.cpp
+++ M2/Macaulay2/e/util.cpp
@@ -3,22 +3,6 @@
 #include "util.hpp"
 #include <string>
 
-M2_arrayint stdvector_to_M2_arrayint(std::vector<size_t> &v)
-{
-  M2_arrayint result = M2_makearrayint(static_cast<int>(v.size()));
-  for (size_t i = 0; i < v.size(); i++)
-    result->array[i] = static_cast<int>(v[i]);
-  return result;
-}
-
-M2_arrayint stdvector_to_M2_arrayint(std::vector<long> &v)
-{
-  M2_arrayint result = M2_makearrayint(static_cast<int>(v.size()));
-  for (size_t i = 0; i < v.size(); i++)
-    result->array[i] = static_cast<int>(v[i]);
-  return result;
-}
-
 void M2_ArrayString_to_stdvector(M2_ArrayString strs, std::vector<std::string> &result)
 {
   for (size_t i = 0; i< strs->len; i++)
--- M2/Macaulay2/e/util.hpp
+++ M2/Macaulay2/e/util.hpp
@@ -6,9 +6,18 @@
 #include <vector>
 #include "engine-includes.hpp"
 
-M2_arrayint stdvector_to_M2_arrayint(std::vector<size_t> &v);
-M2_arrayint stdvector_to_M2_arrayint(std::vector<long> &v);
+//M2_arrayint stdvector_to_M2_arrayint(std::vector<size_t> &v);
+//M2_arrayint stdvector_to_M2_arrayint(std::vector<long> &v);
 void M2_ArrayString_to_stdvector(M2_ArrayString strs, std::vector<std::string> &result);
+
+template<typename inttype>
+M2_arrayint stdvector_to_M2_arrayint(std::vector<inttype> &v)
+{
+  M2_arrayint result = M2_makearrayint(static_cast<int>(v.size()));
+  for (size_t i = 0; i < v.size(); i++)
+    result->array[i] = static_cast<int>(v[i]);
+  return result;
+}
 
 
 #endif
