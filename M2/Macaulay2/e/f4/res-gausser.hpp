8971e2e4944ae117d44b781308a013f794d12871
Mike Stillman <mikestillman1@gmail.com>
2017-02-08 22:35:14 +0100

    add virtual to most public functions in res-gausser.  Slows down program by perhaps 10%.

--- M2/Macaulay2/e/f4/res-gausser.hpp
+++ M2/Macaulay2/e/f4/res-gausser.hpp
@@ -38,7 +38,7 @@ class ResGausser
     return * reinterpret_cast<std::vector<FieldElement>* >(f.mValue);
   }
 public:
-  ~ResGausser() {}
+  virtual ~ResGausser() {}
 
   static ResGausser *newResGausser(const Ring* K1);
 
@@ -71,49 +71,49 @@ private:
   }
 
 public:
-  void pushBackOne(CoefficientVector& coeffs) const;
-  void pushBackMinusOne(CoefficientVector& coeffs) const;
-  void pushBackElement(CoefficientVector& coeffs, const CoefficientVector& take_from_here, size_t loc) const;
-  void pushBackNegatedElement(CoefficientVector& coeffs, const CoefficientVector& take_from_here, size_t loc) const;
+  virtual void pushBackOne(CoefficientVector& coeffs) const;
+  virtual void pushBackMinusOne(CoefficientVector& coeffs) const;
+  virtual void pushBackElement(CoefficientVector& coeffs, const CoefficientVector& take_from_here, size_t loc) const;
+  virtual void pushBackNegatedElement(CoefficientVector& coeffs, const CoefficientVector& take_from_here, size_t loc) const;
 
-  ring_elem to_ring_elem(const CoefficientVector& coeffs, size_t loc) const; // in res-f4-m2-interface.cpp
-  void from_ring_elem(CoefficientVector& result, ring_elem a) const; // appends to result.
+  virtual ring_elem to_ring_elem(const CoefficientVector& coeffs, size_t loc) const; // in res-f4-m2-interface.cpp
+  virtual void from_ring_elem(CoefficientVector& result, ring_elem a) const; // appends to result.
   
-  size_t size(CoefficientVector r) const {
+  virtual size_t size(CoefficientVector r) const {
     return coefficientVector(r).size();
   }
   
-  CoefficientVector allocateCoefficientVector(ComponentIndex nelems) const;
+  virtual CoefficientVector allocateCoefficientVector(ComponentIndex nelems) const;
   // create a row of 0's (over K).
 
-  CoefficientVector allocateCoefficientVector() const;
+  virtual CoefficientVector allocateCoefficientVector() const;
   // create an empty array
 
-  void clear(CoefficientVector r, ComponentIndex first, ComponentIndex last) const;
+  virtual void clear(CoefficientVector r, ComponentIndex first, ComponentIndex last) const;
   // set the elements in the range first..last to 0.
 
-  void deallocate(CoefficientVector r) const;
+  virtual void deallocate(CoefficientVector r) const;
 
-  ComponentIndex nextNonzero(CoefficientVector r, ComponentIndex first, ComponentIndex last) const;
+  virtual ComponentIndex nextNonzero(CoefficientVector r, ComponentIndex first, ComponentIndex last) const;
   // returns last+1 in the case when there are no non-zero elements left.
 
-  void fillFromSparse(CoefficientVector r,
+  virtual void fillFromSparse(CoefficientVector r,
                       ComponentIndex len,
                       CoefficientVector sparse,
                       ComponentIndex* comps) const;
   // Fills 'r' from 'sparse' (and 'comps')
 
-  void sparseCancel(CoefficientVector r,
-                    CoefficientVector sparse,
-                    ComponentIndex* comps,
-                    CoefficientVector result_loc
-                    ) const;
+  virtual void sparseCancel(CoefficientVector r,
+                            CoefficientVector sparse,
+                            ComponentIndex* comps,
+                            CoefficientVector result_loc
+                            ) const;
   // dense += c * sparse, where c is chosen to cancel column comps[0].
   // ASSUMPTION: the lead coeff of 'sparse' is 1 or -1 (in the field)
   // The value of c is recorded in result_c.
 
-  void debugDisplay(CoefficientVector r) const;
-  void debugDisplayRow(int ncolumns, const std::vector<int>& comps, CoefficientVector coeffs) const;
+  virtual void debugDisplay(CoefficientVector r) const;
+  virtual void debugDisplayRow(int ncolumns, const std::vector<int>& comps, CoefficientVector coeffs) const;
 };
 
 #endif
