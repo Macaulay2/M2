680d01306142a84a5dad8db35d5be68228b1cea8
Mike Stillman <mikestillman1@gmail.com>
2018-05-04 11:51:32 -0400

    create files that we will place arbitrary precision SVD into

---
+++ M2/Macaulay2/e/eigen.cpp
@@ -0,0 +1,16 @@
+#include "eigen.hpp"
+
+bool SVD(const LMatrixRRR *A,
+         LMatrixRRR *Sigma,
+         LMatrixRRR *U,
+         LMatrixRRR *VT)
+{
+  return false;
+}
+
+/*
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
+// indent-tabs-mode: nil
+// End:
+*/
---
+++ M2/Macaulay2/e/eigen.hpp
@@ -0,0 +1,20 @@
+#ifndef __eigen_hpp_
+#define __eigen_hpp_
+
+#include "dmat.hpp"
+using LMatrixRRR = DMat<M2::ARingRRR>;
+
+bool SVD(const LMatrixRRR *A,
+         LMatrixRRR *Sigma,
+         LMatrixRRR *U,
+         LMatrixRRR *VT);
+
+#endif
+
+/*
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
+// indent-tabs-mode: nil
+// End:
+*/
+
