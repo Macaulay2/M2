7a54a27d0acdd03d8a51c1744bf3c8790ae41407
Mike Stillman <mikestillman1@gmail.com>
2019-01-25 17:01:41 -0500

    first shot at NCGroebner class structure

--- M2/Macaulay2/e/NCGroebner.hpp
+++ M2/Macaulay2/e/NCGroebner.hpp
@@ -5,6 +5,83 @@
 
 class KnuthMorrisPratt; // defined in NCGroebner.cpp
 
+#if 0
+  class Triple
+  {
+  public:
+    size_t mGroebnerElement1Index; // will be suffix of this one
+    size_t mGroebnerElement2Index; // and a prefix of this one
+    size_t mOverlapLength; // length of the overlap
+  };
+
+  class MonomialLookup
+  {
+    // ConstMonomial m : m.cbegin(), m.cend(),  *i is the current variable.
+    // degree(m).
+    // ++i, --i.  Maybe not --i...
+    // for (auto v : m) {
+    //    v is each variables in turn from the first.
+    // }
+  public:
+    MonomialLookup() {}
+    ~MonomialLookup() {}
+    
+    void insert(ConstMonomial& mon);
+    // perhaps: remove() later...
+
+    auto subwords(ConstMonomial& word) -> std::vector<std::tuple<int,int,int>>; // which monomial, index into word, length of monomial.
+    //    void subwords(ConstMonomial& word, std::vector<std::tuple<int,int,int>>& matches);
+
+    auto leftOverlaps(ConstMonomial& word) -> std::vector<Triple>;
+    auto rightOverlaps(ConstMonomial& word) -> std::vector<Triple>;
+
+    bool isSubword(ConstMonomial& word);
+    bool isSuperword(ConstMonomial& word);
+
+    // iterator? const_iterator?
+  private:
+    size_t mMonomialCount;
+    MonomialPool mTipMonomials;
+    std::vector<int> mTipMonomials2;
+  };
+
+  class AugmentedTriple
+  {
+    int mDegree;
+    Triple mTriple;
+    // perhaps other info for determining order in Triples structure.
+  };
+
+  // Triples: the set of critical "pairs": 2 polynomials often have multiple critical pairs...
+  class Triples
+  {
+  public:
+    void insertTriple(AugmentedTriple t); // this might remove other Triple's or even this one!  "Early retirement"
+    AugmentedTriple removeMinimalTriple(); // this might also remove some triples, finally return one. "Late retirement"
+  };
+
+  class Reductor
+  {
+  public:
+    Reductor(Poly*); // also needs the ring information.
+
+    void subtract(ConstMonomial& left, Poly* gbElement, ConstMonomial& right, Coefficient c);
+
+    // returns true if leadMonom is set, returns false if entire poly represented is zero.
+    bool leadMonomial(ConstMonomial& leadMonom); 
+  };
+
+  class PolynomialConstructor
+  {
+  public:
+    PolynomialConstructor(); // input should be a ring.
+    appendTerm(Coefficient c, ConstMonomial& m);
+    appendMonomial(ConstMonomial& m);
+    Poly* value();
+  };
+  // during reduction: structure to keep track of what reductions we have done.
+#endif
+
 class NCGroebner
 {
 public:
@@ -37,16 +114,11 @@ private:
   const PolynomialAlgebra* mRing;
   const ConstPolyList mInput;
   int mTopComputedDegree;
-  
-  // KMP algorithm for subwords
-
-  // given a pair of monomials, find all the overlaps. (suffix of first = prefix of second, order matters)
-
-  // reduction algorithm
-
-  // structure to keep track of which pairs we have to do.
-
-  // during reduction: structure to keep track of what reductions we have done.
+#if 0
+  // chose one of these two, or use VECTOR.
+  std::vector<Poly*> mGroebner;
+  std::vector<Poly> mGroebner2;
+#endif
 };
 
 #endif
