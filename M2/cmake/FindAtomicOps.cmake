96eecd0a611710d8109db8d305b633b828c6236b
Mahrud Sayrafi <mahrud@berkeley.edu>
2020-05-16 18:31:59 -0500

    simplify the library dependencies

--- M2/cmake/FindAtomicOps.cmake
+++ M2/cmake/FindAtomicOps.cmake
@@ -2,25 +2,25 @@
 # https://github.com/ivmai/libatomic_ops
 #
 # This file sets up AtomicOps for CMake. Once done this will define
-#  ATOMIC_OPS_FOUND             - system has the libatomic_ops library
-#  ATOMIC_OPS_INCLUDE_DIR       - the libatomic_ops include directory
-#  ATOMIC_OPS_LIBRARY           - the libatomic_ops library
+#  ATOMICOPS_FOUND             - system has the libatomic_ops library
+#  ATOMICOPS_INCLUDE_DIR       - the libatomic_ops include directory
+#  ATOMICOPS_LIBRARY           - the libatomic_ops library
 #
 # Copyright (c) 2020, Mahrud Sayrafi, <mahrud@umn.edu>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 
-find_path(ATOMIC_OPS_INCLUDE_DIR NAMES atomic_ops.h
+find_path(ATOMICOPS_INCLUDE_DIR NAMES atomic_ops.h
   HINTS /usr/local/opt/libatomic_ops
   PATHS ${INCLUDE_INSTALL_DIR} ${CMAKE_INSTALL_PREFIX}/include
   )
 
-find_library(ATOMIC_OPS_LIBRARY NAMES atomic_ops
+find_library(ATOMICOPS_LIBRARY NAMES atomic_ops
   HINTS /usr/local/opt/libatomic_ops
   PATHS ${LIB_INSTALL_DIR} ${CMAKE_INSTALL_PREFIX}/lib
   )
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(AtomicOps DEFAULT_MSG ATOMIC_OPS_INCLUDE_DIR ATOMIC_OPS_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(AtomicOps DEFAULT_MSG ATOMICOPS_INCLUDE_DIR ATOMICOPS_LIBRARY)
 
-mark_as_advanced(ATOMIC_OPS_INCLUDE_DIR ATOMIC_OPS_LIBRARY)
+mark_as_advanced(ATOMICOPS_INCLUDE_DIR ATOMICOPS_LIBRARY)
