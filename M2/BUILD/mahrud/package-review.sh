3bcd7bb7842d8e1015d69e3812a223a5437f02fb
Mahrud Sayrafi <mahrud@berkeley.edu>
2020-07-16 23:53:08 -0400

    update package review script

--- M2/BUILD/mahrud/package-review.sh
+++ M2/BUILD/mahrud/package-review.sh
@@ -1,8 +1,8 @@
-owner=mahrud
+user=mahrud
 token=`cat ~/.github_token` # https://github.com/settings/tokens
 
 # List workflows:
-#curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$owner/M2/actions/workflows
+#curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Macaulay2/M2/actions/workflows
 
 if [[ -z "$1" ]];
 then package="FirstPackage"
@@ -20,7 +20,7 @@ echo package: $package
 echo     ref: $ref
 
 curl \
-  -u $owner:$token -X POST \
+  -u $user:$token -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -d "{\"inputs\":{\"package\":\"$package\"}, \"ref\":\"$ref\"}" \
-  https://api.github.com/repos/$owner/M2/actions/workflows/1877698/dispatches
+  https://api.github.com/repos/Macaulay2/M2/actions/workflows/1904671/dispatches
