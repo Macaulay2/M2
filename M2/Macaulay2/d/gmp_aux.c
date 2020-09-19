7ed22e9eccaf6c18c26c6c8e364808771c2d2c4e
Michael Burr <burr2@clemson.edu>
2020-09-18 20:16:55 -0400

    Introduced mpfi.h into code.  The current version compiles for me, but requires some additional checks

--- M2/Macaulay2/d/gmp_aux.c
+++ M2/Macaulay2/d/gmp_aux.c
@@ -21,14 +21,17 @@ int mpfr_hash(mpfr_srcptr x) {
   return 777 + h * 3737 + x->_mpfr_exp + 11 * x->_mpfr_sign;
 }
 
-int mpfi_hash(mpfr_srcptr x) { // Added for MPFI
+int mpfi_hash(mpfi_srcptr x) { // Really not sure if this is doing the right thing.
     int h = 0;
-    int n = (x->_mpfr_prec+mp_bits_per_limb-1)/mp_bits_per_limb;
+    int n_left = (x->left._mpfr_prec+mp_bits_per_limb-1)/mp_bits_per_limb;
+    int n_right = (x->right._mpfr_prec+mp_bits_per_limb-1)/mp_bits_per_limb;
     int i;
-    if (0 != mpfr_sgn(x))
-    for (i = 0; i<n; i++, h*=3737) h += x->_mpfr_d[i];
-    return 777 + h * 3737 + x->_mpfr_exp + 11 * x->_mpfr_sign;
-} // End added for MPFI
+    if (0 != mpfr_sgn(&x->left))
+    for (i = 0; i<n_left; i++, h*=3737) h += x->left._mpfr_d[i];
+    if (0 != mpfr_sgn(&x->right))
+    for (i = 0; i<n_right; i++, h*=3737) h += x->right._mpfr_d[i];
+    return 777 + h * 3737 + x->left._mpfr_exp + x->right._mpfr_exp + 11 * x->left._mpfr_sign + 11 * x->right._mpfr_sign;
+}
 
 /*
  Local Variables:
