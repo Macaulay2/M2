f64e9a6a067ff0a46fc91f49386b842df137de97
Anton Leykin <antonleykin@gmail.com>
2021-02-02 12:28:54 -0500

    hello world

--- M2/Macaulay2/bin/main.cpp
+++ M2/Macaulay2/bin/main.cpp
@@ -24,6 +24,7 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <mpi.h>
 
 /* ######################################################################### */
 
@@ -58,6 +59,29 @@ void* testFunc(ArgCell* p);
 
 int main(/* const */ int argc, /* const */ char *argv[], /* const */ char *env[])
 {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  // Print off a hello world message
+  std::cout << "Hello world from processor " << processor_name
+	    << ", rank " << world_rank
+	    << " out of " << world_size << " processors" << std::endl;
+
+  if (world_rank == 0) {
+    
   /* find the number of environment variables defined */
   int envc = -1;
   while (env[++envc] != NULL) { /* iterate over environ until you hit NULL */ }
@@ -108,8 +132,15 @@ int main(/* const */ int argc, /* const */ char *argv[], /* const */ char *env[]
     pushTask(interpTask);
     waitOnTask(interpTask);
   }
+  
+  } //end// if (world_rank == 0) 
+  std::cout << "Bye world from processor " << processor_name
+	    << ", rank " << world_rank
+	    << " out of " << world_size << " processors" << std::endl;
+  while (true);
+  MPI_Finalize();
   return 0;
-}
+} //end// main
 
 /* ######################################################################### */
 
