9c3fc87f511b4fafa7a94d114d1c36dab7d209cb
Doug Torrance <dtorrance@piedmont.edu>
2021-06-04 11:26:20 -0400

    Add logical xor
    Closes: #1800

--- M2/Macaulay2/d/binding.d
+++ M2/Macaulay2/d/binding.d
@@ -249,6 +249,8 @@ bumpPrecedence();
 bumpPrecedence();
      export orS := makeKeyword(binaryrightword("or"));
 bumpPrecedence();
+     export xorS := makeKeyword(binaryrightword("xor"));
+bumpPrecedence();
      export andS := makeKeyword(binaryrightword("and"));
 bumpPrecedence();
      export notS := makeKeyword(unaryword("not"));
@@ -469,7 +471,7 @@ export opsWithBinaryMethod := array(SymbolClosure)(
      LongDoubleRightArrowS, LongLongDoubleRightArrowS,
      LongDoubleLeftArrowS, LongLongDoubleLeftArrowS,
      ColonS, BarS, HatHatS, AmpersandS, DotDotS, DotDotLessS, MinusS, PlusS, PlusPlusS, StarStarS, StarS, BackslashBackslashS, DivideS, LeftDivideS, PercentS, SlashSlashS, AtS, 
-     AdjacentS, AtAtS, PowerS, UnderscoreS, PowerStarStarS, orS, andS);
+     AdjacentS, AtAtS, PowerS, UnderscoreS, PowerStarStarS, orS, andS, xorS);
 export opsWithUnaryMethod := array(SymbolClosure)(
      StarS, MinusS, PlusS, LessLessS, 
      LongDoubleLeftArrowS, LongLongDoubleLeftArrowS, 
