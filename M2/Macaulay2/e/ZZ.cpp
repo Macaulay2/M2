0be182565d0e77ba36315d4720bf9c9773b525a6
pzinn <pzinnjustin@gmail.com>
2021-11-17 17:06:44 +1100

    fix #2245

--- M2/Macaulay2/e/ZZ.cpp
+++ M2/Macaulay2/e/ZZ.cpp
@@ -292,7 +292,9 @@ ring_elem RingZZ::invert(const ring_elem f) const
 ring_elem RingZZ::divide(const ring_elem f, const ring_elem g) const
 {
   mpz_ptr result = new_elem();
-  mpz_fdiv_q(result, f.get_mpz(), g.get_mpz());
+  mpz_ptr rem = new_elem();
+  mpz_fdiv_qr(result, rem, f.get_mpz(), g.get_mpz());
+  if (mpz_sgn(rem)) ERROR("not divisible");
   mpz_reallocate_limbs(result);
   return ring_elem(result);
 }
