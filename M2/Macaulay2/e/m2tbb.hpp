db7630570307ec4a19f4ec54cef1420298587312
Mike Stillman <mikestillman1@gmail.com>
2022-02-25 09:50:09 -0500

    simplify header file include invocation

--- M2/Macaulay2/e/m2tbb.hpp
+++ M2/Macaulay2/e/m2tbb.hpp
@@ -4,6 +4,6 @@
 // The plan: All uses of TBB go through the following interface.
 
 #include <M2/config.h>                    // make sure WITH_TBB is set before including mtbb.hpp
-#include "mathicgb/mathicgb/mtbb.hpp"
+#include "mathicgb/mtbb.hpp"
 
 #endif
