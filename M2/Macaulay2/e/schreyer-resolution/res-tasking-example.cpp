b5daa45ac0a27f02fe179e68e35b6abd7f0e213e
moorewf <moorewf@wfu.edu>
2022-05-04 22:29:16 -0400

    Adding a mutex to clean up the garbled output.

--- M2/Macaulay2/e/schreyer-resolution/res-tasking-example.cpp
+++ M2/Macaulay2/e/schreyer-resolution/res-tasking-example.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <memory>
 #include <unistd.h>
+#include <mutex>
 
 using Node = tbb::flow::continue_node<tbb::flow::continue_msg>;
 using NodePtr = std::shared_ptr<Node>;
@@ -13,12 +14,16 @@ using NodePtr = std::shared_ptr<Node>;
 tbb::flow::graph G;
 std::vector<std::vector<NodePtr>> nodes; // nodes[lev][sldeg] is that particular node.
 
+// just for syncing the output in the nodes
+std::mutex myMutex;
+
 NodePtr createNode(tbb::flow::graph& G, int lev, int sldeg)
 {
   return std::make_shared<Node>(G,
                                 [lev, sldeg](const tbb::flow::continue_msg &msg)
                                 {
-                                  sleep(2);
+                                  sleep(1);
+				  std::lock_guard<std::mutex> guard(myMutex);
                                   std::cout << "computed lev=" << lev << " sldeg=" << sldeg << std::endl;
                                   return msg;
                                 });
@@ -33,6 +38,7 @@ void makeDependencyGraph(int nlevels, int nslanted_degrees)
     for (int sldeg=0; sldeg < nslanted_degrees; ++sldeg)
       nodes[lev][sldeg] = createNode(G, lev, sldeg);
 
+  // Add the edges
   for (int lev=1; lev<nlevels; ++lev)
     for (int sldeg=0; sldeg < nslanted_degrees; ++sldeg)
       {
@@ -44,16 +50,18 @@ void makeDependencyGraph(int nlevels, int nslanted_degrees)
 
 int main()
 {
-  std::cout << "hi there, we have " << tbb::info::default_concurrency()  << " thread(s) available" << std::endl;
-  makeDependencyGraph(5, 3);
+  std::cout << "Hi there, we have " << tbb::info::default_concurrency()
+            <<" thread(s) available." << std::endl;
+  const int nlevels = 5;
+  const int nslanted_degrees = 10;
+  makeDependencyGraph(nlevels,nslanted_degrees);
 
-  nodes[4][2]->try_put(tbb::flow::continue_msg());
-  //  nodes[2][2]->try_put(tbb::flow::continue_msg());
+  nodes[nlevels - 1][nslanted_degrees - 1]->try_put(tbb::flow::continue_msg());
 
   G.wait_for_all();
   
-  for (int lev=0; lev<5; ++lev)
-    for (int sldeg=0; sldeg < 3; ++sldeg)
+  for (int lev=0; lev<nlevels; ++lev)
+    for (int sldeg=0; sldeg < nslanted_degrees; ++sldeg)
       std::cout << "nodes[" << lev << "," << sldeg << "] = " << nodes[lev][sldeg] << std::endl;
 
   return 0;
