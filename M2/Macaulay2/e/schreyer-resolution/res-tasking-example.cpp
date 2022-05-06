5b18d43eecd7d1398ad14ac1976be3aae037f2e2
moorewf <moorewf@wfu.edu>
2022-05-06 10:22:26 -0400

    Randomized sleep time and started node struct.

--- M2/Macaulay2/e/schreyer-resolution/res-tasking-example.cpp
+++ M2/Macaulay2/e/schreyer-resolution/res-tasking-example.cpp
@@ -7,6 +7,9 @@
 #include <memory>
 #include <unistd.h>
 #include <mutex>
+#include <thread>
+#include <chrono>
+#include <random>
 
 using Node = tbb::flow::continue_node<tbb::flow::continue_msg>;
 using NodePtr = std::shared_ptr<Node>;
@@ -14,17 +17,36 @@ using NodePtr = std::shared_ptr<Node>;
 tbb::flow::graph G;
 std::vector<std::vector<NodePtr>> nodes; // nodes[lev][sldeg] is that particular node.
 
+struct OurNode {
+   int lev;
+   int sldeg;
+   int firstDependency;
+   int lastDependency;
+   int computationStatus;
+};
+
+//std::vector<Node> ourNodes;
+//std::vector<int> ourDependencies;
+
 // just for syncing the output in the nodes
 std::mutex myMutex;
+
+// randomizer
+std::random_device rd;
+std::mt19937 rng(rd());
+std::uniform_int_distribution<int> uni(250,1000);
 
 NodePtr createNode(tbb::flow::graph& G, int lev, int sldeg)
 {
   return std::make_shared<Node>(G,
                                 [lev, sldeg](const tbb::flow::continue_msg &msg)
                                 {
-                                  sleep(1);
+				  int sleepTime = uni(rng);
+                                  std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));;
 				  std::lock_guard<std::mutex> guard(myMutex);
-                                  std::cout << "computed lev=" << lev << " sldeg=" << sldeg << std::endl;
+                                  std::cout << "computed lev=" << lev << " sldeg="
+                                            << sldeg << " sum=" << lev + sldeg
+					    << " sleep time=" << sleepTime << std::endl;
                                   return msg;
                                 });
 }
@@ -39,12 +61,13 @@ void makeDependencyGraph(int nlevels, int nslanted_degrees)
       nodes[lev][sldeg] = createNode(G, lev, sldeg);
 
   // Add the edges
-  for (int lev=1; lev<nlevels; ++lev)
+  for (int lev=0; lev<nlevels; ++lev)
     for (int sldeg=0; sldeg < nslanted_degrees; ++sldeg)
       {
-        tbb::flow::make_edge(* nodes[lev][sldeg], * nodes[lev-1][sldeg]);
+        if (lev > 0)
+	  tbb::flow::make_edge(* nodes[lev-1][sldeg], * nodes[lev][sldeg]);
         if (sldeg > 0)
-          tbb::flow::make_edge(* nodes[lev][sldeg], * nodes[lev][sldeg-1]);
+          tbb::flow::make_edge(* nodes[lev][sldeg-1], * nodes[lev][sldeg]);
       }
 }
 
@@ -56,7 +79,7 @@ int main()
   const int nslanted_degrees = 10;
   makeDependencyGraph(nlevels,nslanted_degrees);
 
-  nodes[nlevels - 1][nslanted_degrees - 1]->try_put(tbb::flow::continue_msg());
+  nodes[0][0]->try_put(tbb::flow::continue_msg());
 
   G.wait_for_all();
   
