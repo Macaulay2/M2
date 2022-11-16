52c1fed405cd88e4fe01f657bc6bd562233d04e5
Doug Torrance <dtorrance@piedmont.edu>
2022-11-15 22:13:38 -0500

    Print error message when one occurs in ForeignFunctions callback

--- M2/Macaulay2/d/ffi.d
+++ M2/Macaulay2/d/ffi.d
@@ -593,6 +593,7 @@ ffiClosureFunction(cif:Pointer "ffi_cif *", ret:voidPointer,
     do Ccode(void, "memcpy(",
 	endianAdjust(ret, Ccode(voidPointer, "((ffi_cif *)", cif, ")->rtype")),
 	", ", ptr.v, ", ", cif, "->rtype->size)")
+    is err:Error do printErrorMessage(err)
     else nothing);
 
 ffiClosureFinalizer(ptr:voidPointer, closure:voidPointer):void := (
