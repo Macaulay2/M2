604680bf8181af05b3f7e331632debc0d0af5a6a
Frank Moore <frank@FranksMacStudio.dhcp.wfu.edu>
2023-04-19 10:11:02 -0400

    Working on interface design.

--- M2/Macaulay2/e/gb-f4/Basis.hpp
+++ M2/Macaulay2/e/gb-f4/Basis.hpp
@@ -12,31 +12,60 @@ private:
   std::vector<MonomialIndex> mMonomials;
 public:
   // creation (output iterator?)
-  // iteration (for a const one)
+  // iteration (for a const one) (similar to NC Poly)
   // access
 };
 
-/// Basis: keeping the Groebner basis (and original generators)
+// not a final choice of statuses
+enum class GBPolyStatus { Gen, MinGen, MinGB, NonMinGB, Retired };
+
+// What classes do we need?
+// Macaulay matrix creation
+
+class MacaulayMatrix
+{
+private:
+};
+
+class SPair
+{
+};
+
+class SPairTable
+{
+};
+
+struct Column
+{
+};
+
+struct Row
+{
+};
+
+// Basis: keeping the Groebner basis (and original generators)
 class Basis
 {
 private:
   const VectorArithmetic& mVectorArithmetic;
-  MonomialHashTable mHashTable; // this is where all the monomials are stored.
+  MonomialHashTable& mHashTable; // this is where all the monomials are stored.
 
-  std::vector<bool> mIsMinimal; // MinimalGen, MinimalGB, NonminimalGB, Retired
+  std::vector<GBPolyStatus> mIsMinimal; 
   std::vector<GBPolynomial> mPolynomials;
 public:
   Basis(const VectorArithmetic& VA) : mVectorArithmetic(VA) {}
   ~Basis() {}
 
   const VectorArithmetic& vectorArithmetic() const { return mVectorArithmetic; }
+  
   // What functionality do we need here?
+
+  // accessor functions
 };
 
 }  
-/// append_to_basis
+// append_to_basis
 void append_to_basis(newf4::Basis& B, const Matrix* M);
-
 auto basis_to_M2_matrix(const newf4::Basis & B, const FreeModule* F) -> Matrix*;
 auto basis_to_M2_mutable_matrix(const newf4::Basis & B, const FreeModule* F) -> MutableMatrix*;
   
