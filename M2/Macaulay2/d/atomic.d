0dbab0b142c885e0808862c49406869244bbd405
Doug Torrance <dtorrance@piedmont.edu>
2024-01-21 22:43:40 -0500

    Switch from AO_t -> atomic_int in atomic.d
    No longer need to include atomic_ops.h

--- M2/Macaulay2/d/atomic.d
+++ M2/Macaulay2/d/atomic.d
@@ -2,23 +2,26 @@ use arithmetic;
 declarations "
   #ifdef __cplusplus
     #include <atomic>
+    using std::atomic_fetch_add;
+    using std::atomic_int;
+    using std::atomic_load;
     using std::atomic_signal_fence;
+    using std::atomic_store;
     using std::memory_order_seq_cst;
   #else
     #include <stdatomic.h>
   #endif
 
-  #include <atomic_ops.h>
   #ifndef atomic_field_decl
   #define atomic_field_decl
   /* if you change this, change the copy in ../system/supervisor.hpp, too; better yet, remove it. */
   struct atomic_field {
-       AO_t field;
+       atomic_int field;
        };
 
-  #define load_Field(x) AO_load(&(x).field)
+  #define load_Field(x) atomic_load(&(x).field)
   #define test_Field(x) (load_Field(x) != 0)
-  #define store_Field(x,val) AO_store(&(x).field,val)
+  #define store_Field(x,val) atomic_store(&(x).field,val)
   #endif
   ";
 export atomicField := Type "struct atomic_field";
@@ -26,17 +29,17 @@ export atomicFieldPointer := Pointer "struct atomic_field *";
 export atomicFieldPointerOrNull := atomicFieldPointer or null;
 export address(x:atomicField) ::= Ccode(atomicFieldPointer,"(&(",x,"))");
 export contents(x:atomicFieldPointer) ::= Ccode(atomicField,"(*(",x,"))");
-export atomicInt := integerType "AO_t";
+export atomicInt := integerType "atomic_int";
 export load(x:atomicField) ::= Ccode(atomicInt, "load_Field(",x,")");
 export test(x:atomicField) ::= (load(x) != atomicInt(0));
 export store(x:atomicField,y:atomicInt) ::= Ccode(void, "store_Field(",x,",",y,")");
 export store(x:atomicFieldPointer,y:atomicInt) ::= store(contents(x),y);
 export store(x:atomicFieldPointer,y:bool) ::= store(contents(x),y);
 export store(x:atomicFieldPointer,y:int) ::= store(contents(x),y);
-export toAtomicInt(x:bool) ::= Ccode(atomicInt,"((AO_t)",x,")");
+export toAtomicInt(x:bool) ::= Ccode(atomicInt,"((atomic_int)",x,")");
 export store(x:atomicField,y:bool) ::= store(x,toAtomicInt(y));
 export store(x:atomicField,y:int) ::= store(x,atomicInt(y));
-export increment(x:atomicInt) ::= Ccode(atomicInt,"AO_fetch_and_add1(&(",x,"))");
+export increment(x:atomicInt) ::= Ccode(atomicInt,"atomic_fetch_add(&(",x,", 1))");
 export compilerBarrier() ::= Ccode(void,"atomic_signal_fence(memory_order_seq_cst)");
 
 -- here is the way to declare one of these:
