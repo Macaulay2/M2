45a484e112b652e59a2cf67f8bf11abb6fedbe52
Doug Torrance <dtorrance@piedmont.edu>
2024-01-22 06:11:43 -0500

    Move definition of atomic_field struct to its own header
    Used in both d and system directories

--- M2/Macaulay2/d/atomic.d
+++ M2/Macaulay2/d/atomic.d
@@ -1,29 +1,18 @@
 use arithmetic;
+
 declarations "
+  #include \"M2/atomic-field.h\"
   #ifdef __cplusplus
     #include <atomic>
     using std::atomic_fetch_add;
     using std::atomic_int;
-    using std::atomic_load;
     using std::atomic_signal_fence;
-    using std::atomic_store;
     using std::memory_order_seq_cst;
   #else
     #include <stdatomic.h>
   #endif
-
-  #ifndef atomic_field_decl
-  #define atomic_field_decl
-  /* if you change this, change the copy in ../system/supervisor.hpp, too; better yet, remove it. */
-  struct atomic_field {
-       atomic_int field;
-       };
-
-  #define load_Field(x) atomic_load(&(x).field)
-  #define test_Field(x) (load_Field(x) != 0)
-  #define store_Field(x,val) atomic_store(&(x).field,val)
-  #endif
   ";
+
 export atomicField := Type "struct atomic_field";
 export atomicFieldPointer := Pointer "struct atomic_field *";
 export atomicFieldPointerOrNull := atomicFieldPointer or null;
