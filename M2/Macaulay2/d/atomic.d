cb2650638bc466b5e6ed7fcdc9dcefcb3627c29c
Doug Torrance <dtorrance@piedmont.edu>
2024-01-23 08:06:13 -0500

    Remove cruft in atomic.d
    There were lots of things we were never actually using

--- M2/Macaulay2/d/atomic.d
+++ M2/Macaulay2/d/atomic.d
@@ -4,9 +4,6 @@ declarations "
   #include \"M2/atomic-field.h\"
   #ifdef __cplusplus
     #include <atomic>
-    using std::atomic_fetch_add;
-    using std::atomic_int;
-    using std::atomic_load;
     using std::atomic_signal_fence;
     using std::memory_order_seq_cst;
   #else
@@ -15,36 +12,10 @@ declarations "
   ";
 
 export atomicField := Type "struct atomic_field";
-export atomicFieldPointer := Pointer "struct atomic_field *";
-export atomicFieldPointerOrNull := atomicFieldPointer or null;
-export address(x:atomicField) ::= Ccode(atomicFieldPointer,"(&(",x,"))");
-export contents(x:atomicFieldPointer) ::= Ccode(atomicField,"(*(",x,"))");
-export atomicInt := integerType "atomic_int";
-export load(x:atomicInt) ::= Ccode(int, "atomic_load(&", x, ")");
 export load(x:atomicField) ::= Ccode(int, "load_Field(",x,")");
 export test(x:atomicField) ::= (load(x) != 0);
-export store(x:atomicField,y:atomicInt) ::= store(x, load(y));
-export store(x:atomicFieldPointer,y:atomicInt) ::= store(contents(x), load(y));
-export store(x:atomicFieldPointer,y:bool) ::= store(contents(x),y);
-export store(x:atomicFieldPointer,y:int) ::= store(contents(x),y);
-export toAtomicInt(x:bool) ::= Ccode(atomicInt,"((atomic_int)",x,")");
-export store(x:atomicField,y:bool) ::= store(x, Ccode(int, y));
-export store(x:atomicField,y:int) ::= Ccode(void, "store_Field(",x,",",y,")");
-export increment(x:atomicInt) ::= Ccode(atomicInt,"atomic_fetch_add(&(",x,", 1))");
+export store(x:atomicField,y:bool) ::= Ccode(void, "store_Field(",x,",",y,")");
 export compilerBarrier() ::= Ccode(void,"atomic_signal_fence(memory_order_seq_cst)");
-
--- here is the way to declare one of these:
--- header "struct atomic_field x;";
--- import x:atomicField;
--- y := load(x);
--- store(x,1);
-
--- j := atomicInt(0);
--- increment(j);
--- increment(j);
--- k := int(j);
--- ar := iar.j;
-
 
 -- Local Variables:
 -- compile-command: "make -C $M2BUILDDIR/Macaulay2/d atomic.o DEPENDS=no "
