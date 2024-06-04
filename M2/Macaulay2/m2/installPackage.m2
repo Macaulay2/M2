Macaulay2/M2

commit 1bc2bba70e8ca206814a7ce5b66477aaaaba33d0

Update html navigation bar appearance
Mimics the appearance of Sphinx-generated documentation, and also adds
a <span> tag that will contain the drop-down version selection menu on
the website.

M2/Macaulay2/m2/installPackage.m2
@@ -240,8 +240,8 @@ BACKWARD  := tag -> if PREV#?tag then BACKWARD0 PREV#tag else if UP#?tag then UP
 topNodeButton  := (htmlDirectory, topFileName)   -> HREF {htmlDirectory | topFileName,   "top" };
 indexButton    := (htmlDirectory, indexFileName) -> HREF {htmlDirectory | indexFileName, "index"};
 tocButton      := (htmlDirectory, tocFileName)   -> HREF {htmlDirectory | tocFileName,   "toc"};
-pkgButton      := TO2 {"packages provided with Macaulay2", "packages"};
-homeButton     := HREF {"http://macaulay2.com/", "Macaulay2 website"};
+pkgButton      := TO2 {"packages provided with Macaulay2", "Packages"};
+homeButton     := HREF {"https://macaulay2.com/", "Macaulay2"};
 
 nextButton     := tag -> if NEXT#?tag then HREF { htmlFilename NEXT#tag, "next" }     else "next"
 prevButton     := tag -> if PREV#?tag then HREF { htmlFilename PREV#tag, "previous" } else "previous"
@@ -250,10 +250,34 @@ backwardButton := tag -> ( b := BACKWARD tag; if b =!= null then HREF { htmlFile
 upButton       := tag -> if   UP#?tag then HREF { htmlFilename   UP#tag, "up" } else "up"
 topButton      := tag -> if tag =!= topDocumentTag then topNodeButton(htmlDirectory, topFileName) else "top"
 
+upAncestors := tag -> reverse(
+    n := 0; prepend(tag, while UP#?tag and n < 20 list (n = n+1; tag = UP#tag)))
+
 -- TODO: revamp this using Bootstrap
-buttonBar := tag -> TABLE { "class" => "buttons", TR { TD { DIV splice between_" | " {
-		nextButton tag, prevButton tag, forwardButton tag, backwardButton tag, upButton tag, topButton tag,
-		indexButton(htmlDirectory, indexFileName), tocButton(htmlDirectory, tocFileName), pkgButton, homeButton}}}}
+buttonBar := tag -> DIV {
+    "id" => "buttons",
+    DIV {
+	homeButton, " ", SPAN {"id" => "version-select-container", ""},
+	" » ", TO2 {"Macaulay2Doc", "Documentation "}, " ",
+	BR {},
+	pkgButton, " » ",
+	SPAN if UP#?tag
+	then between(" > ", apply(upAncestors tag, i -> TO i))
+	else (TO topDocumentTag, " :: ",
+	    if tag === "index"
+	    then HREF {htmlDirectory | indexFileName, "Index"}
+	    else if tag === "toc"
+	    then HREF {htmlDirectory | tocFileName, "Table of Contents"}
+	    else TO tag)},
+    prepend("class" => "right",
+	DIV splice between_" | " {
+	    nextButton tag,
+	    prevButton tag,
+	    forwardButton tag,
+	    backwardButton tag,
+	    upButton tag,
+	    indexButton(htmlDirectory, indexFileName),
+	    tocButton(htmlDirectory, tocFileName)})}
 
 -----------------------------------------------------------------------------
 -- makePackageIndex
@@ -377,9 +401,6 @@ installInfo := (pkg, installPrefix, installLayout, verboseLog) -> (
 -- install HTML documentation for package
 -----------------------------------------------------------------------------
 
-upAncestors := tag -> reverse(
-    n := 0; prepend(tag, while UP#?tag and n < 20 list (n = n+1; tag = UP#tag)))
-
 makeSortedIndex := (nodes, verboseLog) -> (
      numAnchorsMade = 0;
      fn := installPrefix | htmlDirectory | indexFileName;
@@ -388,7 +409,7 @@ makeSortedIndex := (nodes, verboseLog) -> (
      fn << html validate HTML {
 	  defaultHEAD title,
 	  BODY nonnull {
-	       DIV { topNodeButton(htmlDirectory, topFileName), " | ", tocButton(htmlDirectory, tocFileName), " | ", pkgButton, " | ", homeButton },
+	       buttonBar "index",
 	       HR{},
 	       HEADER1 title,
 	       DIV between(LITERAL "&nbsp;&nbsp;&nbsp;",apply(alpha, c -> HREF {"#"|c, c})),
@@ -406,7 +427,7 @@ makeTableOfContents := (pkg, verboseLog) -> (
      fn << html validate HTML {
 	  defaultHEAD title,
 	  BODY {
-	       DIV { topNodeButton(htmlDirectory, topFileName), " | ", indexButton(htmlDirectory, indexFileName), " | ", pkgButton, " | ", homeButton },
+	       buttonBar "toc",
 	       HR{},
 	       HEADER1 title,
 	       toDoc unbag pkg#"table of contents"
@@ -441,9 +462,6 @@ installHTML := (pkg, installPrefix, installLayout, verboseLog, rawDocumentationC
 		defaultHEAD {fkey, commentize headline fkey},
 		BODY {
 		    buttonBar tag,
-		    if UP#?tag
-		    then DIV between(" > ", apply(upAncestors tag, i -> TO i))
-		    else DIV (TO topDocumentTag, " :: ", TO tag),
 		    HR{},
 		    fetchProcessedDocumentation(pkg, fkey)
 		    }

M2/Macaulay2/packages/Style/doc.css
@@ -96,6 +96,10 @@ kbd {
   box-shadow: inset 0 -1px 0;
 }
 
+div#buttons {display: flex;}
+div#buttons div {flex-grow: 1;}
+.right {text-align: right;}
+
 /*
 
  Local Variables:

commit d94073fe95aaea202ae06ba124e24655cd582a1b

Add missing "website" to main viewHelp page
This was broken in 1bc2bba when the navigation buttons were modified.

M2/Macaulay2/m2/installPackage.m2
@@ -318,7 +318,7 @@ makePackageIndex List := path -> (
 	BODY {
 	    PARA {"This is the directory for Macaulay2 and its packages. Bookmark this page for future reference,
 		or run the ", TT "viewHelp", " command in Macaulay2 to open up your browser on this page.
-		See the ", homeButton, " for the latest version."},
+		See the ", homeButton, " website for the latest version."},
 	    HEADER3 "Documentation",
 	    UL nonnull splice {
 		if prefixDirectory =!= null then (