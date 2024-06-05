7cbba2304b314f355e5855d0f3ea0fe31c5635e9
Doug Torrance <dtorrance@piedmont.edu>
2024-06-04 23:23:03 -0400

    Add basic conversion to/from flint's ball arithmetic types
    We require FLINT 3 (and thus Factory 4.4.0) in the cmake build.

--- M2/include/M2/config.h.cmake
+++ M2/include/M2/config.h.cmake
@@ -57,6 +57,9 @@
 /* whether we have the flint header file nmod.h */
 #cmakedefine HAVE_FLINT_NMOD_H 1
 
+/* whether we have the flint header file arb.h */
+#cmakedefine HAVE_FLINT_ARB_H 1
+
 /* whether frobby has frobby_version >=0.9.4 or constants::version <0.9.4 */
 #cmakedefine HAVE_FROBBY_VERSION 1
 
