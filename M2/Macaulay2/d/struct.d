41e1506ce670deed3fcb70bc5cda9bdd1513e5fa
Mahrud Sayrafi <mahrud@berkeley.edu>
2024-07-28 01:07:46 +0200

    removed reverse from typicalvalues.m2

--- M2/Macaulay2/d/struct.d
+++ M2/Macaulay2/d/struct.d
@@ -4,8 +4,10 @@ use util;
 
 reverse(e:Expr):Expr := (
      when e
+     -- # typical value: reverse, BasicList, BasicList
      is a:Sequence do Expr(reverse(a))
      is a:List do Expr(reverse(a))
+     -- # typical value: reverse, String, String
      is s:stringCell do toExpr(reverse(s.v))
      else WrongArg("a list, sequence, or string"));
 setupfun("reverse",reverse);
