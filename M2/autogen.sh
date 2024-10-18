98bf8b0376e102dc8f53ff881723a9c28d437da2
Doug Torrance <dtorrance@piedmont.edu>
2024-10-18 17:43:10 -0400

    Allow running autogen.sh from build directory

--- M2/autogen.sh
+++ M2/autogen.sh
@@ -2,31 +2,34 @@
 
 set -e
 
+srcdir=$(dirname $0)
+test -z "$srcdir" && srcdir=.
+
 echo "-- Generating configure script"
-autoreconf --verbose --force --install
+autoreconf --verbose --force --install $srcdir
 
 # These files may not be created by older versions of autoconf
-if test ! -f config.guess
+if test ! -f $srcdir/config.guess
 then
-    cp -v $(automake --print-libdir)/config.guess .
+    cp -v $(automake --print-libdir)/config.guess $srcdir
 fi
 
-if test ! -f config.sub
+if test ! -f $srcdir/config.sub
 then
-    cp -v $(automake --print-libdir)/config.sub .
+    cp -v $(automake --print-libdir)/config.sub $srcdir
 fi
 
-if test ! -f install-sh
+if test ! -f $srcdir/install-sh
 then
-    cp -v $(automake --print-libdir)/install-sh .
+    cp -v $(automake --print-libdir)/install-sh $srcdir
 fi
 
-if test ! -f Macaulay2/editors/emacs/M2.el
+if test ! -f $srcdir/Macaulay2/editors/emacs/M2.el
 then
-    if test -e ../.git
+    if test -e $srcdir/../.git
     then
 	echo "-- Updating M2-emacs submodule"
-	git submodule update --init Macaulay2/editors/emacs
+	git submodule update --init $srcdir/Macaulay2/editors/emacs
     else
 	echo "-- Warning: Not in a git repository; unable to update M2-emacs submodule."
 	echo "-- You may download it from https://github.com/Macaulay2/M2-emacs and extract"
