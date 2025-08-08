06ee9473d150c06fb0b81b5173f0c9930cdbdfa3
Doug Torrance <dtorrance@piedmont.edu>
2025-08-08 17:02:36 -0400

    Add common header guard for all d-generated headers (D_GENERATED_H)

--- M2/Macaulay2/c/scc1.c
+++ M2/Macaulay2/c/scc1.c
@@ -333,6 +333,8 @@ int main(int argc, char **argv){
 		    if (NULL == freopen(n,"w", stdout)) {
 			 fatal("can't open file %s",n);
 			 }
+		    put("#ifndef D_GENERATED_H\n");
+		    put("#define D_GENERATED_H\n");
 		    printf("#ifndef %s_included\n",targetname);
 		    printf("#define %s_included\n",targetname);
 		    declarationsstrings = reverse(declarationsstrings);
@@ -347,6 +349,7 @@ int main(int argc, char **argv){
 		    /* printtypecodes(); */
 		    cprinttypes();
 		    put(declarations_trailer);
+		    put("#endif\n");
 		    put("#endif\n");
 		    }
 	       if (TRUE) {
