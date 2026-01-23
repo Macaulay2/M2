46176b8587cb4b1cf6f516d4228e78da8fe49bde
Doug Torrance <dtorrance@piedmont.edu>
2026-01-23 10:28:29 -0500

    Use list() constructor where possible
    The only time we ever needed a List instead of an Expr when creating a
    List object directly was in copy, and we only needed this in a few
    places, so we just use the list() constructor and copy the sequence
    instead.
    
    Also simplify sethash -- no need to pass is_mutable separately since
    that information is kept in the list.
    
    Also remove unused "newlist" function

--- M2/Macaulay2/d/actors5.d
+++ M2/Macaulay2/d/actors5.d
@@ -60,15 +60,13 @@ prependfun(e:Expr):Expr := (
 			 foreach t in y do provide t;
 			 )		    
 		    )
-	       is y:List do (
-		    r := List(
+	       is y:List do list(
 			 y.Class,
 			 new Sequence len length(y.v) + 1 do (
 			      provide elem;
 			      foreach t in y.v do provide t;
 			      ),
-			 hash_t(0),y.Mutable);
-		    Expr(sethash(r,y.Mutable)))
+			 y.Mutable)
 	       else WrongArg(1+1,"a list or sequence")
 	       )
 	  )
@@ -90,15 +88,13 @@ appendfun(e:Expr):Expr := (
 			 provide elem;
 			 )		    
 		    )
-	       is y:List do (
-		    r := List(
+	       is y:List do list(
 			 y.Class,
 			 new Sequence len length(y.v) + 1 do (
 			      foreach t in y.v do provide t;
 			      provide elem;
 			      ),
-			 hash_t(0),y.Mutable);
-		    Expr(sethash(r,y.Mutable)))
+			 y.Mutable)
 	       else WrongArg(0+1,"a list or sequence")
 	       )
 	  )
@@ -1644,7 +1640,7 @@ setupfun("isGlobalSymbol",isGlobalSymbol);
 --      else WrongNumArgs(0));
 -- setupfun("history",history);
 
-listFrame(s:Sequence):Expr := Expr(List(mutableListClass, s, nextHash(), true));	  
+listFrame(s:Sequence):Expr := list(mutableListClass, s, true);
 listFrame(f:Frame):Expr := if f.frameID == 0 then listFrame(emptySequence) else listFrame(f.values); -- refuse to defeat the protection of global variables
 frame(e:Expr):Expr := (
      when e
