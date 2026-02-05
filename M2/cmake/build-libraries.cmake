564465b395d8234d061b8b54510ba1af8eb69642
Doug Torrance <dtorrance@piedmont.edu>
2026-02-04 23:53:55 -0500

    Bump ntl to 11.6.0

--- M2/cmake/build-libraries.cmake
+++ M2/cmake/build-libraries.cmake
@@ -283,13 +283,12 @@ _ADD_COMPONENT_DEPENDENCY(libraries mpfi "gmp;mpfr" MPFI_FOUND)
 
 # http://shoup.net/ntl
 ExternalProject_Add(build-ntl
-  URL               https://github.com/libntl/ntl/archive/refs/tags/v11.5.1.tar.gz
-  URL_HASH          SHA256=ef578fa8b6c0c64edd1183c4c303b534468b58dd3eb8df8c9a5633f984888de5
+  URL               https://libntl.org/ntl-11.6.0.tar.gz
+  URL_HASH          SHA256=bc0ef9aceb075a6a0673ac8d8f47d5f8458c72fe806e4468fbd5d3daff056182
   PREFIX            libraries/ntl
   SOURCE_DIR        libraries/ntl/build
   DOWNLOAD_DIR      ${CMAKE_SOURCE_DIR}/BUILD/tarfiles
   BUILD_IN_SOURCE   ON
-  PATCH_COMMAND     patch --batch -p1 < ${CMAKE_SOURCE_DIR}/libraries/ntl/patch-11.5.1
   CONFIGURE_COMMAND cd src && ${CONFIGURE} PREFIX=${M2_HOST_PREFIX}
                       #-C --cache-file=${CONFIGURE_CACHE}
                       TUNE=generic
